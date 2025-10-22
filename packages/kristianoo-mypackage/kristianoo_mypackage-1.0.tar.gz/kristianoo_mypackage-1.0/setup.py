98bd3ea1877866652564caf6266f6c6c5ce2b959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:06:20 +0000

    Add package kristianoo-mypackage/1.0/kristianoo_mypackage-1.0.tar.gz

---
+++ packages/kristianoo-mypackage/kristianoo_mypackage-1.0.tar.gz/kristianoo_mypackage-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kristianoo_mypackage',
+    version='1.0',
+    packages=['kristianoo_mypackage'],
+    include_package_data=True,
+)
