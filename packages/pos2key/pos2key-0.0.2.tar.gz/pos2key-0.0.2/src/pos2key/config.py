cd4662d30456fc1c6aae78200c75d48995baa504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:05:44 +0000

    Add package pos2key/0.0.2/pos2key-0.0.2.tar.gz

---
+++ packages/pos2key/pos2key-0.0.2.tar.gz/pos2key-0.0.2/src/pos2key/config.py
@@ -0,0 +1,16 @@
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Config(metaclass=Singleton):
+    def __init__(self):
+        self.settings = {}
+
+    def set(self, key, value):
+        self.settings[key] = value
+
+    def get(self, key, default=None):
+        return self.settings.get(key, default)
