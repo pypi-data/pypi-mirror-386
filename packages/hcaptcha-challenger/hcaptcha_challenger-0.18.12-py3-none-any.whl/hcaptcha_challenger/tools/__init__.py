44946a488fb2e419c3e59ffd9c1ec94f4eadb38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:04:55 +0000

    Add package hcaptcha-challenger/0.18.12/hcaptcha_challenger-0.18.12-py3-none-any.whl

---
+++ packages/hcaptcha-challenger/hcaptcha_challenger-0.18.12-py3-none-any.whl/hcaptcha_challenger/tools/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Time       : 2023/8/19 17:52
+# Author     : QIN2DIM
+# GitHub     : https://github.com/QIN2DIM
+# Description:
+
+from .challenge_classifier import ChallengeClassifier
+from .image_classifier import ImageClassifier
+from .spatial_bbox_reasoning import SpatialBboxReasoner
+from .spatial_path_reasoning import SpatialPathReasoner
+from .spatial_point_reasoning import SpatialPointReasoner
+
+__all__ = [
+    "ImageClassifier",
+    'ChallengeClassifier',
+    'SpatialPathReasoner',
+    'SpatialPointReasoner',
+    'SpatialBboxReasoner',
+]
