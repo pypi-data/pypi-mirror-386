a52186d07ebf78a78428b6bb45f49e5ae00b838f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:06:08 +0000

    Add package MeUtils/2025.10.22.14.4.55/meutils-2025.10.22.14.4.55-py3-none-any.whl

---
+++ packages/MeUtils/meutils-2025.10.22.14.4.55-py3-none-any.whl/meutils/apis/gradio_api/deepseek_ocr.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : deepseek_orc
+# @Time         : 2025/10/21 15:58
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : demo
+# @Time         : 2024/8/7 09:06
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+import gradio as gr
+
+from gradio_client import Client, file
+
+# client = Client("http://120.92.209.146:8887")
+# print(client.view_api(all_endpoints=True))
+
+# print(client.view_api())
+
+#
+# decode_type='sampling'
+# parameter_13 = {
+#     'files': '/Users/betterme/PycharmProjects/AI/QR.png',
+#     'text': '解释下',
+#     'index': 0
+# }
+#
+# client.predict(parameter_13, api_name="/respond")
+# print(client.predict("parameter_14", api_name="/select_chat_type"))
+
+from gradio_client import Client, handle_file
+
+client = Client("khang119966/DeepSeek-OCR-DEMO")
+# result = client.predict(
+# 		image=handle_file('https://s3.ffire.cc/files/pdf_to_markdown.jpg'),
+# 		model_size="Gundam (Recommended)",
+# 		task_type="📄 Convert to Markdown",
+# 		ref_text="Hello!!",
+# 		api_name="/process_ocr_task"
+# )
+# print(result)
+
+
+from gradio_client import Client, handle_file
+
+client = Client("axiilay/DeepSeek-OCR-Demo",
+                # download_files=f"./",
+                download_files=False
+
+                )
+result = client.predict(
+    image=handle_file('https://s3.ffire.cc/files/pdf_to_markdown.jpg'),
+    model_size="Gundam (Recommended)",
+    task_type="Convert to Markdown",
+    is_eval_mode=False,
+    api_name="/process_image",
+
+)
+print(result)
+
---
+++ packages/MeUtils/meutils-2025.10.22.14.4.55-py3-none-any.whl/meutils/apis/oai/images.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : images
+# @Time         : 2025/10/17 22:55
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
