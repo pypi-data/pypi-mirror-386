003958779b8e1eaac931a377295a0811d01dabe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:05:45 +0000

    Add package tinyshift/0.8.3/tinyshift-0.8.3.tar.gz

---
+++ packages/tinyshift/tinyshift-0.8.3.tar.gz/tinyshift-0.8.3/tinyshift/tracker/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024-2025 Lucas Leão
+# tinyshift - A small toolbox for mlops
+# Licensed under the MIT License
+
+
+from .continuous import ContinuousDriftTracker
+from .categorical import CategoricalDriftTracker
+from .performance import PerformanceTracker
+from .anomaly import AnomalyTracker
