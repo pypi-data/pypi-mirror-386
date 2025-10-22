fcf00d321198d84f0fdeca651eb054c442f62eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:04:56 +0000

    Add package 2048-river/0.1.6/2048_river-0.1.6.tar.gz

---
+++ packages/2048-river/2048_river-0.1.6.tar.gz/2048_river-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="2048-river",
+    version="0.1.6",
+    packages=find_packages(), 
+    include_package_data=True,
+    install_requires=["requests", "pygame"],
+    entry_points={
+        "console_scripts": [
+            "2048-river=main_launcher:main", 
+        ],
+    },
+)
