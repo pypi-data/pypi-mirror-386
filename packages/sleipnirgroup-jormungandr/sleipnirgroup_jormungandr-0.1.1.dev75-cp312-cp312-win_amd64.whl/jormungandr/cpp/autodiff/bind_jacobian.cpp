376cbcc9734873b3e604e6ed50218292eada60f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:04:03 +0000

    Add package sleipnirgroup-jormungandr/0.1.1.dev75/sleipnirgroup_jormungandr-0.1.1.dev75-cp312-cp312-win_amd64.whl

---
+++ packages/sleipnirgroup-jormungandr/sleipnirgroup_jormungandr-0.1.1.dev75-cp312-cp312-win_amd64.whl/jormungandr/cpp/autodiff/bind_jacobian.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) Sleipnir contributors
+
+#include <nanobind/eigen/sparse.h>
+#include <nanobind/nanobind.h>
+#include <sleipnir/autodiff/jacobian.hpp>
+
+#include "docstrings.hpp"
+
+namespace nb = nanobind;
+
+namespace slp {
+
+void bind_jacobian(nb::class_<Jacobian>& cls) {
+  using namespace nb::literals;
+
+  cls.def(nb::init<Variable, Variable>(), "variable"_a, "wrt"_a,
+          DOC(slp, Jacobian, Jacobian));
+  cls.def(nb::init<Variable, VariableMatrix>(), "variable"_a, "wrt"_a,
+          DOC(slp, Jacobian, Jacobian));
+  cls.def(nb::init<VariableMatrix, VariableMatrix>(), "variables"_a, "wrt"_a,
+          DOC(slp, Jacobian, Jacobian));
+  cls.def("get", &Jacobian::get, DOC(slp, Jacobian, get));
+  cls.def("value", &Jacobian::value, DOC(slp, Jacobian, value));
+}
+
+}  // namespace slp
