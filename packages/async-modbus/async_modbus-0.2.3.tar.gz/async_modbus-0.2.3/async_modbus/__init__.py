dede39599c3329214fe7f7b3e4c57515fa7cf004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 17:04:18 +0000

    Add package async-modbus/0.2.3/async_modbus-0.2.3.tar.gz

---
+++ packages/async-modbus/async_modbus-0.2.3.tar.gz/async_modbus-0.2.3/async_modbus/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Async ModBus project
+#
+# Copyright (c) 2020-2022 Tiago Coutinho
+# Distributed under the GNU General Public License v3. See LICENSE for info.
+from .core import AsyncClient  # noqa
+from .core import AsyncRTUClient  # noqa
+from .core import AsyncTCPClient  # noqa
+from .core import modbus_for_url  # noqa
+
+
+__author__ = """Tiago Coutinho"""
+__email__ = "coutinhotiago@gmail.com"
+__version__ = "0.2.3"
---
+++ packages/async-modbus/async_modbus-0.2.3.tar.gz/async_modbus-0.2.3/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of the async-modbus project
+#
+# Copyright (c) 2020-2022 Tiago Coutinho
+# Distributed under the GNU General Public License v3. See LICENSE for more info.
+"""The setup script."""
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
