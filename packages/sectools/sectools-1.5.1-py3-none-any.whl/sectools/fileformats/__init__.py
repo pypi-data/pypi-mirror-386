1065b50f0f456f2567b3d51689c1b8b43ccbce9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:06:08 +0000

    Add package sectools/1.5.1/sectools-1.5.1-py3-none-any.whl

---
+++ packages/sectools/sectools-1.5.1-py3-none-any.whl/sectools/fileformats/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : __init__.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 6 Nov 2022
+
+from .Markdown import Markdown
+
+__all__ = ["Markdown"]
