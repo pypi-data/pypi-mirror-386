cef820427481291cbe75b50d13096252b21f1ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 17:04:54 +0000

    Add package fkin-anfu/0.5.1/fkin_anfu-0.5.1.tar.gz

---
+++ packages/fkin-anfu/fkin_anfu-0.5.1.tar.gz/fkin_anfu-0.5.1/fkin_anfu/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# vim: set tabstop=2 shiftwidth=2 textwidth=80 expandtab :
+#
+#
+"""
+fkin_anfu init
+
+@author: cyhfvg
+@date: 2025/05/17
+"""
+__version__ = "0.5.1"
