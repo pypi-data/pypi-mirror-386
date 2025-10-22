e75130c07dd2612b73db854157889765bbe04906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:05:44 +0000

    Add package sentry-kafka-schemas/2.1.10/sentry-kafka-schemas-2.1.10.tar.gz

---
+++ packages/sentry-kafka-schemas/sentry-kafka-schemas-2.1.10.tar.gz/sentry-kafka-schemas-2.1.10/python/sentry_kafka_schemas/schema_types/profiles_v1.py
@@ -0,0 +1,30 @@
+from typing import List, TypedDict, Union, Any, Required, Dict
+
+
+class Profile(TypedDict, total=False):
+    """
+    profile.
+
+    profile data from relay
+    """
+
+    type: Required[str]
+    """ Required property """
+
+    organization_id: Required[int]
+    """ Required property """
+
+    project_id: Required[int]
+    """ Required property """
+
+    key_id: Required[int]
+    """ Required property """
+
+    received: int
+    payload: Required[Union[str, Union[int, float], Dict[str, Any], List[Any], bool, None]]
+    """
+    bytes
+
+    Required property
+    """
+
