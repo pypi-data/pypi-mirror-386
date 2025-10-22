8b310652e9ef559de6a85331ad0f0d09e6f7101b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:04:37 +0000

    Add package nsj-rest-lib/4.16.0/nsj_rest_lib-4.16.0-py3-none-any.whl

---
+++ packages/nsj-rest-lib/nsj_rest_lib-4.16.0-py3-none-any.whl/nsj_rest_lib/descriptor/__init__.py
@@ -0,0 +1,9 @@
+from .dto_field      import DTOField, DTOFieldFilter, DTOAutoIncrementField
+from .dto_aggregator import DTOAggregator
+
+__all__ = [
+    'DTOField',
+    'DTOFieldFilter',
+    'DTOAutoIncrementField',
+    'DTOAggregator',
+]
