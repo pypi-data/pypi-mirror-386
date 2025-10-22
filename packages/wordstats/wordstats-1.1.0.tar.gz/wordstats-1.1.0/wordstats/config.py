0c39d23eed5d7ee25ef9a481e261a62602d408d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 17:04:43 +0000

    Add package wordstats/1.1.0/wordstats-1.1.0.tar.gz

---
+++ packages/wordstats/wordstats-1.1.0.tar.gz/wordstats-1.1.0/wordstats/config.py
@@ -0,0 +1,26 @@
+import os
+
+import platform
+import tempfile
+import logging as log
+
+tempdir = "/tmp" if platform.system() == "Darwin" else tempfile.gettempdir()
+
+db_uri = 'sqlite:///' + tempdir  + '/wordinfo.db'
+
+log.debug("running with DB URI: " + db_uri )
+
+# for mysql, we want to declare the default character encoding
+# for comm. with the db
+if db_uri.startswith("mysql"):
+    db_uri += '?charset=utf8'
+
+DATA_HERMIT_FOLDER = 'language_data/hermitdave/2018'
+
+package_directory = os.path.dirname(os.path.abspath(__file__))
+DATA_COMMON_FOLDER = package_directory + os.sep +'language_data/common'
+
+# Use full frequency lists instead of limiting to top N words
+# Filter by minimum occurrence count to exclude noise (typos, rare compounds, proper names, etc.)
+MIN_OCCURRENCE_COUNT = 10  # Words must occur at least 10 times in the corpus to be included
+MAX_WORDS = None           # No limit - use all words above MIN_OCCURRENCE_COUNT
---
+++ packages/wordstats/wordstats-1.1.0.tar.gz/wordstats-1.1.0/wordstats/language_info.py
@@ -0,0 +1,209 @@
+import codecs
+import os
+import sqlite3
+from datetime import datetime
+
+import sqlalchemy
+from sqlalchemy import Table
+
+from .word_info import WordInfo, UnknownWordInfo
+from .utils.mem_footprint import total_size
+from .base_service import BaseService, Base
+from .config import MIN_OCCURRENCE_COUNT, MAX_WORDS
+from .metrics_computers import *
+import logging as log
+
+
+class LanguageInfo(object):
+    def __init__(self, language_id):
+        self.language_id = language_id
+        self.word_info_dict = dict()
+
+    @classmethod
+    def load(cls, language_code, use_sqlite_cache=False):
+        from wordstats.loading_from_hermit import load_language_from_hermit
+
+        if not use_sqlite_cache:
+            return load_language_from_hermit(language_code)
+
+        log.info(f"loading {language_code} from DB")
+
+        try:
+            lang = LanguageInfo.load_from_db(language_code)
+        except sqlalchemy.exc.PendingRollbackError:
+            print("Rolling back session in WordStats to clear a previous race condition")
+            BaseService.session.rollback()
+            lang = LanguageInfo.load_from_db(language_code)
+
+        if len(lang.all_words()) == 0:
+
+            log.info(f"loading {language_code} from file")
+            lang = load_language_from_hermit(language_code)
+            log.info(f"caching {language_code} to DB")
+            try:
+                lang.cache_to_db()
+            except sqlite3.IntegrityError:
+                # If this happens, we've very likely had a race condition
+                BaseService.session.rollback()
+
+        return lang
+
+    def get(self, word):
+        """
+
+            Main use of this class is this method which retrieves
+            information about a given word.
+
+            If the word is not found we return an UnknownWordInfo
+            which behaves like a WordInfo with default values.
+
+        :param word: utf-8 string
+        :return:
+        """
+
+        # TODO: think whether we want to care about the lowercase or not. For now, we don't
+        word = word.lower()
+        if word in self.word_info_dict:
+            return self.word_info_dict[word]
+        else:
+            return UnknownWordInfo()
+
+    def all_words(self):
+        return list(self.word_info_dict.keys())
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    @classmethod
+    def load_from_file(cls, file_name, lang_code):
+        """
+        Loads the word info from the given file
+        where the format expected is a word and the number
+        of occurrences per line. e.g.
+
+        der 123123
+        die 123000
+        das 121023
+        ...
+
+        :param file_name:
+        :param lang_code:
+        :return:
+        """
+
+        new_registry = cls(lang_code)
+
+        word_rank = 0
+
+        package_directory = os.path.dirname(os.path.abspath(__file__))
+
+        with codecs.open(package_directory + os.sep + file_name, encoding="utf8") as words_file:
+            words_list = words_file.read().splitlines()
+
+            for word_and_freq in words_list:
+                word_and_freq_array = word_and_freq.split(" ")
+                word = word_and_freq_array[0]
+                occurrences = int(word_and_freq_array[1])
+
+                # Skip words below minimum occurrence count (likely typos/noise)
+                if occurrences < MIN_OCCURRENCE_COUNT:
+                    continue
+
+                frequency = compute_frequency(occurrences)
+                difficulty = compute_difficulty(word_rank)
+                importance = compute_importance(occurrences)
+                klevel = compute_klevel(word_rank)
+
+                word_rank += 1
+
+                # Include all words above MIN_OCCURRENCE_COUNT (MAX_WORDS is now optional)
+                if MAX_WORDS is None or word_rank <= MAX_WORDS:
+
+                    if word.lower() not in new_registry.word_info_dict:
+                        r = WordInfo(
+                            word.lower(),
+                            lang_code,
+                            frequency,
+                            difficulty,
+                            importance,
+                            word_rank,
+                            klevel)
+                        new_registry.word_info_dict[word.lower()] = r
+
+        return new_registry
+
+    @classmethod
+    def load_from_db(cls, language_id):
+        """
+        Assumes the ~./word_info file contains
+        information about how to connect to the
+        database. Also, assumes the database contains
+        the information.
+
+        :param language_id:
+        :return:
+        """
+
+        new_registry = cls(language_id)
+
+        all_word_info_items = WordInfo.find_all(language_id)
+
+        for each in all_word_info_items:
+            new_registry.word_info_dict[each.word] = each
+
+        return new_registry
+
+    def cache_to_db(self):
+        """
+        Useful to save the informations about a given language
+        in the DB. Loading from the DB is faster than from file.
+        See the tests file
+        :return:
+        """
+
+        def clear_corresponding_entries_in_db(self):
+            table = Table('word_info', Base.metadata, autoload=True, autoload_with=BaseService.engine)
+            words = BaseService.session.query(table).filter(table.c.language_id == self.language_id)
+            words.delete(synchronize_session=False)
+
+        clear_corresponding_entries_in_db(self)
+        for word_info in list(self.word_info_dict.values()):
+            BaseService.session.add(word_info)
+        BaseService.session.commit()
+
+    # Everything that follows is private
+    def print_load_stats(self, a, b):
+        memory_footprint = total_size(self.word_info_dict, set()) / 1024 / 1024
+        print(("Elapsed time to load the {0} data: {1} ({2} entries)".format(self.language_id, b - a,
+                                                                             len(self.word_info_dict))))
+        print(("Required memory for the {0} registry: {1}MB".format(self.language_id, memory_footprint)))
+
+    @classmethod
+    def profile_load_from_db(cls, language_id, output=False):
+
+        a = datetime.now()
+        new_registry = cls.load_from_db(language_id)
+        b = datetime.now()
+        if output:
+            new_registry.print_load_stats(a, b)
+
+        return b - a
+
+    @classmethod
+    def profile_load_from_file(cls, file_name, language_id, output=False):
+
+        a = datetime.now()
+        new_registry = cls.load_from_file(file_name, language_id)
+        b = datetime.now()
+        if output:
+            new_registry.print_load_stats(a, b)
+
+        return b - a
+
+    @classmethod
+    def load_in_memory_for(cls, language_codes, use_sqlite_cache=False):
+        from .word_stats import Word
+
+        for each in language_codes:
+            Word.stats_dict[each] = cls.load(each, use_sqlite_cache)
+
