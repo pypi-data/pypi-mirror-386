b116a85503fa6123fa05a083ecefe68bfcb72dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 17:05:09 +0000

    Add package sounava/0.0.1/sounava-0.0.1.tar.gz

---
+++ packages/sounava/sounava-0.0.1.tar.gz/sounava-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sounava",
+    version="0.0.1",
+    author="Ankit Mehta",
+    author_email="starexx.m@gmail.com",
+    description="A telegram SDK offering clean and simple route decorators.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
