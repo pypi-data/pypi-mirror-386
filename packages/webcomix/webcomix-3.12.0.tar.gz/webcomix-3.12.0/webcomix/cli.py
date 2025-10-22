6405655e9cda513d59dde29841b69d3e86c5296f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:04:34 +0000

    Add package webcomix/3.12.0/webcomix-3.12.0.tar.gz

---
+++ packages/webcomix/webcomix-3.12.0.tar.gz/webcomix-3.12.0/webcomix/cli.py
@@ -0,0 +1,313 @@
+#! python3
+
+import click
+
+from webcomix.comic import Comic
+from webcomix.exceptions import CrawlerBlocked, NextLinkNotFound
+from webcomix.search import discovery
+from webcomix.supported_comics import supported_comics
+from webcomix.docker import DockerManager
+
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command()
+def comics():
+    """
+    Shows all predefined comics
+    """
+    comics_content = [
+        "{}: {}".format(key, value["start_url"])
+        for key, value in sorted(supported_comics.items())
+    ]
+
+    click.echo("\n".join(comics_content))
+
+
+@cli.command()
+@click.argument("name", type=click.Choice(list(supported_comics.keys())))
+@click.option(
+    "--cbz", is_flag=True, default=False, help="Outputs the comic as a cbz file"
+)
+@click.option(
+    "--title", is_flag=True, default=False, help="Add title of comic in image names"
+)
+@click.option(
+    "--verbose", "-v", is_flag=True, default=False, help="Add debugging output"
+)
+def download(name, cbz, title, verbose):
+    """
+    Downloads a predefined comic by name
+    """
+    if name in list(supported_comics.keys()):
+        comic = Comic(**supported_comics[name], title=title, debug=verbose)
+        download_webcomic(comic, cbz)
+
+
+@cli.command()
+@click.argument("name", type=click.STRING)
+@click.option(
+    "--start-url",
+    "--start_url",
+    prompt=True,
+    type=click.STRING,
+    help="URL of the comic's first page",
+)
+@click.option(
+    "--end-url",
+    "--end_url",
+    prompt=False,
+    type=click.STRING,
+    help="URL of the comic's last page",
+)
+@click.option(
+    "--start-page",
+    "--start_page",
+    type=click.INT,
+    default=1,
+    help="Number of comic's first page to be downloaded",
+)
+@click.option(
+    "--cbz", default=False, is_flag=True, help="Outputs the comic as a cbz file"
+)
+@click.option(
+    "--single-page",
+    "--single_page",
+    default=False,
+    is_flag=True,
+    help="Downloads from a single webpage",
+)
+@click.option(
+    "--javascript",
+    "--js",
+    "-j",
+    default=False,
+    is_flag=True,
+    help="Renders javascript in the page (slower)",
+)
+@click.option(
+    "--title", is_flag=True, default=False, help="Add title of comic in image names"
+)
+@click.option(
+    "--alt-text",
+    "-a",
+    default=None,
+    type=click.STRING,
+    help="Optional XPath to fetch an additionnal text while scraping",
+)
+@click.option(
+    "--yes", "-y", default=False, is_flag=True, help="Skips the verification prompt"
+)
+@click.option(
+    "--verbose", "-v", is_flag=True, default=False, help="Add debugging output"
+)
+def search(
+    name,
+    start_url,
+    end_url,
+    start_page,
+    cbz,
+    single_page,
+    javascript,
+    title,
+    alt_text,
+    yes,
+    verbose,
+):
+    """
+    Downloads a webcomic using a general XPath
+    """
+    with DockerManager(javascript):
+        comic, validation = discovery(
+            name,
+            start_url,
+            end_url=end_url,
+            start_page=start_page,
+            alt_text=alt_text,
+            single_page=single_page,
+            javascript=javascript,
+            title=title,
+            debug=verbose,
+        )
+        if comic is not None:
+            print_verification(validation)
+            click.echo("Verify that the links above are correct.")
+            if yes or click.confirm("Are you sure you want to proceed?"):
+                download_webcomic(comic, cbz)
+
+
+@cli.command()
+@click.argument("name", type=click.STRING)
+@click.option(
+    "--start-url",
+    "--start_url",
+    prompt=True,
+    type=click.STRING,
+    help="URL of the comic's first page to be downloaded",
+)
+@click.option(
+    "--end-url",
+    "--end_url",
+    prompt=False,
+    type=click.STRING,
+    help="URL of the last comic's page to be downloaded",
+)
+@click.option(
+    "--start-page",
+    "--start_page",
+    type=click.INT,
+    default=1,
+    help="Number of comic's first page to be downloaded",
+)
+@click.option(
+    "--image-xpath",
+    "--image_xpath",
+    prompt=True,
+    type=click.STRING,
+    help="XPath expression giving the url to the image",
+)
+@click.option(
+    "--next-page-xpath",
+    "--next_page_xpath",
+    prompt=True,
+    type=click.STRING,
+    help="XPath expression giving the url to the next page",
+)
+@click.option(
+    "--block-xpath",
+    "--block_xpath",
+    "-x",
+    type=click.STRING,
+    multiple=True,
+    help="XPath expressions that block the download of an image when any expression has elements",
+)
+@click.option(
+    "--cbz", default=False, is_flag=True, help="Outputs the comic as a cbz file"
+)
+@click.option(
+    "--single-page",
+    "--single_page",
+    "-s",
+    default=False,
+    is_flag=True,
+    help="Downloads from a single webpage",
+)
+@click.option(
+    "--javascript",
+    "--js",
+    "-j",
+    default=False,
+    is_flag=True,
+    help="Renders javascript in the page (slower)",
+)
+@click.option(
+    "--title", is_flag=True, default=False, help="Add title of comic in image names"
+)
+@click.option(
+    "--alt-text",
+    "-a",
+    default=None,
+    type=click.STRING,
+    help="XPath to fetch an additionnal text while scraping",
+)
+@click.option(
+    "--cookie",
+    "-c",
+    default=[],
+    type=click.Tuple([str, str]),
+    multiple=True,
+    help="Value of cookie to add to the requests",
+)
+@click.option(
+    "--yes", "-y", default=False, is_flag=True, help="Skips the verification prompt"
+)
+@click.option(
+    "--verbose", "-v", is_flag=True, default=False, help="Add debugging output"
+)
+def custom(
+    name,
+    start_url,
+    end_url,
+    start_page,
+    next_page_xpath,
+    image_xpath,
+    block_xpath,
+    cbz,
+    single_page,
+    javascript,
+    title,
+    alt_text,
+    cookie,
+    yes,
+    verbose,
+):
+    """
+    Downloads a user-defined webcomic
+    """
+    with DockerManager(javascript):
+        comic = Comic(
+            name,
+            start_url,
+            image_xpath,
+            next_page_xpath,
+            end_url=end_url,
+            block_selectors=block_xpath,
+            start_page=start_page,
+            alt_text=alt_text,
+            single_page=single_page,
+            javascript=javascript,
+            title=title,
+            cookies=cookie,
+            debug=verbose,
+        )
+        try:
+            validation = comic.verify_xpath()
+        except NextLinkNotFound as exception:
+            click.echo(
+                "Could not find next link of: {} \nwith next page XPath expression: {}".format(
+                    exception.failed_url, exception.next_page_xpath
+                )
+            )
+            click.echo(
+                "Have you tried testing your XPath expression with 'scrapy shell'?"
+            )
+            raise click.Abort()
+        try:
+            print_verification(validation)
+        except CrawlerBlocked as exception:
+            click.echo("{} could not be accessed with webcomix.".format(name))
+            click.echo(
+                "Chances are the website you're trying to download images from doesn't want to be scraped."
+            )
+            raise click.Abort()
+        click.echo("Verify that the links above are correct.")
+        if yes or click.confirm("Are you sure you want to proceed?"):
+            download_webcomic(comic, cbz)
+
+
+def main():
+    cli(windows_expand_args=False)
+
+def print_verification(validation):
+    """
+    Prints the verification given by the verify_xpath function
+    """
+    if validation is None:
+        raise CrawlerBlocked()
+    for item in sorted(validation, key=lambda x: x.get("page")):
+        output = "Page {}:\nPage URL: {}\nImage URLs:\n{}\n".format(
+            item.get("page"), item.get("url"), "\n".join(item.get("image_urls"))
+        )
+        if item.get("alt_text") is not None:
+            output += "Alt text: {}\n".format(item.get("alt_text"))
+        click.echo(output)
+
+
+def download_webcomic(comic, cbz):
+    comic.download()
+    if cbz:
+        comic.convert_to_cbz()
---
+++ packages/webcomix/webcomix-3.12.0.tar.gz/webcomix-3.12.0/webcomix/tests/test_comic_availability.py
@@ -0,0 +1,32 @@
+import pytest
+import os
+
+from webcomix.comic import Comic
+from webcomix.supported_comics import supported_comics
+from webcomix.util import check_first_pages
+
+
+# TODO: Handle 403 errors
+#  supported_comics_ignored = {
+    #  k: v
+    #  for k, v in supported_comics.items()
+    #  if not (
+        #  (k == "TheAbominableCharlesChristopher" or k == "Lackadaisy")
+        #  and os.environ.get("CI", False)
+    #  )
+#  }
+
+
+@pytest.mark.flaky(reruns=5, reruns_delay=60)
+@pytest.mark.slow
+@pytest.mark.parametrize("comic_name", supported_comics.keys())
+def test_supported_comics(comic_name):
+    comic = Comic(**supported_comics[comic_name], debug=True)
+    first_pages = comic.verify_xpath()
+    try:
+        check_first_pages(first_pages)
+    except AssertionError:
+        print("Comic failed! First pages:")
+        print(first_pages)
+        assert False
+        pytest.fail("Comic could not be fetched!")
