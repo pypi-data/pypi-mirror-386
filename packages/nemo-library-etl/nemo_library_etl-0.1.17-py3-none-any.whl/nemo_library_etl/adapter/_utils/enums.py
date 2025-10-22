5a9a665444b41a6a0267140ba7896cf2cc2ed5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 17:04:26 +0000

    Add package nemo-library-etl/0.1.17/nemo_library_etl-0.1.17-py3-none-any.whl

---
+++ packages/nemo-library-etl/nemo_library_etl-0.1.17-py3-none-any.whl/nemo_library_etl/adapter/_utils/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ETLStep(Enum):
+    EXTRACT = "extract"
+    TRANSFORM = "transform"
+    LOAD = "load"
+
+class ETLAdapter(Enum):
+    GEDYS = "gedys"
+    ZENTIS = "zentis"
+    GENERICODBC = "genericodbc"
+    INFORCOM = "inforcom"
+    SAPECC = "sapecc"
+
+
