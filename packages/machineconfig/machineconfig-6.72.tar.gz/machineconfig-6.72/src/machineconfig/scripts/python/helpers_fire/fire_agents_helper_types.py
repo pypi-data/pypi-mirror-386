507eede42595872011ef2ee974f2fd6b49840a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:06:13 +0000

    Add package machineconfig/6.72/machineconfig-6.72.tar.gz

---
+++ packages/machineconfig/machineconfig-6.72.tar.gz/machineconfig-6.72/src/machineconfig/scripts/python/helpers_fire/fire_agents_helper_types.py
@@ -0,0 +1,30 @@
+
+from typing import Literal, TypeAlias, TypedDict
+
+
+AGENTS: TypeAlias = Literal["cursor-agent", "gemini", "crush", "q", "opencode"]
+HOST: TypeAlias = Literal["local", "docker"]
+PROVIDER: TypeAlias = Literal["azure", "google", "aws", "openai", "anthropic", "openrouter", "xai"]
+MODEL: TypeAlias = Literal["zai/glm-4.6", "anthropic/sonnet-4.5", "google/gemini-2.5-pro", "openai/gpt-5-codex",
+                           "openrouter/supernova", "x-ai/grok-4-fast:free",
+                           ]
+PROVIDER2MODEL: dict[PROVIDER, list[MODEL]] = {
+    "azure": ["zai/glm-4.6"],
+    "google": ["google/gemini-2.5-pro"],
+    "aws": [],
+    "openai": ["openai/gpt-5-codex"],
+    "anthropic": ["anthropic/sonnet-4.5"],
+    "openrouter": ["openrouter/supernova"],
+    "xai": ["x-ai/grok-4-fast:free"]
+}
+
+class AI_SPEC(TypedDict):
+    provider: PROVIDER
+    model: MODEL
+    agent: AGENTS
+    machine: HOST    
+
+
+AGENT_NAME_FORMATTER = "agent_{idx}_cmd.sh"  # e.g., agent_0_cmd.sh
+SEARCH_STRATEGIES: TypeAlias = Literal["file_path", "keyword_search", "filename_pattern"]
+
