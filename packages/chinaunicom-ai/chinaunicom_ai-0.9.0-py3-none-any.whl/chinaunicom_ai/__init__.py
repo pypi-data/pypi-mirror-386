2d4c05b47e266905cc5620ed51ac982f4473f3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 16:06:32 +0000

    Add package chinaunicom-ai/0.9.0/chinaunicom_ai-0.9.0-py3-none-any.whl

---
+++ packages/chinaunicom-ai/chinaunicom_ai-0.9.0-py3-none-any.whl/chinaunicom_ai/__init__.py
@@ -0,0 +1,11 @@
+# src/chinaunicom_ai/__init__.py
+from . import asr_toolbox
+from . import dify_toolbox
+from . import container_toolbox
+from . import chromadb_toolbox
+from . import algorithm_toolbox
+
+__version__ = "0.9.0"
+__author__ = "zhaoyongzheng"
+
+__all__ = ['asr_toolbox', 'dify_toolbox', 'container_toolbox', 'chromadb_toolbox', 'algorithm_toolbox']
