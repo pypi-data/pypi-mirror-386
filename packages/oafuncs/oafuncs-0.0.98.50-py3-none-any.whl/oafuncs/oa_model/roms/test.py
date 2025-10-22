4ea90218b370d56cc3acd387fc52ec1c33dfe49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 17:04:28 +0000

    Add package oafuncs/0.0.98.50/oafuncs-0.0.98.50-py3-none-any.whl

---
+++ packages/oafuncs/oafuncs-0.0.98.50-py3-none-any.whl/oafuncs/oa_model/roms/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Author: Liu Kun && 16031215@qq.com
+Date: 2025-03-09 16:30:54
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2025-03-09 16:30:54
+FilePath: \\Python\\My_Funcs\\OAFuncs\\oafuncs\\oa_model\\roms\\depth.py
+Description:
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.12
+"""
+
+__all__ = ['test']
+
+def test():
+    print('test')
