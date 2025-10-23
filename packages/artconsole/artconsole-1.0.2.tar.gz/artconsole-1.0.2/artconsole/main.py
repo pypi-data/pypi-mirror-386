8eb249543ef9c1f0beb6c70a3eb3697dce4f7744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:06:02 +0000

    Add package artconsole/1.0.2/artconsole-1.0.2.tar.gz

---
+++ packages/artconsole/artconsole-1.0.2.tar.gz/artconsole-1.0.2/artconsole/main.py
@@ -0,0 +1,7 @@
+from . import DRAWINGS
+
+def print_drawing(name_of_drawing):
+    print(drawings.DRAWINGS[name_of_drawing])
+
+def return_drawing(name_of_drawing):
+    return drawings.DRAWINGS[name_of_drawing]
---
+++ packages/artconsole/artconsole-1.0.2.tar.gz/artconsole-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'artconsole',
+    version = '1.0.2',
+    author = 'Dima M. Shirokov',
+    author_email = 'D.Shirokov05@yandex.ru',
+    description = 'This is a simple library for outputting to the console or receiving simple ASCII art as a string.',
+    packages = find_packages()
+)
