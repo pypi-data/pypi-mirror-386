6a60357c111ae03da961eae78da4fa0edd930aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:48 +0000

    Add package confy-addons/1.1.0/confy_addons-1.1.0-py3-none-any.whl

---
+++ packages/confy-addons/confy_addons-1.1.0-py3-none-any.whl/confy_addons/prefixes.py
@@ -0,0 +1,10 @@
+"""Module defines string prefixes used for different types of messages and keys.
+
+This file is licensed under the GNU GPL-3.0 license.
+See the LICENSE file at the root of this repository for full details.
+"""
+
+SYSTEM_PREFIX = 'system-message:'
+KEY_EXCHANGE_PREFIX = 'key-exchange:'
+AES_KEY_PREFIX = 'aes-key:'
+AES_PREFIX = 'enc:'
