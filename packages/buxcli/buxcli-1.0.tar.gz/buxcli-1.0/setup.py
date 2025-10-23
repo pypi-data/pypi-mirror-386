fb6379ec55affcbaf3801ee7371d64301804d4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 15:06:11 +0000

    Add package buxcli/1.0/buxcli-1.0.tar.gz

---
+++ packages/buxcli/buxcli-1.0.tar.gz/buxcli-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="buxcli",  # Package name
+    version="1.0",  # Version
+    packages=find_packages(),  # Finds all packages automatically
+    install_requires=[  # Dependencies
+        "inquirer",
+        "rich"
+    ],
+    entry_points={  # CLI command
+        "console_scripts": [
+            "buxcli=buxcli.main:cli",
+        ],
+    },
+    author="Mohd. Rushaad Buxy",
+    author_email="m.rushaadq@gmail.com",
+    description="A simple CLI tool for project setup automation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rushaad2911/Mycli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
