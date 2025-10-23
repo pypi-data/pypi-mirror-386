448aedfc6b6731e63be4eeea82d50d6911a5fa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 16:05:11 +0000

    Add package ansible-vault-tools/0.1.0/ansible_vault_tools-0.1.0.tar.gz

---
+++ packages/ansible-vault-tools/ansible_vault_tools-0.1.0.tar.gz/ansible_vault_tools-0.1.0/ansible_vault_tools/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Max Mehl <https://mehl.mx>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Initialise importable functions"""
+
+from importlib.metadata import version
+
+__version__ = version("ansible-vault-tools")
