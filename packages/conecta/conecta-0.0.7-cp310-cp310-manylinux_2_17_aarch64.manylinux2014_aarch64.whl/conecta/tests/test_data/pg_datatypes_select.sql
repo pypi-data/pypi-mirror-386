ed4ab92c3ef8dc8c806f7cc27a215d462cd729c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:45 +0000

    Add package conecta/0.0.7/conecta-0.0.7-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/conecta/conecta-0.0.7-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/conecta/tests/test_data/pg_datatypes_select.sql
@@ -0,0 +1,38 @@
+select id,
+       small_int,
+       int_,
+       big_int,
+       -- decimal, ON PURPOSE
+       -- numeric_, ON PURPOSE
+       double_,
+       varchar_,
+       text_,
+       bool_,
+       uuid_,
+       bytea_,
+       date_,
+       time_,
+       --timetz_,
+
+       timestamp_,
+
+       --arrays
+       int_array,
+       smallint_array,
+       bigint_array,
+       real_array,
+       double_array,
+       text_array,
+       uuid_array,
+       bool_array,
+       bytea_array,
+
+       --geo
+       point_,
+       circle_,
+       line_,
+       box_,
+       lseg_,
+       path_,
+       polygon_
+from pg_datatypes
