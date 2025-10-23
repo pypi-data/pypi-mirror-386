93fed042b04df5c9880e8196f091142778ef0ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 08:05:40 +0000

    Add package dm-client-ai/1.0.1/dm_client_ai-1.0.1-py3-none-any.whl

---
+++ packages/dm-client-ai/dm_client_ai-1.0.1-py3-none-any.whl/dm_client/ai/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DmClientAiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dm_client.ai'
+    label = 'dm_client_ai'
