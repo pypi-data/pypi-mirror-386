26600fbdf6e3d6535f9c4bb2d259ccc2298d9366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:04:44 +0000

    Add package koxs-terminal-tools/0.2.3/koxs_terminal_tools-0.2.3.tar.gz

---
+++ packages/koxs-terminal-tools/koxs_terminal_tools-0.2.3.tar.gz/koxs_terminal_tools-0.2.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+# 读取 README
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+else:
+    long_description = "A comprehensive cryptography utility library"
+
+setup(
+    name="koxs-terminal-tools",
+    version="0.2.3",
+    author="koxs",
+    author_email="2931209205@qq.com",
+    description="Cryptography utility tools for terminal applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pycryptodome>=3.10.1",
+        "cryptography>=3.4.8",
+    ],
+    python_requires=">=3.7",
+    # 移除 license 分类器避免警告
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8", 
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
