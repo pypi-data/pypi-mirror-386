7713f9386c9e9192cf75f436f8a557fa61c88492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 03:19:46 +0000

    Add package numpay/0.0.3/numpay-0.0.3-py3-none-any.whl

---
+++ packages/numpay/numpay-0.0.3-py3-none-any.whl/numpay/__init__.py
@@ -0,0 +1,13 @@
+# ===============================
+# Root-finding and Linear Algebra Codes
+# ===============================
+from .numpay import fixed_point
+from .numpay import bisection
+from .numpay import regula_falsi
+from .numpay import newton_raphson
+from .numpay import secant
+from .numpay import linear
+from .numpay import full
+
+
+
