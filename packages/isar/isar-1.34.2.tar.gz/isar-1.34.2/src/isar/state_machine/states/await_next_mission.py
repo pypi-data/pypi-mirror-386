0308efa3ef7e6d18c39cf2457817c021be11770d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 16:05:21 +0000

    Add package isar/1.34.2/isar-1.34.2.tar.gz

---
+++ packages/isar/isar-1.34.2.tar.gz/isar-1.34.2/src/isar/state_machine/states/await_next_mission.py
@@ -0,0 +1,76 @@
+from typing import TYPE_CHECKING, Callable, List, Optional
+
+from isar.apis.models.models import LockdownResponse
+from isar.config.settings import settings
+from isar.eventhandlers.eventhandler import (
+    EventHandlerBase,
+    EventHandlerMapping,
+    TimeoutHandlerMapping,
+)
+from isar.models.events import Event
+from isar.state_machine.utils.common_event_handlers import (
+    return_home_event_handler,
+    start_mission_event_handler,
+    stop_mission_event_handler,
+)
+
+if TYPE_CHECKING:
+    from isar.state_machine.state_machine import StateMachine
+
+
+class AwaitNextMission(EventHandlerBase):
+
+    def __init__(self, state_machine: "StateMachine"):
+        events = state_machine.events
+
+        def _send_to_lockdown_event_handler(
+            event: Event[bool],
+        ) -> Optional[Callable]:
+            should_lockdown: bool = event.consume_event()
+            if not should_lockdown:
+                return None
+
+            events.api_requests.send_to_lockdown.response.trigger_event(
+                LockdownResponse(lockdown_started=True)
+            )
+            return state_machine.request_lockdown_mission  # type: ignore
+
+        event_handlers: List[EventHandlerMapping] = [
+            EventHandlerMapping(
+                name="start_mission_event",
+                event=events.api_requests.start_mission.request,
+                handler=lambda event: start_mission_event_handler(
+                    state_machine, event, events.api_requests.start_mission.response
+                ),
+            ),
+            EventHandlerMapping(
+                name="return_home_event",
+                event=events.api_requests.return_home.request,
+                handler=lambda event: return_home_event_handler(state_machine, event),
+            ),
+            EventHandlerMapping(
+                name="stop_mission_event",
+                event=events.api_requests.return_home.request,
+                handler=lambda event: stop_mission_event_handler(state_machine, event),
+            ),
+            EventHandlerMapping(
+                name="send_to_lockdown_event",
+                event=events.api_requests.send_to_lockdown.request,
+                handler=_send_to_lockdown_event_handler,
+            ),
+        ]
+
+        timers: List[TimeoutHandlerMapping] = [
+            TimeoutHandlerMapping(
+                name="should_return_home_timer",
+                timeout_in_seconds=settings.RETURN_HOME_DELAY,
+                handler=lambda: state_machine.request_return_home,  # type: ignore
+            )
+        ]
+
+        super().__init__(
+            state_name="await_next_mission",
+            state_machine=state_machine,
+            event_handler_mappings=event_handlers,
+            timers=timers,
+        )
