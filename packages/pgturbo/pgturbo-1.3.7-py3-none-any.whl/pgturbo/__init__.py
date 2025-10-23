10e7f9309a78a26e33a8ac9454dbbf89f2699c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:05:12 +0000

    Add package pgturbo/1.3.7/pgturbo-1.3.7-py3-none-any.whl

---
+++ packages/pgturbo/pgturbo-1.3.7-py3-none-any.whl/pgturbo/__init__.py
@@ -0,0 +1,11 @@
+"""Pygame Turbo, fast moving for of the zero-boilerplate game framework for
+education Pygame Turbo.
+
+You shouldn't need to import things from the 'pgturbo' package directly; just
+use 'pgtrun' to run the game file.
+
+"""
+
+from ._version import __version__
+
+__all__ = ['__version__']
