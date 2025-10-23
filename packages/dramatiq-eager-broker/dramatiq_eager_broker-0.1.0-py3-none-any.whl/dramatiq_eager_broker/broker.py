2d461213e63bbd4c9befe8c48ccc9250a7bcf805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:05:37 +0000

    Add package dramatiq-eager-broker/0.1.0/dramatiq_eager_broker-0.1.0-py3-none-any.whl

---
+++ packages/dramatiq-eager-broker/dramatiq_eager_broker-0.1.0-py3-none-any.whl/dramatiq_eager_broker/broker.py
@@ -0,0 +1,32 @@
+from dramatiq.brokers.stub import StubBroker
+
+
+class EagerBroker(StubBroker):
+    def __init__(self, middleware, *args, **kwargs):
+        super().__init__(middleware)
+
+    def process_message(self, message):
+        actor = self.get_actor(message.actor_name)
+
+        # Adds pipeline support
+        if "pipe_target" in message.options:
+            result = actor(*message.args, **message.kwargs)
+            actor = self.get_actor(message.options["pipe_target"]["actor_name"])
+            if message.options["pipe_target"]["options"].get("pipe_ignore", False):
+                extra_args = tuple()
+            else:
+                extra_args = (result,)
+            actor.send_with_options(
+                args=message.options["pipe_target"]["args"] + extra_args,
+                kwargs=message.options["pipe_target"]["kwargs"],
+                **message.options["pipe_target"]["options"],
+            )
+        else:
+            actor(*message.args, **message.kwargs)
+
+        # Ensure middlewares are executed
+
+        self.emit_after("process_message", message)
+
+    def enqueue(self, message, *, delay=None):
+        self.process_message(message)
