88a8e2710f87118d856bd3ee4d271312ff7ee0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:05:00 +0000

    Add package icotest/0.6.0/icotest-0.6.0-py3-none-any.whl

---
+++ packages/icotest/icotest-0.6.0-py3-none-any.whl/icotest/test/test_stu.py
@@ -0,0 +1,36 @@
+"""Test STU"""
+
+# -- Imports ------------------------------------------------------------------
+
+from icotronic.can import STU
+
+from icotest.config import settings
+from icotest.test.support.node import (
+    check_firmware_upload,
+    check_connection,
+    check_eeprom_product_data,
+    check_eeprom_statistics,
+    check_eeprom_status,
+)
+
+# -- Functions ----------------------------------------------------------------
+
+
+async def test_firmware_upload():
+    """Upload firmware"""
+
+    await check_firmware_upload(settings.stu)
+
+
+async def test_connection(stu: STU):
+    """Test if connection to STU is possible"""
+
+    await check_connection(stu)
+
+
+async def test_eeprom(stu: STU):
+    "Test if reading and writing of EEPROM values works"
+
+    await check_eeprom_product_data(stu, settings.stu)
+    await check_eeprom_statistics(stu, settings.stu)
+    await check_eeprom_status(stu)
