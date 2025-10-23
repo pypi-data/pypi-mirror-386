24e9a3326cbf0256713015a97ac82707e34d2ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:05:09 +0000

    Add package plain.models/0.54.0/plain_models-0.54.0.tar.gz

---
+++ packages/plain.models/plain_models-0.54.0.tar.gz/plain_models-0.54.0/plain/models/config.py
@@ -0,0 +1,16 @@
+from plain.packages import (
+    PackageConfig,
+    packages_registry,
+    register_config,
+)
+
+from .registry import models_registry
+
+
+@register_config
+class Config(PackageConfig):
+    def ready(self) -> None:
+        # Trigger register calls to fire by importing the modules
+        packages_registry.autodiscover_modules("models", include_app=False)
+
+        models_registry.ready = True
