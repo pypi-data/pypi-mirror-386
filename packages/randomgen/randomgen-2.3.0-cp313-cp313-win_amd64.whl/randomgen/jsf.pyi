dc9609876088d533862c15b8914bceb829efb731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:06 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp313-cp313-win_amd64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp313-cp313-win_amd64.whl/randomgen/jsf.pyi
@@ -0,0 +1,20 @@
+from randomgen.common import BitGenerator
+from randomgen.typing import IntegerSequenceSeed, SeedMode
+
+class JSF(BitGenerator):
+    def __init__(
+        self,
+        seed: IntegerSequenceSeed | None = ...,
+        *,
+        seed_size: int = ...,
+        size: int = ...,
+        p: int | None = ...,
+        q: int | None = ...,
+        r: int | None = ...,
+        mode: SeedMode | None = ...,
+    ) -> None: ...
+    def seed(self, seed: IntegerSequenceSeed | None = ...) -> None: ...
+    @property
+    def state(self) -> dict[str, str | int | dict[str, int]]: ...
+    @state.setter
+    def state(self, value: dict[str, str | int | dict[str, int]]) -> None: ...
