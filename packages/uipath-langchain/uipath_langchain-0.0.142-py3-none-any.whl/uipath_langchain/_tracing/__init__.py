3cf490df5e4e02542fa49912d2ec7dba492113a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 17:05:47 +0000

    Add package uipath-langchain/0.0.142/uipath_langchain-0.0.142-py3-none-any.whl

---
+++ packages/uipath-langchain/uipath_langchain-0.0.142-py3-none-any.whl/uipath_langchain/_tracing/__init__.py
@@ -0,0 +1,4 @@
+from ._instrument_traceable import _instrument_traceable_attributes
+from ._oteladapter import LangChainExporter
+
+__all__ = ["LangChainExporter", "_instrument_traceable_attributes"]
