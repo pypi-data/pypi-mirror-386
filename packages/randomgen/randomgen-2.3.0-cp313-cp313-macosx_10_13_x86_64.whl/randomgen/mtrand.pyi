677441cfaad6d8387c8cc735f23ccb356896360a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:02 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp313-cp313-macosx_10_13_x86_64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp313-cp313-macosx_10_13_x86_64.whl/randomgen/mtrand.pyi
@@ -0,0 +1,59 @@
+from typing import Any
+
+from randomgen.common import BitGenerator
+
+class RandomState:
+    def __init__(self, bit_generator: BitGenerator | None = ...) -> None: ...
+
+def _raises_not_implemented(*args: Any, **kwargs: Any) -> None: ...
+
+beta = _raises_not_implemented
+binomial = _raises_not_implemented
+bytes = _raises_not_implemented
+chisquare = _raises_not_implemented
+choice = _raises_not_implemented
+dirichlet = _raises_not_implemented
+exponential = _raises_not_implemented
+f = _raises_not_implemented
+gamma = _raises_not_implemented
+get_state = _raises_not_implemented
+geometric = _raises_not_implemented
+gumbel = _raises_not_implemented
+hypergeometric = _raises_not_implemented
+laplace = _raises_not_implemented
+logistic = _raises_not_implemented
+lognormal = _raises_not_implemented
+logseries = _raises_not_implemented
+multinomial = _raises_not_implemented
+multivariate_normal = _raises_not_implemented
+negative_binomial = _raises_not_implemented
+noncentral_chisquare = _raises_not_implemented
+noncentral_f = _raises_not_implemented
+normal = _raises_not_implemented
+pareto = _raises_not_implemented
+permutation = _raises_not_implemented
+poisson = _raises_not_implemented
+power = _raises_not_implemented
+rand = _raises_not_implemented
+randint = _raises_not_implemented
+randn = _raises_not_implemented
+random = _raises_not_implemented
+random_integers = _raises_not_implemented
+random_sample = _raises_not_implemented
+rayleigh = _raises_not_implemented
+seed = _raises_not_implemented
+set_state = _raises_not_implemented
+shuffle = _raises_not_implemented
+standard_cauchy = _raises_not_implemented
+standard_exponential = _raises_not_implemented
+standard_gamma = _raises_not_implemented
+standard_normal = _raises_not_implemented
+standard_t = _raises_not_implemented
+triangular = _raises_not_implemented
+uniform = _raises_not_implemented
+vonmises = _raises_not_implemented
+wald = _raises_not_implemented
+weibull = _raises_not_implemented
+zipf = _raises_not_implemented
+sample = _raises_not_implemented
+ranf = _raises_not_implemented
