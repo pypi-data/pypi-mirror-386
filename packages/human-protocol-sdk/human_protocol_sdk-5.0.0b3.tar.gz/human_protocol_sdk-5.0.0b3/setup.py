33c07768f2e19b2c85164f131d08b7092120f61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 17:05:47 +0000

    Add package human-protocol-sdk/5.0.0b3/human_protocol_sdk-5.0.0b3.tar.gz

---
+++ packages/human-protocol-sdk/human_protocol_sdk-5.0.0b3.tar.gz/human_protocol_sdk-5.0.0b3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="human-protocol-sdk",
+    version="4.1.3",
+    author="HUMAN Protocol",
+    description="A python library to launch escrow contracts to the HUMAN network.",
+    url="https://github.com/humanprotocol/human-protocol/packages/sdk/python/human-protocol-sdk",
+    include_package_data=True,
+    exclude_package_data={"artifacts": ["*.dbg.json"]},
+    zip_safe=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    packages=setuptools.find_packages() + ["artifacts"],
+    setup_requires="setuptools-pipfile",
+    use_pipfile=True,
+    extras_require={"agreement": ["numpy", "pyerf"]},
+)
