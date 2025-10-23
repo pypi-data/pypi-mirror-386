e05390fb605830b2cc22cbf5b2de3378997cde2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:04:49 +0000

    Add package teleporter/3.1.1/teleporter-3.1.1-py3-none-any.whl

---
+++ packages/teleporter/teleporter-3.1.1-py3-none-any.whl/teleporter/desktop/__init__.py
@@ -0,0 +1,11 @@
+"""
+Desktop is based on opentele
+https://github.com/thedemons/opentele/tree/1a6f0816eac47ff3cb907af72ed9f8cbbbe8fba0/src/td
+"""
+
+from .create_local_key import create_local_key
+from .decrypt import decrypt
+from .file import file
+from .prepare_aes_old_mtp import prepare_aes_old_mtp
+from .read import read
+from .desktop import Desktop
