b17474d965a50cb62b13edc4de056d826e5493ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:59 +0000

    Add package arit/2.5/arit-2.5.tar.gz

---
+++ packages/arit/arit-2.5.tar.gz/arit-2.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='arit',
+  version='2.5',
+  author='Olegs Verhodubs',
+  author_email='oleg.verhodub@inbox.lv',
+  description='This is a prototype package for generating rules from raw text',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  license='Apache-2.0',
+  classifiers=[
+    'Programming Language :: Python :: 3.13',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent'
+  ],
+  python_requires='>=3.13'
+)
