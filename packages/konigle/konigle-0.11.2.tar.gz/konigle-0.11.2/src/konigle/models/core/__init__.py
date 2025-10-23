61ad7a0c3ce686dd73ec5abb9a058abf8a45bb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 15:05:57 +0000

    Add package konigle/0.11.2/konigle-0.11.2.tar.gz

---
+++ packages/konigle/konigle-0.11.2.tar.gz/konigle-0.11.2/src/konigle/models/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Core resource models for the Konigle SDK.
+
+This module exports models for core platform resources like media assets
+and uploads.
+"""
+
+from .media_asset import *  # noqa
+from .upload import *  # noqa
