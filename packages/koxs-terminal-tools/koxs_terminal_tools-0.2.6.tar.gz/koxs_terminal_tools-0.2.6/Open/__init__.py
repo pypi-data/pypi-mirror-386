488a62a7f7ba1353a3409d22a8c37a3434a09dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:05:04 +0000

    Add package koxs-terminal-tools/0.2.6/koxs_terminal_tools-0.2.6.tar.gz

---
+++ packages/koxs-terminal-tools/koxs_terminal_tools-0.2.6.tar.gz/koxs_terminal_tools-0.2.6/Open/__init__.py
@@ -0,0 +1,11 @@
+"""
+koxs文件编辑器 - 一个功能强大的纯Python文件编辑器
+"""
+
+__version__ = "0.1.0"
+__author__ = "koxs"
+__email__ = "koxs@example.com"
+
+from .core import koxsFileEditor
+
+__all__ = ['koxsFileEditor']
