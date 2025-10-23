c5375ab87a8a5e731fbb0e59f12590ed22e98221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 16:05:06 +0000

    Add package torchfont/0.4.0/torchfont-0.4.0-py3-none-any.whl

---
+++ packages/torchfont/torchfont-0.4.0-py3-none-any.whl/torchfont/datasets/__init__.py
@@ -0,0 +1,9 @@
+from torchfont.datasets.folder import FontFolder
+from torchfont.datasets.google_fonts import GoogleFonts
+from torchfont.datasets.repo import FontRepo
+
+__all__ = [
+    "FontFolder",
+    "FontRepo",
+    "GoogleFonts",
+]
---
+++ packages/torchfont/torchfont-0.4.0-py3-none-any.whl/torchfont/transforms/__init__.py
@@ -0,0 +1,11 @@
+from torchfont.transforms.transforms import (
+    Compose,
+    LimitSequenceLength,
+    Patchify,
+)
+
+__all__ = [
+    "Compose",
+    "LimitSequenceLength",
+    "Patchify",
+]
