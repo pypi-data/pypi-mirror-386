e4965f3539ba6eb95b41c66f4c432cadbe7df209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:10 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp39-cp39-macosx_10_9_x86_64.whl/randomgen/common.pyi
@@ -0,0 +1,32 @@
+from typing import Any, NamedTuple
+
+from numpy import ndarray
+
+from randomgen.seed_sequence import SeedSequence
+from randomgen.typing import IntegerSequenceSeed, SeedMode
+
+class Interface(NamedTuple):
+    state_address: int
+    state: int
+    next_uint64: int
+    next_uint32: int
+    next_double: int
+    bit_generator: int
+
+class BitGenerator:
+    seed_seq: SeedSequence
+    def __init__(
+        self,
+        seed: IntegerSequenceSeed = ...,
+        mode: SeedMode | None = ...,
+    ) -> None: ...
+    def random_raw(
+        self,
+        size: int | None = ...,
+        output: bool = ...,
+    ) -> int | ndarray | None: ...
+    def _benchmark(self, cnt: int, method: str = ...) -> None: ...
+    @property
+    def ctypes(self) -> Any: ...
+    @property
+    def cffi(self) -> Any: ...
