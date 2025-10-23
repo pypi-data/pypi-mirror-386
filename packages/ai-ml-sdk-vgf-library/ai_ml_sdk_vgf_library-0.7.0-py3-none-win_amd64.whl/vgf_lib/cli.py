95ee8fd0506649a8e4175084f489922191d4fa61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:05:16 +0000

    Add package ai-ml-sdk-vgf-library/0.7.0/ai_ml_sdk_vgf_library-0.7.0-py3-none-win_amd64.whl

---
+++ packages/ai-ml-sdk-vgf-library/ai_ml_sdk_vgf_library-0.7.0-py3-none-win_amd64.whl/vgf_lib/cli.py
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+import os
+import subprocess
+import sys
+
+
+def main():
+    binary_name = "vgf_dump.exe" if sys.platform.startswith("win") else "vgf_dump"
+    binary_path = os.path.join(os.path.dirname(__file__), "binaries/bin", binary_name)
+    subprocess.run([binary_path] + sys.argv[1:])
