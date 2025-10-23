4976d5034a2b8d271f68fa929d6c731c9bb941a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 06:06:10 +0000

    Add package zendl1/0.6.0/zendl1-0.6.0-py3-none-any.whl

---
+++ packages/zendl1/zendl1-0.6.0-py3-none-any.whl/zendl1/__init__.py
@@ -0,0 +1,14 @@
+from .util5 import get1, get2, get3, get4, get5, get6, requirements, get2_, get7d
+
+__all__ = [
+    "get1",
+    "get2",
+    "get3",
+    "get4",
+    "get5",
+    "get6",
+    "requirements",
+    "get2_",
+    "get7d",
+]
+__version__ = "0.6.0"
