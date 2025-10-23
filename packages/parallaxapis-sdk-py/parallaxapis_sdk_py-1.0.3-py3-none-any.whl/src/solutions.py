65b4d761dc100414789e58f3211e70281607ed0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:41 +0000

    Add package parallaxapis-sdk-py/1.0.3/parallaxapis_sdk_py-1.0.3-py3-none-any.whl

---
+++ packages/parallaxapis-sdk-py/parallaxapis_sdk_py-1.0.3-py3-none-any.whl/src/solutions.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+
+class GenerateUserAgentSolution(BaseModel):
+    UserAgent: str
+    secHeader: str
+    secFullVersionList: str
+    secPlatform: str
+    secArch: str
+
+
+class GenerateDatadomeCookieSolution(BaseModel):
+    cookie: str
+    userAgent: str
+
+
+class GeneratePXCookiesSolution(BaseModel):
+    cookie: str
+    vid: str
+    cts: str
+    isFlagged: bool
+    isMaybeFlagged: bool
+    UserAgent: str
+    data: str
+
+
+class GenerateHoldCaptchaSolution(GeneratePXCookiesSolution, BaseModel):
+    flaggedPOW: bool
