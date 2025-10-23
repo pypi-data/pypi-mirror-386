d030cc8444d5b542761c87035cece28415f38210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:05:15 +0000

    Add package autowordle/2.3.2/autowordle-2.3.2.tar.gz

---
+++ packages/autowordle/autowordle-2.3.2.tar.gz/autowordle-2.3.2/autowordle/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from . import prog
+
+if __name__ == "__main__":
+    prog.top()
---
+++ packages/autowordle/autowordle-2.3.2.tar.gz/autowordle-2.3.2/autowordle/game/__init__.py
@@ -0,0 +1,4 @@
+from .parse import MISSING_GAME, GameParser, ParsedGame
+from .detect import Detector, DetectionType
+
+__all__ = ["MISSING_GAME", "GameParser", "ParsedGame", "Detector", "DetectionType"]
