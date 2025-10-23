2d4d9b9886012cdb3231b0a7b307d4396000c6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:30 +0000

    Add package redblackgraph/0.5.1/redblackgraph-0.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/redblackgraph/redblackgraph-0.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/redblackgraph/__config__.py
@@ -0,0 +1,10 @@
+# This file is generated during the build process
+# It contains configuration information about how the package was built
+
+def show():
+    """Show configuration information."""
+    print("RedBlackGraph configuration:")
+    print("  Python version: 3.10.13 (main, Jan 23 2024, 07:07:05) [GCC 10.2.1 20210130 (Red Hat 10.2.1-11)]")
+    print("  Build system: Meson")
+    
+__all__ = ['show']
