a81dda4ed56799031dac2d5e5dbed448bb51085b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:56 +0000

    Add package clifpy/0.2.7/clifpy-0.2.7-py3-none-any.whl

---
+++ packages/clifpy/clifpy-0.2.7-py3-none-any.whl/clifpy/tables/medication_admin_intermittent.py
@@ -0,0 +1,11 @@
+from .base_table import BaseTable
+
+
+class MedicationAdminIntermittent(BaseTable):
+    """
+    Medication administration intermittent table wrapper inheriting from BaseTable.
+
+    This class handles medication administration intermittent data and validations
+    while leveraging the common functionality provided by BaseTable.
+    """
+    pass
