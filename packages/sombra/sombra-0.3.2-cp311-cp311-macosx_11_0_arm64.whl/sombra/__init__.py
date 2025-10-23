29fda9a798f8abd74517a165c9c3bd03cba96884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:05:18 +0000

    Add package sombra/0.3.2/sombra-0.3.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/sombra/sombra-0.3.2-cp311-cp311-macosx_11_0_arm64.whl/sombra/__init__.py
@@ -0,0 +1,19 @@
+from .sombra import *
+
+__all__ = [
+    "SombraDB",
+    "SombraTransaction",
+    "SombraNode",
+    "SombraEdge",
+    "BfsResult",
+    "DegreeDistribution",
+    "Subgraph",
+    "PropertyBound",
+    "PropertyRangeFilter",
+    "PropertyFilters",
+    "NodePattern",
+    "EdgePattern",
+    "Pattern",
+    "Match",
+    "__version__",
+]
