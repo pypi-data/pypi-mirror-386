7051bbe764fe0239bd239af316f64902114e6a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:48 +0000

    Add package mns-scheduler/1.3.6.6/mns-scheduler-1.3.6.6.tar.gz

---
+++ packages/mns-scheduler/mns-scheduler-1.3.6.6.tar.gz/mns-scheduler-1.3.6.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mns-scheduler',
+    version='1.3.6.6',
+    packages=find_packages(),
+    install_requires=[],  # 如果有依赖项，可以在这里列出
+)
