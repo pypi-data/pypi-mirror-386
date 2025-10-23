b181bdf7d5573d833421bf508520e7ad50cf6285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 03:19:08 +0000

    Add package psyke/0.10.1.dev7/psyke-0.10.1.dev7-py3-none-any.whl

---
+++ packages/psyke/psyke-0.10.1.dev7-py3-none-any.whl/psyke/extraction/hypercubic/gridrex/__init__.py
@@ -0,0 +1,16 @@
+from psyke import get_default_random_seed, Target
+from psyke.extraction.hypercubic import Grid, RegressionCube
+from psyke.extraction.hypercubic.gridex import GridEx
+
+
+class GridREx(GridEx):
+    """
+    Explanator implementing GridREx algorithm.
+    """
+
+    def __init__(self, predictor, grid: Grid, min_examples: int, threshold: float, normalization,
+                 seed=get_default_random_seed()):
+        super().__init__(predictor, grid, min_examples, threshold, Target.REGRESSION, None, normalization, seed)
+
+    def _default_cube(self) -> RegressionCube:
+        return RegressionCube()
