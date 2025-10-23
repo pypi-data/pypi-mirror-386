9469ca9cf3c695d6fd2f14a739dd9c1c6ae54f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:58 +0000

    Add package 3lc/2.18.0/3lc-2.18.0-cp310-cp310-win_amd64.whl

---
+++ packages/3lc/3lc-2.18.0-cp310-cp310-win_amd64.whl/tlc/core/helpers/__init__.pyi
@@ -0,0 +1,4 @@
+from .keypoint_helper import KeypointHelper as KeypointHelper
+from .segmentation_helper import SegmentationHelper as SegmentationHelper
+
+__all__ = ['KeypointHelper', 'SegmentationHelper']
---
+++ packages/3lc/3lc-2.18.0-cp310-cp310-win_amd64.whl/tlcsaas/pyarmor_runtime_005329/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.9 (pro), 005329, 2025-10-22T15:04:56.720831
+from .pyarmor_runtime import __pyarmor__
