3c29fbe69a12385cf4a3659a76689ae90036e203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 15:06:06 +0000

    Add package modusa/0.4.30/modusa-0.4.30-py3-none-any.whl

---
+++ packages/modusa/modusa-0.4.30-py3-none-any.whl/modusa/__init__.py
@@ -0,0 +1,14 @@
+# Audio related
+from modusa.tools import load, play, convert, record
+from modusa.tools import download
+
+# Annotation related
+from modusa.tools import load_ann, save_ann
+
+# Plotting related
+from modusa.tools import dist_plot, hill_plot, plot, fig
+
+# Synthsizing related
+from modusa.tools import synth_f0
+
+__version__ = "0.4.30" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
