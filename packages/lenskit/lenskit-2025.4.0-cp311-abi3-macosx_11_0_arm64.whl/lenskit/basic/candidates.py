6617340461fe9f8ac431431823166229ada2217f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:57 +0000

    Add package lenskit/2025.4.0/lenskit-2025.4.0-cp311-abi3-macosx_11_0_arm64.whl

---
+++ packages/lenskit/lenskit-2025.4.0-cp311-abi3-macosx_11_0_arm64.whl/lenskit/basic/candidates.py
@@ -0,0 +1,79 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import logging
+
+from typing_extensions import override
+
+from lenskit.data import Dataset, ItemList, QueryInput, RecQuery, Vocabulary
+from lenskit.pipeline import Component
+from lenskit.training import Trainable, TrainingOptions
+
+_logger = logging.getLogger(__name__)
+
+
+class TrainingCandidateSelectorBase(Component[ItemList], Trainable):
+    """
+    Base class for candidate selectors using the training data.
+
+    Stability:
+        Caller
+    """
+
+    config: None
+    items_: Vocabulary
+    """
+    List of known items from the training data.
+    """
+
+    @override
+    def train(self, data: Dataset, options: TrainingOptions = TrainingOptions()):
+        if hasattr(self, "items_") and not options.retrain:
+            return
+
+        self.items_ = data.items
+
+
+class AllTrainingItemsCandidateSelector(TrainingCandidateSelectorBase):
+    """
+    Candidate selector that selects all known items from the training data.
+
+    Stability:
+        Caller
+    """
+
+    config: None
+
+    def __call__(self) -> ItemList:
+        return ItemList.from_vocabulary(self.items_)
+
+
+class UnratedTrainingItemsCandidateSelector(TrainingCandidateSelectorBase):
+    """
+    Candidate selector that selects all known items from the training data that
+    do not appear in the request user's history (:attr:`RecQuery.user_items`).
+    If no item history is available, then all training items are returned.
+
+    In order to look up the user's history in the training data, this needs to
+    be combined with a component like
+    :class:`~.history.UserTrainingHistoryLookup`.
+
+    Stability:
+        Caller
+    """
+
+    config: None
+
+    def __call__(self, query: QueryInput) -> ItemList:
+        query = RecQuery.create(query)
+        items = ItemList.from_vocabulary(self.items_)
+
+        if query.user_items is not None:
+            items = items.remove(numbers=query.user_items.numbers(vocabulary=self.items_))
+
+        return items
---
+++ packages/lenskit/lenskit-2025.4.0-cp311-abi3-macosx_11_0_arm64.whl/lenskit/batch/__init__.py
@@ -0,0 +1,103 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+"""
+Batch-run recommendation pipelines for evaluation.
+"""
+
+from __future__ import annotations
+
+from collections.abc import Iterable
+from typing import Literal, Mapping
+
+import pandas as pd
+
+from lenskit.data import ID, GenericKey, ItemList, ItemListCollection, UserIDKey
+from lenskit.pipeline import Pipeline, PipelineProfiler
+
+from ._results import BatchResults
+from ._runner import BatchPipelineRunner, InvocationSpec
+
+__all__ = ["BatchPipelineRunner", "BatchResults", "InvocationSpec", "predict", "recommend"]
+
+
+def predict(
+    pipeline: Pipeline,
+    test: ItemListCollection[GenericKey] | Mapping[ID, ItemList] | pd.DataFrame,
+    *,
+    n_jobs: int | Literal["ray"] | None = None,
+) -> ItemListCollection[GenericKey]:
+    """
+    Convenience function to batch-generate rating predictions (or other per-item
+    scores) from a pipeline.  This is a batch version of :func:`lenskit.predict`.
+
+    .. note::
+
+        If ``test`` is just a sequence of IDs, this method will still work, but
+        it will score _all candidate items_ for each of the IDs.
+
+    Stability:
+        Caller
+    """
+
+    runner = BatchPipelineRunner(n_jobs=n_jobs)
+    runner.predict()
+    outs = runner.run(pipeline, test)
+    return outs.output("predictions")  # type: ignore
+
+
+def score(
+    pipeline: Pipeline,
+    test: ItemListCollection[GenericKey] | Mapping[ID, ItemList] | pd.DataFrame,
+    *,
+    n_jobs: int | Literal["ray"] | None = None,
+) -> ItemListCollection[GenericKey]:
+    """
+    Convenience function to batch-generate personalized scores from a pipeline.
+    This is a batch version of :func:`lenskit.predict`.
+
+    .. note::
+
+        If ``test`` is just a sequence of IDs, this method will still work, but
+        it will score _all candidate items_ for each of the IDs.
+
+    Stability:
+        Caller
+    """
+
+    runner = BatchPipelineRunner(n_jobs=n_jobs)
+    runner.score()
+    outs = runner.run(pipeline, test)
+    return outs.output("scores")  # type: ignore
+
+
+def recommend(
+    pipeline: Pipeline,
+    users: ItemListCollection[GenericKey]
+    | Mapping[ID, ItemList]
+    | Iterable[ID | GenericKey]
+    | pd.DataFrame,
+    n: int | None = None,
+    *,
+    n_jobs: int | Literal["ray"] | None = None,
+    profiler: PipelineProfiler | None = None,
+) -> ItemListCollection[UserIDKey]:
+    """
+    Convenience function to batch-generate recommendations from a pipeline. This
+    is a batch version of :func:`lenskit.recommend`.
+
+    .. todo::
+
+        Support more inputs than just user IDs.
+
+    Stability:
+        Caller
+    """
+
+    runner = BatchPipelineRunner(n_jobs=n_jobs, profiler=profiler)
+    runner.recommend(n=n)
+    outs = runner.run(pipeline, users)
+    return outs.output("recommendations")  # type: ignore
