95d43b95e047a06733bdc91c1cb30a45757e57e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 03:19:47 +0000

    Add package xlsxhelper/0.3.4/xlsxhelper-0.3.4.tar.gz

---
+++ packages/xlsxhelper/xlsxhelper-0.3.4.tar.gz/xlsxhelper-0.3.4/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import (
+    absolute_import,
+    division,
+    generators,
+    nested_scopes,
+    print_function,
+    unicode_literals,
+    with_statement,
+)
+import os
+from io import open
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+with open(os.path.join(here, "requirements.txt"), "r", encoding="utf-8") as fobj:
+    requires = [x.strip() for x in fobj.readlines() if x.strip()]
+
+
+setup(
+    name="xlsxhelper",
+    version="0.3.4",
+    description="Excel file manipulation toolset.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="rRR0VrFP",
+    maintainer="rRR0VrFP",
+    python_requires=">=2.7, !=3.2.*, !=3.3.*",
+    license="MIT",
+    license_files=("LICENSE",),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords=["excel utils"],
+    install_requires=requires,
+    packages=find_packages(".", exclude=["tests"]),
+    py_modules=["xlsxhelper"],
+)
