9f809d74d67d33de9a26a9daa4a09d9df91598f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:14 +0000

    Add package adipylib/0.0.1/adipylib-0.0.1.tar.gz

---
+++ packages/adipylib/adipylib-0.0.1.tar.gz/adipylib-0.0.1/adipylib/__init__.py
@@ -0,0 +1,10 @@
+# adipylib/__init__.py
+
+# Import functions from your .py files to make them
+# available when someone imports your package.
+
+from .core import hello_adipylib, add_numbers
+
+# You can also define the package version here,
+# though putting it in pyproject.toml is more modern.
+__version__ = "0.0.1"
