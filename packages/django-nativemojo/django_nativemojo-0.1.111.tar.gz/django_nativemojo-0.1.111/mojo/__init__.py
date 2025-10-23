401b1494b826d8c9c452d83a9b5bd4120c88a989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 06:05:14 +0000

    Add package django-nativemojo/0.1.111/django_nativemojo-0.1.111.tar.gz

---
+++ packages/django-nativemojo/django_nativemojo-0.1.111.tar.gz/django_nativemojo-0.1.111/mojo/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.111"
+
+from mojo.helpers.response import JsonResponse
---
+++ packages/django-nativemojo/django_nativemojo-0.1.111.tar.gz/django_nativemojo-0.1.111/mojo/apps/account/rest/__init__.py
@@ -0,0 +1,5 @@
+APP_NAME = ""
+from .user import *
+from .group import *
+from .device import *
+from .push import *
