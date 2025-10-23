3900007f7cb8bf7486ea32ae625af0df61ddd008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:35 +0000

    Add package phxutils/1.1.64/phxutils-1.1.64.tar.gz

---
+++ packages/phxutils/phxutils-1.1.64.tar.gz/phxutils-1.1.64/setup.py
@@ -0,0 +1,26 @@
+import glob
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+binfiles = glob.glob('bin/*')
+
+setuptools.setup(
+    name='phxutils',
+    version='1.1.64',
+    scripts = binfiles,
+    author='phx',
+    author_email='phx@example.com',
+    description='various useful shell utilities',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/phx/phxutils',
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
