666e3d9012b0a8b0871f71b5bc255146d91eaf8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 08:05:40 +0000

    Add package dm-client-auth/1.0.1/dm_client_auth-1.0.1-py3-none-any.whl

---
+++ packages/dm-client-auth/dm_client_auth-1.0.1-py3-none-any.whl/dm_client/auth/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DmClientAuthAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dm_client.auth'
+    label = 'dm_client_auth'
