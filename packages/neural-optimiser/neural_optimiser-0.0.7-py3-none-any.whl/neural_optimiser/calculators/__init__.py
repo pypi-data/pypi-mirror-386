8364a73375644f9558166ca3127374291a383f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:45 +0000

    Add package neural-optimiser/0.0.7/neural_optimiser-0.0.7-py3-none-any.whl

---
+++ packages/neural-optimiser/neural_optimiser-0.0.7-py3-none-any.whl/neural_optimiser/calculators/__init__.py
@@ -0,0 +1,5 @@
+from ._fairchem import FAIRChemCalculator
+from ._mace import MACECalculator
+from ._rand import RandomCalculator
+
+__all__ = ["MACECalculator", "FAIRChemCalculator", "RandomCalculator"]
