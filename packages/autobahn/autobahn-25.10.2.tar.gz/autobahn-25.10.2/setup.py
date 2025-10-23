f44c212eacadc147a01f9c7f455e420df010460d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:54 +0000

    Add package autobahn/25.10.2/autobahn-25.10.2.tar.gz

---
+++ packages/autobahn/autobahn-25.10.2.tar.gz/autobahn-25.10.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Modern setup.py that works with pyproject.toml for CFFI module building.
+This handles the conditional CFFI module compilation based on environment variables.
+"""
+
+import os
+from setuptools import setup
+
+# cffi based extension modules to build, currently only NVX
+cffi_modules = []
+if "AUTOBAHN_USE_NVX" not in os.environ or os.environ["AUTOBAHN_USE_NVX"] not in [
+    "0",
+    "false",
+]:
+    cffi_modules.append("autobahn/nvx/_utf8validator.py:ffi")
+    cffi_modules.append("autobahn/nvx/_xormasker.py:ffi")
+
+# Include package data from MANIFEST.in
+include_package_data = True
+
+# Use setuptools with pyproject.toml for most configuration
+setup(
+    cffi_modules=cffi_modules,
+    include_package_data=include_package_data,
+)
