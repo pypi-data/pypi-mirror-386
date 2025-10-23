33cf91585f4b1b47653f2dc988a1be396ea812a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 08:05:41 +0000

    Add package dm-client-document/1.0.1/dm_client_document-1.0.1-py3-none-any.whl

---
+++ packages/dm-client-document/dm_client_document-1.0.1-py3-none-any.whl/dm_client/document/service.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from dm_core.meta.decorator import api_outbound_validator
+from dm_core.meta.service import MetaClient
+from dm_core.redis.utils import singleton
+from dm_core.tracer.service import Requests, RequestAuthEnum
+from requests.models import Response, Request
+
+
+@singleton
+class DocumentClient(object):
+
+    def __init__(self):
+        meta_client = MetaClient()
+        self.service = settings.SERVICE
+        self.target_service = 'document'
+        self.url = meta_client.service_info(self.target_service, cache_key=self.target_service)['url']
+        self.private_key = meta_client.my_service_info()['private_key']
+
+    @api_outbound_validator()
+    def _api(self, service, id, method, url_path, *args, **kwargs):
+        response, request = Requests(service, id, self.private_key)(method, '{}{}'.format(self.url, url_path), *args, **kwargs)
+        return response, request
+
+    ############### Version ############################
+
+    def version(self) -> (Response, Request):
+        return self._api(self.target_service, 'version')
+
+
+    ################ Core ##############################
+
+    def core_pdf(self, template_name):
+        return self._api(self.target_service, 'core.pdf', template_name)
+
+    ################## SCHEDULE ######################
+    def schedule_internal_cleanup(self) -> (Response, Request):
+        return self._api(self.target_service, 'schedule.internal.cleanup', request_auth=RequestAuthEnum.INTERNAL)
