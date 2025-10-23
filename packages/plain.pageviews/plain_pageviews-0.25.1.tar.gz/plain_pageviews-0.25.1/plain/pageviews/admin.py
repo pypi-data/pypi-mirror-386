19503ff1f3d328a6e60eb23d13e40e16cccc4f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:05:09 +0000

    Add package plain.pageviews/0.25.1/plain_pageviews-0.25.1.tar.gz

---
+++ packages/plain.pageviews/plain_pageviews-0.25.1.tar.gz/plain_pageviews-0.25.1/plain/pageviews/admin.py
@@ -0,0 +1,45 @@
+from plain.admin.cards import Card, TrendCard
+from plain.admin.views import (
+    AdminModelDetailView,
+    AdminModelListView,
+    AdminViewset,
+    register_viewset,
+)
+
+from .models import Pageview
+
+
+class PageviewsTrendCard(TrendCard):
+    title = "Pageviews trend"
+    model = Pageview
+    datetime_field = "timestamp"
+    size = TrendCard.Sizes.FULL
+
+
+@register_viewset
+class PageviewAdmin(AdminViewset):
+    class ListView(AdminModelListView):
+        model = Pageview
+        nav_section = "Pageviews"
+        nav_icon = "eye"
+        title = "Pageviews"
+        fields = ["user_id", "url", "source", "timestamp", "session_id"]
+        search_fields = ["id", "user_id", "url", "session_id", "source", "medium"]
+        cards = [PageviewsTrendCard]
+
+    class DetailView(AdminModelDetailView):
+        model = Pageview
+
+
+class UserPageviewsCard(Card):
+    title = "Recent pageviews"
+    template_name = "pageviews/card.html"
+
+    def get_template_context(self) -> dict:
+        context = super().get_template_context()
+
+        context["pageviews"] = Pageview.query.filter(
+            user_id=self.view.object.id
+        ).order_by("-timestamp")[:50]
+
+        return context
