6feda179500e523a47eba83d487e8f342b762a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 15:06:22 +0000

    Add package sphinxnotes-incrbuild/1.1/sphinxnotes_incrbuild-1.1-py3-none-any.whl

---
+++ packages/sphinxnotes-incrbuild/sphinxnotes_incrbuild-1.1-py3-none-any.whl/sphinxnotes/incrbuild/sphinxapi.py
@@ -0,0 +1,46 @@
+"""
+sphinxnotes.incrbuild.main
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Sphinx-related code.
+
+:copyright: Copyright 2025 Shengyu Zhang
+:license: BSD, see LICENSE for details.
+"""
+
+import argparse
+from pathlib import Path
+
+# NOTE: There aren't public APIs, but there aren't many better options
+from sphinx.config import eval_config_file
+from sphinx.errors import ConfigError
+from sphinx.cmd.build import get_parser, main
+from sphinx.util.tags import Tags
+
+from .utils import error
+
+
+def get_html_theme(conf_dir: Path) -> str:
+    DEFAULT_HTML_THEME = 'alabaster'
+    try:
+        cfg = eval_config_file(conf_dir.joinpath('conf.py'), Tags())
+    except ConfigError as e:
+        error(f'Failed to get html theme from config file {conf_dir}: {e}')
+        return DEFAULT_HTML_THEME
+    else:
+        return cfg.get('html_theme', DEFAULT_HTML_THEME)
+
+
+def get_build_parser(injection):
+    parser = get_parser()
+    parser.add_argument(  # fix the hidden -M flag.
+        '-M',
+        dest='use_make_mode',
+        help=argparse.SUPPRESS,
+    )
+    injection(parser)
+    return parser
+
+
+def run(args: list[str]) -> int:
+    return main(args)
