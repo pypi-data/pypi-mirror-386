178abcb8c24a458629a1eb2b53f5a6ff298d8192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 17:05:56 +0000

    Add package C2Percentiles/0.1.1/c2percentiles-0.1.1-py3-none-any.whl

---
+++ packages/C2Percentiles/c2percentiles-0.1.1-py3-none-any.whl/C2Percentiles/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# C2Percentiles/__init__.py
+from .percentiles import (
+    quantile_custom,
+    quantiles_for_vars,
+    quantiles_by_group,
+    plot_quantile_line,
+    plot_qfv,
+    plot_qbg
+)
+
+__all__ = [
+    "quantile_custom",
+    "quantiles_for_vars",
+    "quantiles_by_group",
+    "plot_quantile_line",
+    "plot_qfv",
+    "plot_qbg"
+]
+
