0c0596a1fe59f533b2bba891ce7b5690c94a196b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:05:17 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/randomgen/tests/test_final_release_changes.py
@@ -0,0 +1,43 @@
+import pytest
+
+from randomgen import (
+    DSFMT,
+    MT19937,
+    PCG32,
+    PCG64,
+    Philox,
+    ThreeFry,
+    Xoroshiro128,
+    Xorshift1024,
+    Xoshiro256,
+    Xoshiro512,
+)
+
+bit_generators = [
+    DSFMT,
+    MT19937,
+    PCG32,
+    PCG64,
+    Philox,
+    ThreeFry,
+    Xoroshiro128,
+    Xorshift1024,
+    Xoshiro256,
+    Xoshiro512,
+]
+
+
+@pytest.fixture(scope="module", params=bit_generators)
+def bit_generator(request):
+    return request.param
+
+
+@pytest.fixture(scope="module", params=[True, False])
+def endpoint(request):
+    return request.param
+
+
+def test_generator_raises(bit_generator):
+    bg = bit_generator()
+    with pytest.raises(NotImplementedError):
+        _ = bg.generator
