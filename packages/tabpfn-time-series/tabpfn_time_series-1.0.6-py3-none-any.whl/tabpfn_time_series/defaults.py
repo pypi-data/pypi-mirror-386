98b83d1f2d19b9af70883425d0a6a0dfe3b6ca2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:04:51 +0000

    Add package tabpfn-time-series/1.0.6/tabpfn_time_series-1.0.6-py3-none-any.whl

---
+++ packages/tabpfn-time-series/tabpfn_time_series-1.0.6-py3-none-any.whl/tabpfn_time_series/defaults.py
@@ -0,0 +1,5 @@
+TABPFN_TS_DEFAULT_QUANTILE_CONFIG = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+TABPFN_TS_DEFAULT_CONFIG = {
+    "tabpfn_internal": {"model_path": "tabpfn-v2-regressor-2noar4o2.ckpt"},
+    "tabpfn_output_selection": "median",  # mean or median
+}
