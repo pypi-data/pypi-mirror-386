50742f6c42f7504ceb5c70764b894a5f140ebf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 06:05:06 +0000

    Add package ipp-crypto-include/2025.3.0/ipp_crypto_include-2025.3.0-py2.py3-none-win_amd64.whl

---
+++ packages/ipp-crypto-include/ipp_crypto_include-2025.3.0-py2.py3-none-win_amd64.whl/ipp_crypto_include-2025.3.0.data/data/include/aggregator/ippcp.h
@@ -0,0 +1,32 @@
+/*************************************************************************
+* Copyright (C) 2023 Intel Corporation
+*
+* Licensed under the Apache License,  Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* 	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law  or agreed  to  in  writing,  software
+* distributed under  the License  is  distributed  on  an  "AS IS"  BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the  specific  language  governing  permissions  and
+* limitations under the License.
+*************************************************************************/
+
+/*
+//
+//   Intel® Integrated Performance Primitives Cryptography (Intel® IPP Cryptography)
+//
+//   Purpose: 
+//      It's an aggregated header to save compile-time backward compatibility with the versions of 
+//      Intel® oneAPI Base Toolkit (Base Kit) less than 2024.0
+//
+*/
+
+#if !defined( AGGREGATED_IPPCP_H__ )
+#define AGGREGATED_IPPCP_H__
+
+#include "ippcp/ippcp.h"
+
+#endif /* AGGREGATED_IPPCP_H__ */
