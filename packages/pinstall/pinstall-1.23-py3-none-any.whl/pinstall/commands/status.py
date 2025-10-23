9ec02090c99f0148c3bb060000cdba76fb177e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 15:06:26 +0000

    Add package pinstall/1.23/pinstall-1.23-py3-none-any.whl

---
+++ packages/pinstall/pinstall-1.23-py3-none-any.whl/pinstall/commands/status.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Reports systemctl status of services and timers installed from the
+current directory.
+"""
+
+from __future__ import annotations
+
+import sys
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+from ..run import run
+
+
+def init(parser: ArgumentParser) -> None:
+    "Called to add this command's arguments to parser at init"
+    parser.add_argument(
+        '-u', '--user', action='store_true', help='report for user service'
+    )
+    parser.add_argument('units', nargs='*', help='systemd service file[s]')
+
+
+def main(args: Namespace) -> str | None:
+    "Called to action this command"
+    units = (
+        [Path(p) for p in args.units]
+        if args.units
+        else list(Path.cwd().glob('*.service'))
+    )
+
+    if not units:
+        return 'There are no .service files in this directory'
+
+    cmd = 'systemctl --user status' if args.user else 'systemctl status'
+
+    # Iterate over all specified service files ..
+    for unit in units:
+        if not unit.suffix.lower() == '.service':
+            unit = unit.with_suffix('.service')
+
+        if not unit.exists():
+            print(f'### {unit} does not exist', file=sys.stderr)
+            continue
+
+        for ext in ('.timer', '.socket'):
+            other = unit.with_suffix(ext)
+            if other.exists():
+                run(f'{cmd} {other.name}')
+
+        run(f'{cmd} {unit.name}')
+
+    return None
---
+++ packages/pinstall/pinstall-1.23-py3-none-any.whl/pinstall/run.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"Common module to run given command"
+
+from __future__ import annotations
+
+import subprocess
+import sys
+
+
+def run(cmd: str, *, capture: bool = False, ignore_error: bool = False) -> str | None:
+    "Run given command string"
+    if ignore_error:
+        cmd += ' 2>/dev/null'
+
+    if capture:
+        stdout = subprocess.PIPE
+    else:
+        stdout = None
+        print(f'>>> Running {cmd}')
+
+    try:
+        res = subprocess.run(cmd, shell=True, stdout=stdout, text=True)
+    except Exception as e:
+        if not capture and not ignore_error:
+            sys.exit(str(e))
+        return None
+
+    if res.returncode != 0:
+        if not capture and not ignore_error:
+            sys.exit(res.returncode)
+        return None
+
+    return res.stdout and res.stdout.strip()
