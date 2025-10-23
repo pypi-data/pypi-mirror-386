5170d1fc596dbb6f8ee589f215e41c400a313e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:04:55 +0000

    Add package ordeq/1.2.1/ordeq-1.2.1-py3-none-any.whl

---
+++ packages/ordeq/ordeq-1.2.1-py3-none-any.whl/ordeq/__init__.py
@@ -0,0 +1,20 @@
+from ordeq._catalog import check_catalogs_are_consistent
+from ordeq._hook import InputHook, NodeHook, OutputHook, RunHook
+from ordeq._io import IO, Input, IOException, Output
+from ordeq._nodes import Node, node
+from ordeq._runner import run
+
+__all__ = (
+    "IO",
+    "IOException",
+    "Input",
+    "InputHook",
+    "Node",
+    "NodeHook",
+    "Output",
+    "OutputHook",
+    "RunHook",
+    "check_catalogs_are_consistent",
+    "node",
+    "run",
+)
