9a3641850cf770fe1a32639ce11572d466ebca50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 06:05:53 +0000

    Add package elveirdor/2.1.0/elveirdor-2.1.0.tar.gz

---
+++ packages/elveirdor/elveirdor-2.1.0.tar.gz/elveirdor-2.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="elveirdor",
+    version="2.1.0",
+    author="Elveirdor Systems",
+    description="Elveirdor Infinity Computing Pipeline with visual generation, decoding, and export features.",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pillow",
+        "matplotlib",
+    ],
+    entry_points={
+        "console_scripts": [
+            "elveirdor=elveirdor.cli:main",
+        ],
+    },
+    python_requires=">=3.8",
+)
