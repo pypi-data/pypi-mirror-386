f659b72bbd9925989f29c2c8d6278c2b7534daf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 06:06:11 +0000

    Add package zendl1/0.6.0/zendl1-0.6.0.tar.gz

---
+++ packages/zendl1/zendl1-0.6.0.tar.gz/zendl1-0.6.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zendl1",
+    version="0.6.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="zenitsu",
+    python_requires=">=3.6",
+)
