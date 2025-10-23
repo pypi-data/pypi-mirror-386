acd467b76ef1429319d4ab9c81b020068ee67f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 16:05:11 +0000

    Add package fashion-mnist-classifier/0.1.10/fashion_mnist_classifier-0.1.10.tar.gz

---
+++ packages/fashion-mnist-classifier/fashion_mnist_classifier-0.1.10.tar.gz/fashion_mnist_classifier-0.1.10/src/my_project/config.py
@@ -0,0 +1,13 @@
+"""
+Configuration constants for the Fashion-MNIST project.
+
+This file can be used for global constants that are shared across different
+modules of the project.
+
+If we run the gradio app, this parameters are not needed. 
+For training via terminal, this config can be edited to chose the desiered parameters..
+"""
+# Experimental params
+BATCH_SIZE = 128
+NUM_WORKERS = 4
+MAX_EPOCHS = 5
