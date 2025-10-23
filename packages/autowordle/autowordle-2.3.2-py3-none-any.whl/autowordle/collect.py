e9f70991fdd291fc1c853c620d13aea513179cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 04:05:15 +0000

    Add package autowordle/2.3.2/autowordle-2.3.2-py3-none-any.whl

---
+++ packages/autowordle/autowordle-2.3.2-py3-none-any.whl/autowordle/collect.py
@@ -0,0 +1,194 @@
+from enum import Enum
+import re
+import io
+from PIL import Image
+from datetime import datetime
+import subprocess
+import logging
+import tempfile
+
+from .res import Audio
+from .game import Detector, DetectionType
+
+logger = logging.getLogger(__name__)
+
+class GameAccepts(Enum):
+    NONE = "none"
+    TEXT = "text"
+    IMAGE = "image"
+
+class GameStatus(Enum):
+    OPTIONAL = 'optional'
+    PARTIALLY_COLLECTED = 'partially_collected'
+    COLLECTED = 'collected'
+    UNCOLLECTED = 'uncollected'
+    UNELIGIBLE = 'uneligible'
+    def as_str(self):
+        if self == GameStatus.OPTIONAL:
+            return 'Optional'
+        elif self == GameStatus.PARTIALLY_COLLECTED:
+            return 'Awaiting text'
+        elif self == GameStatus.COLLECTED:
+            return 'Collected'
+        elif self == GameStatus.UNCOLLECTED:
+            return 'Uncollected'
+        elif self == GameStatus.UNELIGIBLE:
+            return 'Ineligible'
+
+class Collector:
+    def __init__(self, spec, db, date):
+        self.date = date.strftime("%Y-%m-%d")
+        self.gui = None
+        self.db = db
+        self.detector = Detector(spec)
+        self.name = spec.name
+        self.display_row = spec.display_row
+        self.refresh_hour = spec.refresh_hour
+        self.short_name = self.detector.short_name
+        self.out_filename = f'{self.detector.short_name}-{self.date}'
+        self.result = self.db.load(self.date, self.short_name)
+
+    def log(self, text):
+        if self.gui is not None:
+            self.gui.new_status(text)
+        else:
+            logger.info(text)
+
+    def __repr__(self):
+        return "<{class_name} {game_name}{collectedp}>".format(
+            class_name=type(self).__qualname__,
+            game_name=str(self.name),
+            collectedp=" collected" if self.collectedp() else "",
+        )
+
+    def eligiblep(self):
+        return self.refresh_hour <= datetime.now().hour
+
+    def collectedp(self):
+        if self.detector.detection == DetectionType.TEXT:
+            return self.result.text is not None
+        elif self.detector.detection == DetectionType.IMAGE:
+            return self.result.text is not None and self.result.image is not None
+
+    def status(self):
+        has_image_but_no_text = self.result.text is None and self.result.image is not None
+        if self.collectedp():
+            return GameStatus.COLLECTED
+        elif not self.eligiblep():
+            return GameStatus.UNELIGIBLE
+        elif has_image_but_no_text:
+            return GameStatus.PARTIALLY_COLLECTED
+        else:
+            return GameStatus.UNCOLLECTED
+        # else:
+        #     return GameStatus.OPTIONAL
+
+    def status_str(self):
+        if self.result.text is not None:
+            return self.result.text.splitlines()[self.display_row]
+        else:
+            return self.status().as_str()
+
+    def save_to_db(self, thing):
+        if isinstance(thing, str):
+            r = self.db.save(self.date, self.short_name,
+                         "\n".join(line for line in thing.splitlines()
+                                   if not line.startswith("http")))
+        elif isinstance(thing, bytes): # image
+            r = self.db.save(self.date, self.short_name, thing)
+        else:
+            raise ValueError(f"Don't know what to do with {thing} on saving")
+        self.result = r
+        return r
+
+    def wants(self) -> GameAccepts:
+        match (self.status(), self.detector.detection):
+            case (GameStatus.COLLECTED | GameStatus.UNELIGIBLE, _):
+                return GameAccepts.NONE
+            case ((GameStatus.UNCOLLECTED, DetectionType.TEXT)
+                  | (GameStatus.PARTIALLY_COLLECTED, DetectionType.IMAGE)):
+                return GameAccepts.TEXT
+            case (GameStatus.UNCOLLECTED, DetectionType.IMAGE):
+                return GameAccepts.IMAGE
+            case _:
+                logger.error('Unreachable want!')
+                raise RuntimeError(f'{self} has an unreachable want')
+
+    def collect_if_match(self, thing):
+        if self.wants() == GameAccepts.NONE:
+            return None
+        elif (self.wants() == GameAccepts.TEXT
+              and isinstance(thing, str)
+              and self.detector.matches(thing)):
+            self.result = self.save_to_db(thing)
+            self.log(f'Collected text of {self.name}')
+            Audio.COMPLETE.play()
+        elif (self.wants() == GameAccepts.IMAGE
+              and isinstance(thing, Image.Image)
+              and self.detector.matches(thing)):
+            self.result = self.save_to_db(compress_image(thing))
+            self.log(f'Collected image of {self.name}')
+            Audio.READY_FOR_TEXT.play()
+        else:
+            return None
+        return self
+
+def compress_image(image):
+    with tempfile.NamedTemporaryFile(prefix="autowordle_", suffix='.png', mode='wb',
+                                     delete_on_close=False) as tmpfile:
+        image.save(tmpfile)
+        with subprocess.Popen(['pngcrush', tmpfile.name, '/dev/stdout'],
+                              stdout=subprocess.PIPE) as proc:
+            return proc.stdout.read()
+
+class CollectionStatus(Enum):
+    INCOMPLETE = 'incomplete'
+    COMPLETE_FOR_NOW = 'complete-for-now'
+    COMPLETE = 'complete'
+
+class GameCollection:
+    def __init__(self, collection, db, date):
+        self.collection = [Collector(i, db, date) for i in collection]
+
+    def __repr__(self):
+        return f"<{type(self).__qualname__}, {len(self.collection)} entries>"
+
+    def inject_gui(self, gui):
+        for i in self.collection:
+            i.gui = gui
+
+    def eligible(self):
+        return [x for x in self.collection if x.eligiblep()]
+
+    def status(self):
+        if all(x.collectedp() for x in self.collection):
+            return CollectionStatus.COMPLETE
+        elif all(x.collectedp() for x in self.eligible()):
+            return CollectionStatus.COMPLETE_FOR_NOW
+        else:
+            return CollectionStatus.INCOMPLETE
+
+    def get(self, game_name):
+        return next(x for x in self.collection if x.name == game_name)
+
+    def find_matching_game(self, thing):
+        for i in self.eligible():
+            if res := i.collect_if_match(thing):
+                return res
+        return None
+
+def clipboard():
+    if re.search(
+            "image/",
+            subprocess.run(
+                ["xclip", "-sel", "c", "-target", "TARGETS", "-out"],
+                capture_output=True).stdout.decode("UTF-8")):
+        return Image.open(
+            io.BytesIO(
+                subprocess.run(
+                    ["xclip", "-sel", "c", "-target", "image/png", "-out"],
+                    capture_output=True).stdout))
+    else:
+        return subprocess.run(
+            ["xclip", "-sel", "c", "-out"],
+            capture_output=True).stdout.decode("UTF-8")
---
+++ packages/autowordle/autowordle-2.3.2-py3-none-any.whl/autowordle/collect_tk.py
@@ -0,0 +1,211 @@
+from tkinter import PhotoImage, NORMAL, END, DISABLED, Text, HORIZONTAL, ttk
+from ttkthemes import ThemedTk
+from importlib.metadata import version
+import threading
+import subprocess
+import logging
+
+from .collect import GameStatus, CollectionStatus, clipboard
+from .res import file_res
+
+logger = logging.getLogger(__name__)
+
+class TetsudoruCommands:
+    def __init__(self, gui=None):
+        self.gui = gui
+
+    strings = [
+        "tetsudoru -1 | grep -v '[きぎただあさざかが]'",
+        "tetsudoru -1 | grep -v '[きぎただあさざかがつっづしじんまちぢ]'",
+        "tetsudoru -1 | grep -v '[きぎただあさざかがつっづしじんまちぢいよひびぴらの]'",
+    ]
+    def to_clipboard(self, idx):
+        subprocess.run(['xclip', '-sel', 'c'], input=self.strings[idx - 1], text=True)
+        if self.gui is not None:
+            self.gui.new_status(f"Sent search string for Tetsudoru ({idx} guesses)")
+
+class ClipwaitThread:
+    event_name = "<<NewClipboard>>"
+    finish_event_name = "<<CollectionComplete>>"
+    def __init__(self, gui, *, daemon=False):
+        self.gui = gui
+        self.col = gui.col
+        self.daemon = daemon
+        gui.root.bind(self.event_name, lambda e: self.hook())
+    def _exec(self):
+        subprocess.run(["clipnotify"])
+        self.gui.root.event_generate(self.event_name)
+    def start(self):
+        threading.Thread(target=self._exec, daemon=self.daemon).start()
+        logger.debug('New clip wait thread spawned')
+    def hook(self):
+        dat = clipboard()
+        # self.gui.update_clipboard(dat)
+        if matchp := self.col.find_matching_game(dat):
+            self.gui.update_games_view(matchp)
+        match self.col.status():
+            case CollectionStatus.INCOMPLETE:
+                self.start()
+            case CollectionStatus.COMPLETE_FOR_NOW:
+                self.gui.new_status('Collection complete; no longer listening on clipboard.')
+            case CollectionStatus.COMPLETE:
+                self.gui.root.event_generate(self.finish_event_name)
+
+class Worker:
+    event_name = None
+    def __init__(self, config):
+        self.config = config
+        self.gui = None
+    def inject_gui(self, gui, kickoff_event):
+        self.gui = gui
+        gui.root.bind(kickoff_event, lambda e: self.start(), add='+')
+        gui.root.bind(self.event_name, lambda e: self.hook())
+    def exec(self):
+        pass
+    def _exec(self):
+        self.exec()
+        if self.gui is not None:
+            self.gui.root.event_generate(self.event_name)
+    def start(self):
+        self.thread = threading.Thread(target=self._exec)
+        self.thread.start()
+    def hook(self):
+        pass
+
+class GUI:
+    _icons = {
+        GameStatus.COLLECTED: 'emblem-default-symbolic.png',
+        GameStatus.UNCOLLECTED: 'emblem-error.png',
+        GameStatus.UNELIGIBLE: 'emblem-readonly.png',
+        GameStatus.PARTIALLY_COLLECTED: 'emblem-remove.png',
+        GameStatus.OPTIONAL: 'emblem-question.png',
+    }
+
+    def __init__(self, config):
+        self.col = config.collection
+        self.root = ThemedTk(theme='breeze', className=config.program_name.lower())
+        self.workers = list()
+        self.date = config.date
+        self.complete = False
+        self.tetsudoru = TetsudoruCommands(self)
+        self.root.title(f'{config.program_name}: {config.date:%Y-%m-%d}')
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        for (k, v) in self._icons.items():
+            self._icons[k] = PhotoImage(file=file_res(v), format='png')
+        self.clipwait_thread = ClipwaitThread(self, daemon=True)
+        self.col.inject_gui(self)
+
+    def _title(self):
+        return "Version {}; collecting for {:%Y-%m-%d}".format(
+            version("autowordle"), self.date)
+
+    def add_worker(self, worker, kickoff_event):
+        worker.inject_gui(self, kickoff_event)
+        self.workers.append(worker)
+        logger.info(f'Added worker {worker} on event {kickoff_event}')
+
+    def update_clipboard(self, new_text):
+        self.clipboard_display.config(state=NORMAL)
+        self.clipboard_display.delete('1.0', END)
+        if isinstance(new_text, str):
+            self.clipboard_display.insert(END, new_text)
+        else:
+            self.clipboard_display.insert(END, '<Non-text data>')
+        self.clipboard_display.config(state=DISABLED)
+
+    def update_games_view(self, game):
+        new_status = game.status()
+        self.games_view.item(
+            game.short_name,
+            values=(game.name, game.status_str()),
+            image=self._icons[new_status])
+        if new_status == GameStatus.COLLECTED:
+            self.progress_bar.step(1)
+
+    def new_status(self, status_text):
+        self.status.config(text=status_text)
+        logger.info(status_text)
+
+    def _add_buttons(self, frm):
+        buttons = ttk.Frame(frm, padding=10)
+        buttons.grid(row=3, column=1, columnspan=2)
+        buttons.grid()
+        for i, _ in enumerate(self.tetsudoru.strings):
+            plural = "es" if 0 < i else ""
+            ttk.Button(
+                buttons,
+                text=f"Tetsudoru ({i + 1} guess{plural})",
+                command=(lambda i: lambda: self.tetsudoru.to_clipboard(i + 1))(i)
+            ).grid(column=i, row=1)
+            quit_button = ttk.Button(buttons, text="Quit", command=self.root.destroy)
+            quit_button .grid(column=0, row=2, columnspan=3, sticky='nesw')
+            self.quit_button = ttk.Button(
+                buttons,
+                text="Clean up and finish",
+                state=DISABLED)
+            self.quit_button.grid(column=0, row=3, columnspan=3, sticky='nesw')
+    def _add_clipboard_frame(self, frm):
+        clipboard_frame = ttk.Frame(frm, borderwidth=1, width=400, padding=10)
+        clipboard_frame.grid(column=0, row=0, rowspan=4, sticky='nesw')
+        clipboard_frame.grid()
+        ttk.Label(clipboard_frame, text="Clipboard", anchor="w",).grid(column=0, row=0)
+        self.clipboard_display = Text(clipboard_frame, width=50, height=30)
+        self.clipboard_display.config(state=DISABLED)
+        self.clipboard_display.grid(column=0, row=1, sticky='nesw')
+    def _add_progress_bar(self, frm):
+        ttk.Label(frm, text='Collection progress', width=1).grid(row=2, column=1, sticky='ew')
+        self.progress_bar = ttk.Progressbar(
+            frm,
+            maximum=sum(1 for i in self.col.collection if i.eligiblep())
+        )
+        self.progress_bar.grid(row=2, column=2, sticky='nesw')
+        self.progress_bar.step(sum(1 for i in self.col.collection
+                           if i.status() == GameStatus.COLLECTED))
+    def _add_games_view(self, frm):
+        self.games_view = ttk.Treeview(
+            frm,
+            padding=10,
+            columns=('name', 'status'),
+            height=len(self.col.collection))
+        self.games_view.column('#0', width=50)
+        self.games_view.heading('name', text='Name')
+        self.games_view.column('name', width=150)
+        self.games_view.heading('status', text='Status')
+        self.games_view.column('status', width=300)
+        self.games_view.grid(column=1, row=1, columnspan=2)
+        for game in self.col.collection:
+            self.games_view.insert(
+                '', 'end', game.short_name,
+                values=(game.name, game.status_str()),
+                image=self._icons[game.status()])
+    def _add_status_bar(self, frm):
+        ttk.Separator(frm, orient=HORIZONTAL).grid(row=1, column=0, columnspan=3, sticky='ew')
+        self.status = ttk.Label(frm, text='')
+        self.status.grid(row=2, column=0, sticky='ew')
+    def _mark_complete(self, _event):
+        self.complete = True
+
+    def exec(self):
+        top_frame = ttk.Frame(self.root)
+        top_frame.grid()
+        main_widget_frame = ttk.Frame(top_frame, padding=10)
+        main_widget_frame.grid(row=0, column=0)
+        main_widget_frame.grid()
+        ttk.Label(main_widget_frame, text=self._title()).grid(row=0, column=1, columnspan=2)
+        self._add_games_view(main_widget_frame)
+        self._add_buttons(main_widget_frame)
+        # self._add_clipboard_frame(main_widget_frame)
+        self._add_progress_bar(main_widget_frame)
+        self._add_status_bar(top_frame)
+
+        self.root.bind('q', lambda _: self.root.destroy())
+        self.root.bind(ClipwaitThread.finish_event_name, self._mark_complete, add='+')
+
+        if self.col.status() == CollectionStatus.COMPLETE:
+            self.root.event_generate(ClipwaitThread.finish_event_name)
+        else:
+            self.clipwait_thread.start()
+            self.new_status('Listening on clipboard...')
+        self.root.mainloop()
+        return CollectionStatus.COMPLETE if self.complete else self.col.status()
