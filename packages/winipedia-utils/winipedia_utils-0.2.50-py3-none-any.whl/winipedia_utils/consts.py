c35020e0349c91d7df261f36fb183c8a83744df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:06:09 +0000

    Add package winipedia-utils/0.2.50/winipedia_utils-0.2.50-py3-none-any.whl

---
+++ packages/winipedia-utils/winipedia_utils-0.2.50-py3-none-any.whl/winipedia_utils/consts.py
@@ -0,0 +1,21 @@
+"""Constants used throughout the winipedia_utils package.
+
+This module contains package-wide constants that are used by various
+modules within the package. These constants define core configuration
+values and identifiers for the package.
+"""
+
+PACKAGE_NAME = "winipedia_utils"
+
+_DEV_DEPENDENCIES = [
+    "ruff",
+    "pre-commit",
+    "mypy",
+    "pytest",
+    "bandit",
+    "types-setuptools",
+    "types-tqdm",
+    "types-defusedxml",
+    "types-pyyaml",
+    "pytest-mock",
+]
