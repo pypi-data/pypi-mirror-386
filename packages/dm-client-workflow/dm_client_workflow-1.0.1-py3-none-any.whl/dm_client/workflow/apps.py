4bdeb057545ad3c77e1846822d30ae6067edaf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 08:05:43 +0000

    Add package dm-client-workflow/1.0.1/dm_client_workflow-1.0.1-py3-none-any.whl

---
+++ packages/dm-client-workflow/dm_client_workflow-1.0.1-py3-none-any.whl/dm_client/workflow/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+# from django.conf import settings
+# import logging
+#
+# logger = logging.getLogger()
+
+
+class DmClientWorkflowAppConfig(AppConfig):
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dm_client.workflow'
+    label = 'dm_client_workflow'
