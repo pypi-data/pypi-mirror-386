1820a5ff0f856ea71240174ef1bf7a02a78d737b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:13 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp311-cp311-macosx_11_0_arm64.whl/randomgen/sfmt.pyi
@@ -0,0 +1,24 @@
+import numpy as np
+
+from randomgen.common import BitGenerator
+from randomgen.typing import IntegerSequenceSeed, SeedMode
+
+class SFMT(BitGenerator):
+    def __init__(
+        self,
+        seed: IntegerSequenceSeed | None = ...,
+        *,
+        mode: SeedMode | None = ...,
+    ) -> None: ...
+    def seed(self, seed: IntegerSequenceSeed | None = ...) -> None: ...
+    def jump(self, iter: int = ...) -> SFMT: ...
+    def jumped(self, iter: int = ...) -> SFMT: ...
+    @property
+    def state(
+        self,
+    ) -> dict[str, str | int | np.ndarray | dict[str, int | np.ndarray]]: ...
+    @state.setter
+    def state(
+        self,
+        value: dict[str, str | int | np.ndarray | dict[str, int | np.ndarray]],
+    ) -> None: ...
