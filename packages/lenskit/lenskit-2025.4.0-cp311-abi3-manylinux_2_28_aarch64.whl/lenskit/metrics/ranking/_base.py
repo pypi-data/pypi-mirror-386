f433a8cce552f21a480fcec7e24e1508482458a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:57 +0000

    Add package lenskit/2025.4.0/lenskit-2025.4.0-cp311-abi3-manylinux_2_28_aarch64.whl

---
+++ packages/lenskit/lenskit-2025.4.0-cp311-abi3-manylinux_2_28_aarch64.whl/lenskit/metrics/ranking/_base.py
@@ -0,0 +1,57 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from lenskit.data import ItemList
+
+from .._base import DecomposedMetric, GlobalMetric, ListMetric, Metric
+
+__all__ = ["Metric", "ListMetric", "GlobalMetric", "DecomposedMetric", "RankingMetricBase"]
+
+
+class RankingMetricBase(Metric):
+    """
+    Base class for most ranking metrics, implementing a ``k`` parameter for
+    truncation.
+
+    Args:
+        k:
+            Specify the length cutoff for rankings. Rankings longer than this
+            will be truncated prior to measurement.
+
+    Stability:
+        Caller
+    """
+
+    k: int | None = None
+    "The maximum length of rankings to consider."
+
+    def __init__(self, k: int | None = None):
+        if k is not None and k < 0:
+            raise ValueError("k must be positive or None")
+        self.k = k
+
+    @property
+    def label(self):
+        """
+        Default name — class name, optionally @K.
+        """
+        name = self.__class__.__name__
+        if self.k is not None:
+            return f"{name}@{self.k}"
+        else:
+            return name
+
+    def truncate(self, items: ItemList):
+        """
+        Truncate an item list if it is longer than :attr:`k`.
+        """
+        if self.k is not None:
+            if not items.ordered:
+                raise ValueError("top-k filtering requires ordered list")
+            if len(items) > self.k:
+                return items[: self.k]
+
+        return items
