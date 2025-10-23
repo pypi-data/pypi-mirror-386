b592385ae0e4ba151d6b718c9e7cd6552067ff83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 03:19:24 +0000

    Add package dsf-mobility/3.13.2/dsf_mobility-3.13.2-cp310-cp310-macosx_15_0_arm64.whl

---
+++ packages/dsf-mobility/dsf_mobility-3.13.2-cp310-cp310-macosx_15_0_arm64.whl/dsf/sources/Roundabout.cpp
@@ -0,0 +1,20 @@
+#include "../headers/Roundabout.hpp"
+
+#include <format>
+#include <stdexcept>
+
+namespace dsf {
+  Roundabout::Roundabout(const RoadJunction& node) : RoadJunction{node} {}
+
+  void Roundabout::enqueue(std::unique_ptr<Agent> pAgent) {
+    assert(!isFull());
+    m_agents.push(std::move(pAgent));
+  }
+
+  std::unique_ptr<Agent> Roundabout::dequeue() {
+    assert(!m_agents.empty());
+    std::unique_ptr<Agent> pAgent{std::move(m_agents.front())};
+    m_agents.pop();
+    return pAgent;
+  }
+}  // namespace dsf
