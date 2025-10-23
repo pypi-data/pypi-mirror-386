e70406b07f44a59ba190e09a9a935a4185ef9a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 14:05:31 +0000

    Add package redblackgraph/0.5.1/redblackgraph-0.5.1-cp312-cp312-win_amd64.whl

---
+++ packages/redblackgraph/redblackgraph-0.5.1-cp312-cp312-win_amd64.whl/redblackgraph/__config__.py
@@ -0,0 +1,10 @@
+# This file is generated during the build process
+# It contains configuration information about how the package was built
+
+def show():
+    """Show configuration information."""
+    print("RedBlackGraph configuration:")
+    print("  Python version: 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)]")
+    print("  Build system: Meson")
+    
+__all__ = ['show']
