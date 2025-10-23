47015f6deebcaf63c16efd9e093205db4be12574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:05:20 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp314-cp314t-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp314-cp314t-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/randomgen/hc128.pyi
@@ -0,0 +1,24 @@
+from collections.abc import Sequence
+
+import numpy as np
+
+from randomgen.common import BitGenerator
+from randomgen.typing import IntegerSequenceSeed, SeedMode
+
+class HC128(BitGenerator):
+    def __init__(
+        self,
+        seed: IntegerSequenceSeed | None = ...,
+        *,
+        key: int | Sequence[int] | None = ...,
+        mode: SeedMode = ...,
+    ) -> None: ...
+    def seed(
+        self,
+        seed: IntegerSequenceSeed | None = ...,
+        key: int | Sequence[int] | None = ...,
+    ) -> None: ...
+    @property
+    def state(self) -> dict[str, str | dict[str, int | np.ndarray]]: ...
+    @state.setter
+    def state(self, value: dict[str, str | dict[str, int | np.ndarray]]) -> None: ...
