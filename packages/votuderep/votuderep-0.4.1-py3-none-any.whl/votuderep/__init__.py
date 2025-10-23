5c23e6c0e34c4ba0b56f4538a0f8b59e02c9838a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:00 +0000

    Add package votuderep/0.4.1/votuderep-0.4.1-py3-none-any.whl

---
+++ packages/votuderep/votuderep-0.4.1-py3-none-any.whl/votuderep/__init__.py
@@ -0,0 +1,7 @@
+"""votuderep - A CLI tool for dereplicating and filtering viral contigs."""
+
+__version__ = "0.4.1"
+__author__ = "Andrea Telatin"
+__license__ = "MIT"
+
+__all__ = ["__version__", "__author__", "__license__"]
