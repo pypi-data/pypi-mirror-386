f2396a916c6c9618e8f42bc6641ee065168dc23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 15:06:26 +0000

    Add package chinaunicom-agent/0.3.8/chinaunicom_agent-0.3.8.tar.gz

---
+++ packages/chinaunicom-agent/chinaunicom_agent-0.3.8.tar.gz/chinaunicom_agent-0.3.8/README.md
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#1.安装依赖包
+```cmd
+pip install chinaunicom-agent
+```
+
+#2.配置项目过程管理平台 用户名、密码及iNode本地安装地址
+在Linux/Unix/macOS系统中，可以使用以下命令:
+```cmd
+   export CHINAUNICOM_USERNAME="your_username"
+   export CHINAUNICOM_PASSWORD="your_password"
+   export INODELOCATION="your_inode_location"
+```
+
+在Windows系统中，可以使用以下命令：
+```cmd
+   set CHINAUNICOM_USERNAME=your_username
+   set CHINAUNICOM_PASSWORD=your_password
+   set INODELOCATION=your_inode_location
+```
---
+++ packages/chinaunicom-agent/chinaunicom_agent-0.3.8.tar.gz/chinaunicom_agent-0.3.8/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="chinaunicom-agent",
+    version="0.3.8",
+    author="zhaoyongzheng",
+    author_email="17668860550@163.com",
+    description="可为山东联通产互员工自动填报工时，整理周报",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/your-package",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "httpx",
+        "pendulum",
+        "pymsgbox",
+        "pywinauto",
+        "selenium",
+        "pytesseract",
+        "Pillow",
+        "ddddocr",
+        "requests",
+        "pyautogui",
+        "pyperclip",
+        "opencv-python",
+        "typing-extensions",
+    ],
+)
