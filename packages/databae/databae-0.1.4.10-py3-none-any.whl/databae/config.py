5af18d469663f14847d9994673757ff545847a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 16:05:24 +0000

    Add package databae/0.1.4.10/databae-0.1.4.10-py3-none-any.whl

---
+++ packages/databae/databae-0.1.4.10-py3-none-any.whl/databae/config.py
@@ -0,0 +1,25 @@
+from fyg import Config
+
+config = Config({
+	"prags": None,
+	"cache": True,
+	"optimize": False,
+	"refcount": False,
+	"main": "sqlite:///data.db",
+	"test": "sqlite:///data_test.db",
+	"blob": "blob",
+	"alter": False, # add new columns to tables - sqlite only!
+	"echo": False,
+	"stringsize": 500,
+	"flatkeysize": 80,
+	"index": {
+		"key": False,
+		"named": False
+	},
+	"pool": {
+		"null": True,
+		"size": 10,
+		"recycle": 30,
+		"overflow": 20
+	}
+})
