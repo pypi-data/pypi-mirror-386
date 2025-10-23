5c42756564a5bfa6ed0619db68d89cb3245d2815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 05:06:16 +0000

    Add package randomgen/2.3.0/randomgen-2.3.0-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/randomgen/randomgen-2.3.0-cp313-cp313-macosx_11_0_arm64.whl/randomgen/threefry.pyi
@@ -0,0 +1,34 @@
+import numpy as np
+
+from randomgen.common import BitGenerator
+from randomgen.typing import IntegerSequenceSeed, SeedMode
+
+class ThreeFry(BitGenerator):
+    def __init__(
+        self,
+        seed: IntegerSequenceSeed | None = ...,
+        *,
+        counter: int | np.ndarray | None = ...,
+        key: int | np.ndarray | None = ...,
+        number: int = ...,
+        width: int = ...,
+        mode: SeedMode | None = ...,
+    ) -> None: ...
+    def seed(
+        self,
+        seed: IntegerSequenceSeed | None = ...,
+        counter: int | np.ndarray | None = ...,
+        key: int | np.ndarray | None = ...,
+    ) -> None: ...
+    @property
+    def state(
+        self,
+    ) -> dict[str, str | int | np.ndarray | dict[str, np.ndarray]]: ...
+    @state.setter
+    def state(
+        self,
+        value: dict[str, str | int | np.ndarray | dict[str, np.ndarray]],
+    ) -> None: ...
+    def jump(self, iter: int = ...) -> ThreeFry: ...
+    def jumped(self, iter: int = ...) -> ThreeFry: ...
+    def advance(self, delta: int, counter: bool | None = ...) -> ThreeFry: ...
