ba741200bcb8bdf941e7b2bf4863e2ccfd17f3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-23 07:04:30 +0000

    Add package holado/0.11.0/holado-0.11.0.tar.gz

---
+++ packages/holado/holado-0.11.0.tar.gz/holado-0.11.0/tests/scripts/independant_runner/environment.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+# Import generic environment methods
+from test_testing_solution_ksc.behave_environment import *
+
+# Define project specific environment methods
+
+
+
+
