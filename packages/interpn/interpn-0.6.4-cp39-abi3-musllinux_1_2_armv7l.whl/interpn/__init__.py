039e60184035b4d7d5574bea65971c7aa0f733da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 16:08:04 +0000

    Add package interpn/0.6.4/interpn-0.6.4-cp39-abi3-musllinux_1_2_armv7l.whl

---
+++ packages/interpn/interpn-0.6.4-cp39-abi3-musllinux_1_2_armv7l.whl/interpn/__init__.py
@@ -0,0 +1,29 @@
+"""
+Python bindings to the `interpn` Rust library
+for N-dimensional interpolation and extrapolation.
+"""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+from .multilinear_regular import MultilinearRegular
+from .multilinear_rectilinear import MultilinearRectilinear
+from .multicubic_regular import MulticubicRegular
+from .multicubic_rectilinear import MulticubicRectilinear
+from .nearest_regular import NearestRegular
+from .nearest_rectilinear import NearestRectilinear
+from interpn import raw
+
+__version__ = version("interpn")
+
+__all__ = [
+    "__version__",
+    "MultilinearRegular",
+    "MultilinearRectilinear",
+    "MulticubicRegular",
+    "MulticubicRectilinear",
+    "NearestRegular",
+    "NearestRectilinear",
+    "raw",
+]
