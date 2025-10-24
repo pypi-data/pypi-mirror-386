9264d6929af82741d4fabedf6f716f1779983d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:06:49 +0000

    Add package py-volumn-test/0.0.2/py_volumn_test-0.0.2.tar.gz

---
+++ packages/py-volumn-test/py_volumn_test-0.0.2.tar.gz/py_volumn_test-0.0.2/src/py_volumn_test/_cmd/run.py
@@ -0,0 +1,5 @@
+import py_volumn_test as project
+from j_vault_http_client_xethhung12 import client
+def main():
+    client.load_to_env()
+    project.exec()
---
+++ packages/py-volumn-test/py_volumn_test-0.0.2.tar.gz/py_volumn_test-0.0.2/test/a.sh
@@ -0,0 +1,12 @@
+vault kv put hkse/bk/volumn-test/www.specialtiesexpress.com \
+api_key=d00f34eb1d3aefa0c974bae88ab9763907c2b \
+bootstrap_id=68e71a060d20f7250002812c \
+collection_name=cxstoragexrecord \
+filter_name=www.specialtiesexpress.com \
+host=skhxkk-1d30.restdb.io 
+
+vault_mount_point=hkse
+vault_secret_path=bk/volumn-test/mysql.specialtiesexpress.com
+vault-addr=https://vault.royzmarket.com
+vault-role-id=b66692af-ffd0-1230-cc1a-0b7f1a3168be
+vault-secret-id=94ca9911-78df-003d-969e-708a3ec0c1ab
