0dabfebf20272167f22cc142ef7870d807d6a3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:31 +0000

    Add package spsdk-pylint-plugins/0.2.3/spsdk_pylint_plugins-0.2.3.tar.gz

---
+++ packages/spsdk-pylint-plugins/spsdk_pylint_plugins-0.2.3.tar.gz/spsdk_pylint_plugins-0.2.3/spsdk_pylint_plugins/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2024-2025 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Top-level package for PyLint extensions for SPSDK coding style."""
+
+__author__ = """NXP"""
+__email__ = "spsdk@gmail.com"
+__version__ = "0.2.3"
+
+from pylint.lint import PyLinter
+
+from .assert_isinstance_checker import AssertIsinstanceChecker
+from .click_choice_check import ClickChoiceChecker
+from .typing_checker import TypingChecker
+
+
+def register(linter: PyLinter) -> None:
+    """Register all SPSDK plugins."""
+    checkers = linter.get_checker_names()
+    if ClickChoiceChecker.name not in checkers:
+        linter.register_checker(ClickChoiceChecker(linter))
+    if AssertIsinstanceChecker.name not in checkers:
+        linter.register_checker(AssertIsinstanceChecker(linter))
+    if TypingChecker.name not in checkers:
+        linter.register_checker(TypingChecker(linter))
