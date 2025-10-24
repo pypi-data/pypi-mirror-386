4493a76af3ff570d878fb5e448ffb472b1ba81c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 08:07:06 +0000

    Add package crow-kit/0.1.0/crow_kit-0.1.0-py3-none-any.whl

---
+++ packages/crow-kit/crow_kit-0.1.0-py3-none-any.whl/crow_kit/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    setTableWrapper,
+    setGeneralWrapper,
+    getWrapperData,
+    listWrappers,
+    getTableWrapperData,
+    getGeneralWrapperData
+)
+
+__all__ = [
+    "setTableWrapper",
+    "setGeneralWrapper",
+    "getWrapperData",
+    "listWrappers",
+    "getTableWrapperData",
+    "getGeneralWrapperData",
+]
