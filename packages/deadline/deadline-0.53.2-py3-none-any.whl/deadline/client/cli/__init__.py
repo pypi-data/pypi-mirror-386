a6c2ddc4e232a3ebac690c42c2630f269f51225e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:06:01 +0000

    Add package deadline/0.53.2/deadline-0.53.2-py3-none-any.whl

---
+++ packages/deadline/deadline-0.53.2-py3-none-any.whl/deadline/client/cli/__init__.py
@@ -0,0 +1,7 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+__all__ = ["deadline_dev_gui_main", "main"]
+
+from . import deadline_dev_gui_main
+
+from ._deadline_cli import main
---
+++ packages/deadline/deadline-0.53.2-py3-none-any.whl/deadline/client/config/__init__.py
@@ -0,0 +1,29 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+"""
+This module encapsulate the configuration of AWS Deadline Cloud on a workstation.
+
+By default, configuration is stored in `~/.deadline/config`. If a user sets
+the environment variable DEADLINE_CONFIG_FILE_PATH, it is used as the configuration
+file path instead.
+"""
+
+__all__ = [
+    "get_setting_default",
+    "get_setting",
+    "set_setting",
+    "clear_setting",
+    "get_best_profile_for_farm",
+    "str2bool",
+    "DEFAULT_DEADLINE_ENDPOINT_URL",
+]
+
+from .config_file import (
+    DEFAULT_DEADLINE_ENDPOINT_URL,
+    get_best_profile_for_farm,
+    get_setting,
+    get_setting_default,
+    set_setting,
+    clear_setting,
+    str2bool,
+)
