92677520fc2651f2f31fa50e9ff8a7ddff82072f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:19:54 +0000

    Add package iatikit/3.6.1/iatikit-3.6.1.tar.gz

---
+++ packages/iatikit/iatikit-3.6.1.tar.gz/iatikit-3.6.1/iatikit/utils/helpers.py
@@ -0,0 +1,19 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+http_adapter = HTTPAdapter(max_retries=Retry(total=3))
+
+
+def get_iati_versions():
+    session = requests.Session()
+    session.mount("https://", http_adapter)
+    versions_url = (
+        "http://reference.iatistandard.org/201/codelists/"
+        + "downloads/clv2/json/en/Version.json"
+    )
+    versions_response = session.get(versions_url)
+    versions_response.raise_for_status()
+    versions = [d["code"] for d in versions_response.json()["data"]]
+    versions.reverse()
+    return versions
