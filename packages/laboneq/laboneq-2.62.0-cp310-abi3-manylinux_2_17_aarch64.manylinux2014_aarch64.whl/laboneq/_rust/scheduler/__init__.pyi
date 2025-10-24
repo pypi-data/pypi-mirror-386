7aea34a8de371021e47e8b4a79008e3f4a49b06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:06:16 +0000

    Add package laboneq/2.62.0/laboneq-2.62.0-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/laboneq/laboneq-2.62.0-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/laboneq/_rust/scheduler/__init__.pyi
@@ -0,0 +1,90 @@
+# Copyright 2022 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from laboneq.data.compilation_job import ParameterInfo
+from laboneq.data.experiment_description import Experiment
+from laboneq.compiler.common.awg_info import AwgKey
+from laboneq.compiler.scheduler.oscillator_schedule import (
+    InitialOscillatorFrequencySchedule,
+    InitialLocalOscillatorFrequencySchedule,
+)
+from laboneq.compiler.scheduler.voltage_offset import InitialOffsetVoltageSchedule
+
+class ExperimentInfo:
+    """Object containing the information about the experiment.
+
+    This object cannot be send to threads and must be used in the same thread.
+    """
+
+class Oscillator:
+    def __init__(self, uid: str, frequency: float | ParameterInfo, is_hardware: bool):
+        """A representation of an oscillator."""
+
+class Signal:
+    def __init__(
+        self,
+        uid: str,
+        sampling_rate: float,
+        awg_key: int,
+        device: str,
+        oscillator: Oscillator | None,
+        lo_frequency: float | ParameterInfo | None,
+        voltage_offset: float | ParameterInfo | None,
+    ):
+        """A representation of signal properties."""
+
+def build_experiment(experiment: Experiment, signals: list[Signal]) -> ExperimentInfo:
+    """Build a scheduled experiment.
+
+    Args:
+        experiment: Experiment description.
+        signals: List of signals.
+
+    Returns:
+        An object containing the Rust experiment
+    """
+
+class Schedules:
+    """A compatibility layer for schedules between Python and Rust."""
+
+    initial_oscillator_frequency: list[InitialOscillatorFrequencySchedule]
+    initial_local_oscillator_frequency: list[InitialLocalOscillatorFrequencySchedule]
+    initial_voltage_offset: list[InitialOffsetVoltageSchedule]
+
+class RepetitionInfo:
+    mode: str  # "fastest", "constant", "auto"
+    time: float | None
+    loop_uid: str
+
+class ScheduledExperiment:
+    """Result of an experiment scheduling.
+
+    Attributes:
+        max_acquisition_time_per_awg: Maximum acquisition time per AWG in seconds.
+        repetition_info: Repetition information of the experiment.
+        system_grid: System grid in tinysamples.
+        used_parameters: Used near-time parameters in the experiment.
+        schedules: Schedules object containing various schedules.
+    """
+
+    max_acquisition_time_per_awg: dict[AwgKey, float]
+    repetition_info: RepetitionInfo | None
+    system_grid: int
+    used_parameters: set[str]
+    schedules: Schedules
+
+def schedule_experiment(
+    experiment: ExperimentInfo,
+    parameters: dict[str, float],
+    chunking_info: tuple[int, int] | None,
+) -> ScheduledExperiment:
+    """Schedule an experiment.
+
+    Args:
+        experiment: Experiment information.
+        parameters: Dictionary of parameter values to be resolved.
+        chunking_info: Tuple of (current chunk index, total chunk count) or None.
+
+    Returns:
+        Scheduled experiment.
+    """
