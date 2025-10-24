82bbd3eff150ef4c6887959e1954e36fe09b1ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:18:52 +0000

    Add package enterprise-integrated-channels/0.1.22/enterprise_integrated_channels-0.1.22-py2.py3-none-any.whl

---
+++ packages/enterprise-integrated-channels/enterprise_integrated_channels-0.1.22-py2.py3-none-any.whl/channel_integrations/api/v1/moodle/serializers.py
@@ -0,0 +1,25 @@
+"""
+    Serializer for Moodle configuration.
+"""
+from rest_framework import serializers
+
+from channel_integrations.api.serializers import EnterpriseCustomerPluginConfigSerializer
+from channel_integrations.moodle.models import MoodleEnterpriseCustomerConfiguration
+
+
+class MoodleConfigSerializer(EnterpriseCustomerPluginConfigSerializer):
+    class Meta:
+        model = MoodleEnterpriseCustomerConfiguration
+        extra_fields = (
+            'moodle_base_url',
+            'service_short_name',
+            'category_id',
+            'encrypted_username',
+            'encrypted_password',
+            'encrypted_token',
+        )
+        fields = EnterpriseCustomerPluginConfigSerializer.Meta.fields + extra_fields
+
+    encrypted_password = serializers.CharField(required=False, allow_blank=False, read_only=False)
+    encrypted_username = serializers.CharField(required=False, allow_blank=False, read_only=False)
+    encrypted_token = serializers.CharField(required=False, allow_blank=False, read_only=False)
