b3eec5a03681d6a32e41437682796e14342f41e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:42 +0000

    Add package temporalio/1.14.2/temporalio-1.14.2-cp39-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/temporalio/temporalio-1.14.2-cp39-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/temporalio/api/batch/v1/__init__.py
@@ -0,0 +1,23 @@
+from .message_pb2 import (
+    BatchOperationCancellation,
+    BatchOperationDeletion,
+    BatchOperationInfo,
+    BatchOperationReset,
+    BatchOperationSignal,
+    BatchOperationTermination,
+    BatchOperationTriggerWorkflowRule,
+    BatchOperationUnpauseActivities,
+    BatchOperationUpdateWorkflowExecutionOptions,
+)
+
+__all__ = [
+    "BatchOperationCancellation",
+    "BatchOperationDeletion",
+    "BatchOperationInfo",
+    "BatchOperationReset",
+    "BatchOperationSignal",
+    "BatchOperationTermination",
+    "BatchOperationTriggerWorkflowRule",
+    "BatchOperationUnpauseActivities",
+    "BatchOperationUpdateWorkflowExecutionOptions",
+]
