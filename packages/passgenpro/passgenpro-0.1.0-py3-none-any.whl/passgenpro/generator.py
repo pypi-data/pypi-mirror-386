51e526225e7dfcc09fdf54db231583111bb10646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:45 +0000

    Add package passgenpro/0.1.0/passgenpro-0.1.0-py3-none-any.whl

---
+++ packages/passgenpro/passgenpro-0.1.0-py3-none-any.whl/passgenpro/generator.py
@@ -0,0 +1,21 @@
+﻿import random
+import string
+
+def generate(length=12, symbols=True, numbers=True):
+    """
+    Güclü parol generatoru.
+    Parametrlər:
+        length (int): parol uzunluğu (defolt 12)
+        symbols (bool): simvol əlavə edilsin?
+        numbers (bool): rəqəmlər əlavə edilsin?
+    """
+    letters = string.ascii_letters
+    digits = string.digits if numbers else ""
+    special = "!@#$%^&*()-_=+[]{};:,.<>?" if symbols else ""
+
+    all_chars = letters + digits + special
+    if not all_chars:
+        raise ValueError("Heç bir simvol seçilməyib!")
+
+    password = ''.join(random.choice(all_chars) for _ in range(length))
+    return password
