464a4bbe5a67a5e255cf3ab6c6e2b9add792fed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:18:42 +0000

    Add package webjeux-flash-downloader/1.0/webjeux_flash_downloader-1.0.tar.gz

---
+++ packages/webjeux-flash-downloader/webjeux_flash_downloader-1.0.tar.gz/webjeux_flash_downloader-1.0/README.md
@@ -0,0 +1,11 @@
+A huge thanks to WebJeux website for the service.
+
+This is a simple python program that allow users to download their favorite flash games for free.
+It uses www.webjeux.com API.
+
+This program offers:
+-Simple navigable interface
+-Suggestion list that contains all the games available on the website
+-Errors detection functionality
+
+Thank you for downloading and have fun!
---
+++ packages/webjeux-flash-downloader/webjeux_flash_downloader-1.0.tar.gz/webjeux_flash_downloader-1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="webjeux-flash-downloader",
+    version="1.0",
+    author="Salah eddine Talli",
+    author_email="tallisalaheddine@gmail.com",
+    description="Download flash games from WebJeux website",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={
+        "my_package": ["data/flipline_studio.png", "data/Webjeux_games_names.txt"],
+    },
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "bs4",
+        "pillow",
+        "customtkinter",
+        "tkinter",
+        "os",
+        "pathlib"
+    ],
+    entry_points={
+        "console_scripts": [
+            "webjeux-flash-downloader=my_package.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+)
