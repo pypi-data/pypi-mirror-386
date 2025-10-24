74dc9c616c74e3e531e4bcd0640519a38e8deead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 16:07:48 +0000

    Add package androsing/1.0.0/androsing-1.0.0.tar.gz

---
+++ packages/androsing/androsing-1.0.0.tar.gz/androsing-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name="androsing",
+    version="1.0.0",
+    description="Fast APK file signer",
+    url="https://github.com/FMZNkdv",
+    author="FMZNkdv",
+    author_email="fmznkdev@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
