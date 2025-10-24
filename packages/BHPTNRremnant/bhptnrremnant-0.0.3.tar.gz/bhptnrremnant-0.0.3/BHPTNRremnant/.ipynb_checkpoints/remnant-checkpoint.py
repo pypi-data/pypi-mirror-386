b1178ea8a34971817873530415187232d78b92d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 07:05:37 +0000

    Add package BHPTNRremnant/0.0.3/bhptnrremnant-0.0.3.tar.gz

---
+++ packages/BHPTNRremnant/bhptnrremnant-0.0.3.tar.gz/bhptnrremnant-0.0.3/BHPTNRremnant/.ipynb_checkpoints/remnant-checkpoint.py
@@ -0,0 +1,177 @@
+#############################################################################
+##
+##      Filename: remnant.py
+##
+##      Author: Tousif Islam
+##
+##      Created: 01-05-2023
+##
+##      Description: Calculates properties of the final black hole in a 
+##                   BBH merger using GPR fits
+##
+##      Modified:
+##
+#############################################################################
+
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+import pickle
+import argparse
+import sys, subprocess
+
+import os
+package_home = os.path.dirname(__file__) 
+
+class BHPTNRSurRemnant():
+    """
+    Class to load GPR fits and generate remnant properties
+    """
+    
+    def __init__(self):
+        gpr_models = self._load_fits()
+        self.mf_model = gpr_models['remnant_mass']
+        self.sf_model = gpr_models['remnant_spin']
+        self.vf_model = gpr_models['remnant_kick']
+        self.Lpeak_model = gpr_models['peak_luminosity']
+        
+        
+    def _load_fits(self):
+        """
+        loads pickle file containing GPR fits for the remnant mass, remnant spin,
+        peak luminosity and kick velocity
+        """
+        models = []
+        
+        if os.path.isfile("%s/data/BHPTNRSur1dq1e3_remnant.pickle"%package_home)==False:
+            os.system('wget https://zenodo.org/record/8162005/files/BHPTNRSur1dq1e3_remnant.pickle -P %s/data/'%(package_home))
+            print('... remnant fit downloaded')
+        
+        with open("%s/data/BHPTNRSur1dq1e3_remnant.pickle"%package_home, "rb") as f:
+            while True:
+                try:
+                    models.append(pickle.load(f))
+                except EOFError:
+                    break
+
+        gpr_models = {'remnant_mass': models[0],
+                      'remnant_spin': models[1],
+                      'remnant_kick': models[2], 
+                      'peak_luminosity': models[3]}
+
+        return gpr_models
+
+
+    def _X_parameterization(self, X_input):
+        """
+        transform input params to fit params : q to log10(q)
+        """
+        X_fit = np.array([np.log10(X_input)])
+        return X_fit.reshape(-1,1)
+
+    
+    def _gpr_output_to_remnant_mass(self, gpr_eval):
+        """
+        transforms fit output to physical remnant mass
+        """
+        return 1-10**gpr_eval
+
+    
+    def _gpr_output_to_remnant_spin(self, gpr_eval):
+        """
+        transforms fit output to physical remnant spin
+        """
+        return 10**gpr_eval
+    
+    
+    def _gpr_output_to_remnant_kick(self, gpr_eval):
+        """
+        transforms fit output to physical remnant kick velocity
+        """
+        return 10**gpr_eval
+
+    
+    def _gpr_output_to_peak_lumoinosity(self, gpr_eval):
+        """
+        transforms fit output to physical peak luminosity values
+        """
+        return 10**gpr_eval
+    
+    
+    def _gpr_err_remnant_mass(self, gpr_eval, grp_err_eval):
+        """
+        computes the error in GPR fits in the linear scale
+        """
+        return self._gpr_output_to_remnant_mass(gpr_eval + grp_err_eval) - self._gpr_output_to_remnant_mass(gpr_eval)
+    
+    
+    def _evaluate_Mf_fit_at_Xfit(self, X_fit):
+        """
+        evaluates GPR fits at the parameterized X values for the remnant mass
+        """
+        # compute GPR model predictions
+        mf_gpr, mferr_gpr = self.mf_model.predict(X_fit, return_std=True)
+        # transform the fit outputs to meaningful physical quantities
+        mf = self._gpr_output_to_remnant_mass(mf_gpr)
+        # compute GPR errors in the linear scale
+        mferr = self._gpr_output_to_remnant_mass(mf_gpr + mferr_gpr) - self._gpr_output_to_remnant_mass(mf_gpr)
+        return mf, mferr
+    
+    def _evaluate_Sf_fit_at_Xfit(self, X_fit):
+        """
+        evaluates GPR fits at the parameterized X values for the remnant spin
+        """
+        # compute GPR model predictions
+        sf_gpr, sferr_gpr = self.sf_model.predict(X_fit, return_std=True)
+        # transform the fit outputs to meaningful physical quantities
+        sf = self._gpr_output_to_remnant_mass(sf_gpr)
+        # compute GPR errors in the linear scale
+        sferr = self._gpr_output_to_remnant_spin(sf_gpr + sferr_gpr) - self._gpr_output_to_remnant_spin(sf_gpr)
+        return sf, sferr
+    
+    def _evaluate_vf_fit_at_Xfit(self, X_fit):
+        """
+        evaluates GPR fits at the parameterized X values for the remnant kick
+        """
+        # compute GPR model predictions
+        vf_gpr, vferr_gpr = self.vf_model.predict(X_fit, return_std=True)
+        # transform the fit outputs to meaningful physical quantities
+        vf = self._gpr_output_to_remnant_kick(vf_gpr)
+        # compute GPR errors in the linear scale
+        vferr = self._gpr_output_to_remnant_kick(vf_gpr + vferr_gpr) - self._gpr_output_to_remnant_kick(vf_gpr)
+        return vf, vferr
+    
+    
+    def _evaluate_Lpeak_fit_at_Xfit(self, X_fit):
+        """
+        evaluates GPR fits at the parameterized X values for the peak luminosity
+        """
+        # compute GPR model predictions
+        Lp_gpr, Lperr_gpr = self.Lpeak_model.predict(X_fit, return_std=True)
+        # transform the fit outputs to meaningful physical quantities
+        Lp = self._gpr_output_to_peak_lumoinosity(Lp_gpr)
+        # compute GPR errors in the linear scale
+        Lperr = self._gpr_output_to_peak_lumoinosity(Lp_gpr + Lperr_gpr) -  self._gpr_output_to_peak_lumoinosity(Lp_gpr)
+        return Lp, Lperr
+    
+    
+    def _evaluate_fit_at_Xfit(self, X_fit):
+        """
+        evaluates all GPR fits at the parameterized X values
+        """        
+        mf, mferr = self._evaluate_Mf_fit_at_Xfit(X_fit)
+        sf, sferr = self._evaluate_Sf_fit_at_Xfit(X_fit)
+        vf, vferr = self._evaluate_vf_fit_at_Xfit(X_fit)
+        Lp, Lperr = self._evaluate_Lpeak_fit_at_Xfit(X_fit) 
+        return mf, mferr, sf, sferr, vf, vferr, Lp, Lperr
+        
+        
+    def evaluate_fit(self, X_input):
+        """
+        evaluate all GPR fits at a given value of the BBH parameter
+        """
+        X_fit = self._X_parameterization(X_input)
+        mf, mferr, sf, sferr, vf, vferr, Lp, Lperr = self._evaluate_fit_at_Xfit(X_fit)
+        if isinstance(X_input, np.ndarray) or isinstance(X_input, list):
+            return mf, mferr, sf, sferr, vf, vferr, Lp, Lperr 
+        else:
+            return mf[0], mferr[0], sf[0], sferr[0], vf[0], vferr[0], Lp[0], Lperr[0]
---
+++ packages/BHPTNRremnant/bhptnrremnant-0.0.3.tar.gz/bhptnrremnant-0.0.3/README.md
@@ -0,0 +1,75 @@
+# BHPTNR_Remnant
+
+```BHPTNR_Remnant``` is an easy-to-use python package to efficiently predict the remnant mass, remnant spin, peak luminosity and the final kick imparted on the remnant black hole directly from the gravitational radiation using GPR fits. These fits have been built on the remnant data calculated from numerical relativity informed black hole perturbation theory based waveforms. 
+
+## Tutorials
+
+Example usage is provided in ```tutorial.ipynb``` notebook.
+
+### Using pip
+This package is available in Pypi : [https://pypi.org/project/BHPTNRremnant/](https://pypi.org/project/BHPTNRremnant/)
+To install : ```pip install BHPTNRremnant```
+
+A web-based example jupyter notebook tutorial is hosted here : ```https://github.com/tousifislam/BHPTNR_Remnant/blob/main/tutorials/tutorial.ipynb```
+Once installed, import is as ```import BHPTNRremnant```
+
+NR informed ppBHPT based surrogate fits can then be used as
+```
+# import surrogate fits
+from BHPTNRremnant.remnant import BHPTNRSurRemnant
+
+# instantiate the class
+fits = BHPTNRSurRemnant()
+# evaluate the fits at mass ratio q
+# final mass, final spin, kick velocity and luminosity distance and their associated GPR fit error
+mf, mferr, sf, sferr, vf, vferr, Lp, Lperr = fits.evaluate_fit(15)
+print(mf,sf, vf,Lp)
+```
+
+One can also use the analytical fits for the kick velocities as following:
+```
+from BHPTNRremnant.analytical_fits import BHPTAnalyticalFits
+
+# instantiate analytical fits
+fit_obj = BHPTAnalyticalFits(q=15, a=0)
+# evaluate kick velocity form Sundararajan, Khanna & Hughes
+vf = fit_obj.SKH_kick_fit()
+print(vf)
+# evaluate kick velocity form Sundararajan, Khanna & Hughes
+# corrected for the small mass ratio
+vf = fit_obj.SKH_kick_fit_small_q_corrected()
+print(vf)
+# evaluate kick velocity form Islam, Field & Khanna
+vf = fit_obj.IFK_kick_fit()
+print(vf)
+```
+
+It is also possible to obtain analytical results at extreme mass ratio limit:
+```
+from BHPTNRremnant.point_particle import PointParticle
+
+# instantiate point particle limit results
+pp = PointParticle(q=1000, a=0)
+# obtain final mass and final spin
+pp.obtain_final_state()
+```
+
+## Citation guideline
+
+If you make use of any module from the Toolkit in your research please acknowledge using:
+
+> This work makes use of the Black Hole Perturbation Toolkit.
+
+If you make use of the BHPTNRSur models please cite the following paper:
+
+```
+@article{Islam:2022laz,
+    author = "Islam, Tousif and Field, Scott E. and Khanna, Gaurav.",
+    title = "{Remnant black hole properties from numerical-relativity-informed perturbation theory and implications for waveform modelling}",
+    eprint = "https://arxiv.org/abs/2301.07215",
+    archivePrefix = "arXiv",
+    primaryClass = "gr-qc",
+    month = "1",
+    year = "2023"
+}
+```
