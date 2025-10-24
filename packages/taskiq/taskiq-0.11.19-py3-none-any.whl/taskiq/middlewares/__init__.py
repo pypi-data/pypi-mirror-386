e01cb8bbc5eac9b264c007006fa7f2cf4d077a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:06:31 +0000

    Add package taskiq/0.11.19/taskiq-0.11.19-py3-none-any.whl

---
+++ packages/taskiq/taskiq-0.11.19-py3-none-any.whl/taskiq/middlewares/__init__.py
@@ -0,0 +1,11 @@
+"""Taskiq middlewares."""
+
+from .prometheus_middleware import PrometheusMiddleware
+from .simple_retry_middleware import SimpleRetryMiddleware
+from .smart_retry_middleware import SmartRetryMiddleware
+
+__all__ = (
+    "PrometheusMiddleware",
+    "SimpleRetryMiddleware",
+    "SmartRetryMiddleware",
+)
