d614a618c8c8ee7590b234fb9ca9653132c5804e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 08:06:38 +0000

    Add package yt-dlp-ejs/0.2.0/yt_dlp_ejs-0.2.0.tar.gz

---
+++ packages/yt-dlp-ejs/yt_dlp_ejs-0.2.0.tar.gz/yt_dlp_ejs-0.2.0/yt_dlp_ejs/yt/solver/__init__.py
@@ -0,0 +1,17 @@
+import importlib.resources
+
+import yt_dlp_ejs.yt.solver
+
+
+def core() -> str:
+    """
+    Read the contents of the JavaScript core solver bundle as string.
+    """
+    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "core.min.js").read_text()
+
+
+def lib() -> str:
+    """
+    Read the contents of the JavaScript library solver bundle as string.
+    """
+    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "lib.min.js").read_text()
