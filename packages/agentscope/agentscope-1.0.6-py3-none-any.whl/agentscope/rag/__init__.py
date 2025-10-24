1f998f664786060c9b7b26470ca1b85e434efa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:18:17 +0000

    Add package agentscope/1.0.6/agentscope-1.0.6-py3-none-any.whl

---
+++ packages/agentscope/agentscope-1.0.6-py3-none-any.whl/agentscope/rag/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""The retrieval-augmented generation (RAG) module in AgentScope."""
+
+from ._document import (
+    DocMetadata,
+    Document,
+)
+from ._reader import (
+    ReaderBase,
+    TextReader,
+    PDFReader,
+    ImageReader,
+)
+from ._store import (
+    VDBStoreBase,
+    QdrantStore,
+    MilvusLiteStore,
+)
+from ._knowledge_base import KnowledgeBase
+from ._simple_knowledge import SimpleKnowledge
+
+
+__all__ = [
+    "ReaderBase",
+    "TextReader",
+    "PDFReader",
+    "ImageReader",
+    "DocMetadata",
+    "Document",
+    "VDBStoreBase",
+    "QdrantStore",
+    "MilvusLiteStore",
+    "KnowledgeBase",
+    "SimpleKnowledge",
+]
