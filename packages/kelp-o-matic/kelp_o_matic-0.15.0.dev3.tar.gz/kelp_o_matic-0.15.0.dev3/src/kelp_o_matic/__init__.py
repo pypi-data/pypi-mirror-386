ddc1bb0989e4f7717e48137e2a7407ef789b043f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 07:05:58 +0000

    Add package kelp-o-matic/0.15.0.dev3/kelp_o_matic-0.15.0.dev3.tar.gz

---
+++ packages/kelp-o-matic/kelp_o_matic-0.15.0.dev3.tar.gz/kelp_o_matic-0.15.0.dev3/src/kelp_o_matic/__init__.py
@@ -0,0 +1,21 @@
+"""Kelp-O-Matic: A tool for segmenting audio files using machine learning models."""
+
+from importlib.metadata import version
+
+from kelp_o_matic.config import ModelConfig
+from kelp_o_matic.main import clean, models, revisions, safe2tif, segment
+from kelp_o_matic.model import ONNXModel
+from kelp_o_matic.registry import model_registry
+
+__all__ = [
+    "ModelConfig",
+    "ONNXModel",
+    "clean",
+    "model_registry",
+    "models",
+    "revisions",
+    "segment",
+    "safe2tif",
+]
+
+__version__ = version("kelp_o_matic")
