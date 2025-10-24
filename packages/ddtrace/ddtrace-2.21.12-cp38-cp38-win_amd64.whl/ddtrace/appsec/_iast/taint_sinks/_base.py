815ac69126a8581c351cd74d32de2c87c3f78ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:12 +0000

    Add package ddtrace/2.21.12/ddtrace-2.21.12-cp38-cp38-win_amd64.whl

---
+++ packages/ddtrace/ddtrace-2.21.12-cp38-cp38-win_amd64.whl/ddtrace/appsec/_iast/taint_sinks/_base.py
@@ -0,0 +1,163 @@
+import os
+import sysconfig
+from typing import Any
+from typing import Callable
+from typing import Optional
+from typing import Text
+
+from ddtrace.appsec._deduplications import deduplication
+from ddtrace.appsec._trace_utils import _asm_manual_keep
+from ddtrace.internal.logger import get_logger
+from ddtrace.settings.asm import config as asm_config
+from ddtrace.trace import tracer
+
+from .._iast_request_context import get_iast_reporter
+from .._iast_request_context import set_iast_reporter
+from .._overhead_control_engine import Operation
+from .._stacktrace import get_info_frame
+from .._utils import _is_iast_debug_enabled
+from ..reporter import Evidence
+from ..reporter import IastSpanReporter
+from ..reporter import Location
+from ..reporter import Vulnerability
+
+
+log = get_logger(__name__)
+
+CWD = os.path.abspath(os.getcwd())
+
+PURELIB_PATH = sysconfig.get_path("purelib")
+STDLIB_PATH = sysconfig.get_path("stdlib")
+
+
+class taint_sink_deduplication(deduplication):
+    def _check_deduplication(self):
+        return asm_config._iast_deduplication_enabled
+
+    def _extract(self, args):
+        # We skip positions 0 and 1 because they represent the 'cls' and 'span' respectively
+        return args[2:]
+
+
+def _check_positions_contained(needle, container):
+    needle_start, needle_end = needle
+    container_start, container_end = container
+
+    return (
+        (container_start <= needle_start < container_end)
+        or (container_start < needle_end <= container_end)
+        or (needle_start <= container_start < needle_end)
+        or (needle_start < container_end <= needle_end)
+    )
+
+
+class VulnerabilityBase(Operation):
+    vulnerability_type = ""
+
+    @classmethod
+    def wrap(cls, func: Callable) -> Callable:
+        def wrapper(wrapped: Callable, instance: Any, args: Any, kwargs: Any) -> Any:
+            """Get the current root Span and attach it to the wrapped function. We need the span to report the
+            vulnerability and update the context with the report information.
+            """
+            if not asm_config.is_iast_request_enabled:
+                if _is_iast_debug_enabled():
+                    log.debug(
+                        "iast::propagation::context::VulnerabilityBase.wrapper. No request quota or this vulnerability "
+                        "is outside the context"
+                    )
+                return wrapped(*args, **kwargs)
+            elif cls.has_quota():
+                return func(wrapped, instance, args, kwargs)
+            else:
+                return wrapped(*args, **kwargs)
+
+        return wrapper
+
+    @classmethod
+    @taint_sink_deduplication
+    def _prepare_report(cls, vulnerability_type, evidence, file_name, line_number):
+        if not asm_config.is_iast_request_enabled:
+            if _is_iast_debug_enabled():
+                log.debug(
+                    "iast::propagation::context::VulnerabilityBase._prepare_report. "
+                    "No request quota or this vulnerability is outside the context"
+                )
+            return False
+        if line_number is not None and (line_number == 0 or line_number < -1):
+            line_number = -1
+
+        report = get_iast_reporter()
+        span_id = 0
+        if tracer and hasattr(tracer, "current_root_span"):
+            span = tracer.current_root_span()
+            if span:
+                span_id = span.span_id
+                # Mark the span as kept to avoid being dropped by the agent.
+                #
+                # It is important to do it as soon as the vulnerability is reported
+                # to ensure that any downstream propagation performed has the new priority.
+                _asm_manual_keep(span)
+
+        vulnerability = Vulnerability(
+            type=vulnerability_type,
+            evidence=evidence,
+            location=Location(path=file_name, line=line_number, spanId=span_id),
+        )
+        if report:
+            report.vulnerabilities.add(vulnerability)
+        else:
+            report = IastSpanReporter(vulnerabilities={vulnerability})
+        report.add_ranges_to_evidence_and_extract_sources(vulnerability)
+
+        set_iast_reporter(report)
+
+        return True
+
+    @classmethod
+    def report(cls, evidence_value: Text = "", dialect: Optional[Text] = None) -> None:
+        """Build a IastSpanReporter instance to report it in the `AppSecIastSpanProcessor` as a string JSON"""
+        # TODO: type of evidence_value will be Text. We wait to finish the redaction refactor.
+        if cls.acquire_quota():
+            file_name = None
+            line_number = None
+
+            skip_location = getattr(cls, "skip_location", False)
+            if not skip_location:
+                frame_info = get_info_frame()
+                if not frame_info or frame_info[0] == "" or frame_info[0] == -1:
+                    return None
+
+                file_name, line_number = frame_info
+                if not file_name:
+                    return
+
+                file_name = cls._rel_path(file_name)
+                if not file_name:
+                    log.debug("Could not relativize vulnerability location path: %s", frame_info[0])
+                    return
+
+                if not cls.is_not_reported(file_name, line_number):
+                    return
+            # Evidence is a string in weak cipher, weak hash and weak randomness
+            if isinstance(evidence_value, (str, bytes, bytearray)):
+                evidence = Evidence(value=evidence_value, dialect=dialect)
+            else:
+                log.debug("Unexpected evidence_value type: %s", type(evidence_value))
+                evidence = Evidence(value="", dialect=dialect)
+
+            result = cls._prepare_report(cls.vulnerability_type, evidence, file_name, line_number)
+            # If result is None that's mean deduplication raises and no vulnerability wasn't reported, with that,
+            # we need to restore the quota
+            if not result:
+                cls.increment_quota()
+
+    @staticmethod
+    def _rel_path(file_name: str) -> str:
+        if file_name.startswith(PURELIB_PATH):
+            return os.path.relpath(file_name, start=PURELIB_PATH)
+        if file_name.startswith(STDLIB_PATH):
+            return os.path.relpath(file_name, start=STDLIB_PATH)
+        if file_name.startswith(CWD):
+            return os.path.relpath(file_name, start=CWD)
+        return ""
---
+++ packages/ddtrace/ddtrace-2.21.12-cp38-cp38-win_amd64.whl/ddtrace/appsec/_iast/taint_sinks/command_injection.py
@@ -0,0 +1,71 @@
+from typing import List
+from typing import Union
+
+from ddtrace.appsec._constants import IAST_SPAN_TAGS
+from ddtrace.appsec._iast import oce
+from ddtrace.appsec._iast._logs import iast_error
+from ddtrace.appsec._iast._metrics import _set_metric_iast_executed_sink
+from ddtrace.appsec._iast._metrics import _set_metric_iast_instrumented_sink
+from ddtrace.appsec._iast._metrics import increment_iast_span_metric
+from ddtrace.appsec._iast._taint_tracking._taint_objects import is_pyobject_tainted
+from ddtrace.appsec._iast.constants import VULN_CMDI
+import ddtrace.contrib.internal.subprocess.patch as subprocess_patch
+from ddtrace.internal.logger import get_logger
+from ddtrace.settings.asm import config as asm_config
+
+from ._base import VulnerabilityBase
+
+
+log = get_logger(__name__)
+
+
+def get_version() -> str:
+    return ""
+
+
+_IAST_CMDI = "iast_cmdi"
+
+
+def patch():
+    if asm_config._iast_enabled:
+        subprocess_patch.patch()
+        subprocess_patch.add_str_callback(_IAST_CMDI, _iast_report_cmdi)
+        subprocess_patch.add_lst_callback(_IAST_CMDI, _iast_report_cmdi)
+        _set_metric_iast_instrumented_sink(VULN_CMDI)
+
+
+def unpatch() -> None:
+    subprocess_patch.del_str_callback(_IAST_CMDI)
+    subprocess_patch.del_lst_callback(_IAST_CMDI)
+
+
+@oce.register
+class CommandInjection(VulnerabilityBase):
+    vulnerability_type = VULN_CMDI
+
+
+def _iast_report_cmdi(shell_args: Union[str, List[str]]) -> None:
+    report_cmdi = ""
+
+    try:
+        if asm_config.is_iast_request_enabled:
+            if CommandInjection.has_quota():
+                from .._taint_tracking.aspects import join_aspect
+
+                if isinstance(shell_args, (list, tuple)):
+                    for arg in shell_args:
+                        if is_pyobject_tainted(arg):
+                            report_cmdi = join_aspect(" ".join, 1, " ", shell_args)
+                            break
+                elif is_pyobject_tainted(shell_args):
+                    report_cmdi = shell_args
+
+                if report_cmdi:
+                    CommandInjection.report(evidence_value=report_cmdi)
+
+            # Reports Span Metrics
+            increment_iast_span_metric(IAST_SPAN_TAGS.TELEMETRY_EXECUTED_SINK, CommandInjection.vulnerability_type)
+            # Report Telemetry Metrics
+            _set_metric_iast_executed_sink(CommandInjection.vulnerability_type)
+    except Exception as e:
+        iast_error(f"propagation::sink_point::Error in _iast_report_ssrf. {e}")
