f9f9ff4e0fa05435f19bd10ddae15207b12e5b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:34 +0000

    Add package xpress/9.8.0/xpress-9.8.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/xpress/xpress-9.8.0-cp311-cp311-macosx_11_0_arm64.whl/xpress/examples/modeling_examples/blend2.py
@@ -0,0 +1,30 @@
+# Example for the use of the Python language (Blending problem).
+#
+# Data given in the model.
+#
+# (C) 2018-2025 Fair Isaac Corporation
+
+import xpress as xp
+
+p = xp.problem()
+
+ROres = range(2)
+REV = 125                     # Unit revenue of product.
+MINGRADE = 4                  # Min permitted grade of product.
+MAXGRADE = 5                  # Max permitted grade of product.
+
+COST = [85.00, 93.00]
+AVAIL = [60.00, 45.00]
+GRADE = [2.1, 6.3]
+
+x = [p.addVariable(ub=AVAIL[o]) for o in ROres]
+
+# Objective: maximize total profit.
+p.setObjective(xp.Sum((REV - COST[o]) * x[o] for o in ROres),
+               sense=xp.maximize)
+
+# Lower and upper bounds on ore quality.
+p.addConstraint(xp.Sum((GRADE[o] - MINGRADE) * x[o] for o in ROres) >= 0)
+p.addConstraint(xp.Sum((MAXGRADE - GRADE[o]) * x[o] for o in ROres) >= 0)
+
+p.optimize()
