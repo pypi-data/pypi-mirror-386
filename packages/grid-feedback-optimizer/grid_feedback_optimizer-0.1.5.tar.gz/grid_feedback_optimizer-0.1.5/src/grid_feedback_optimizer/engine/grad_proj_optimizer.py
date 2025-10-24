a3513a6ee544e819414879a62396198d0bf52d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 07:05:24 +0000

    Add package grid-feedback-optimizer/0.1.5/grid_feedback_optimizer-0.1.5.tar.gz

---
+++ packages/grid-feedback-optimizer/grid_feedback_optimizer-0.1.5.tar.gz/grid_feedback_optimizer-0.1.5/src/grid_feedback_optimizer/engine/grad_proj_optimizer.py
@@ -0,0 +1,163 @@
+import cvxpy as cp
+import numpy as np
+import math
+from grid_feedback_optimizer.models.network import Network
+from grid_feedback_optimizer.models.solve_data import OptimizationInputs
+
+class GradientProjectionOptimizer:
+    """
+    Gradient projection optimizer.
+    Caches the CVXPY problem to allow fast updates of parameters.
+    """
+
+    def __init__(self, network: Network, sensitivities: dict, alpha: float = 0.5, solver: str = "CLARABEL", **solver_kwargs):
+        """
+        Initialize optimizer and build cached problem.
+        """
+        self.prob, self.cons, self.obj = self._build_problem(network, sensitivities, alpha)
+        self.solver = solver
+        self.solver_kwargs = solver_kwargs
+
+
+    def _build_problem(self, network: Network, sensitivities: dict, alpha: float):
+        """
+        Build CVXPY problem with Parameters and Variables.
+        Only called once. Returns dictionary with problem and variables/parameters.
+        """    
+        n_bus = len(network.buses)
+        n_line = len(network.lines)
+        self.n_transformer = len(network.transformers)
+        n_gen = len(network.renew_gens)
+
+        # === Scaling factors ===
+        s_inv_mean = np.mean([gen.s_inv for gen in network.renew_gens])
+        self.param_scale = 10**math.floor(math.log10(s_inv_mean)) # Round down to nearest lower power of 10
+
+        # Parameters  
+        self.u_pu_meas = cp.Parameter(n_bus)
+        self.P_line_meas = cp.Parameter(n_line)
+        self.Q_line_meas = cp.Parameter(n_line)
+        if self.n_transformer >= 1:
+            self.P_transformer_meas = cp.Parameter(self.n_transformer)
+            self.Q_transformer_meas = cp.Parameter(self.n_transformer)
+        self.p_gen_last = cp.Parameter(n_gen)
+        self.q_gen_last = cp.Parameter(n_gen)
+
+        # Variables
+        # scaled value instead of values in Watt
+        self.p_gen = cp.Variable(n_gen)
+        self.q_gen = cp.Variable(n_gen)
+
+        # Parameters with given values
+        self.p_max = cp.Parameter(n_gen)
+        self.p_min = cp.Parameter(n_gen)
+        self.p_norm = cp.Parameter(n_gen)
+        self.q_norm = cp.Parameter(n_gen)
+
+        self.p_max.value = np.array([g.p_max for g in network.renew_gens])
+        self.p_min.value = np.array([g.p_min for g in network.renew_gens])
+        self.p_norm.value = np.array([g.p_norm for g in network.renew_gens])
+        self.q_norm.value = np.array([g.q_norm for g in network.renew_gens])
+
+        # Constraints
+        cons = []
+        
+        # renewable gens
+        for i in range(n_gen):
+            cons += [self.p_gen[i] <= self.p_max[i] / self.param_scale]
+            cons += [self.p_gen[i] >= self.p_min[i] / self.param_scale]
+            cons += [cp.SOC(1.0, cp.hstack([self.p_gen[i], self.q_gen[i]])/(network.renew_gens[i].s_inv / self.param_scale))]
+        
+        # voltage
+        cons += [self.u_pu_meas + sensitivities["du_dp"]@(self.p_gen * self.param_scale - self.p_gen_last) 
+                 + sensitivities["du_dq"]@(self.q_gen * self.param_scale  - self.q_gen_last) <= np.array([bus.u_pu_max for bus in network.buses])]
+        cons += [self.u_pu_meas + sensitivities["du_dp"]@(self.p_gen * self.param_scale - self.p_gen_last)
+                 + sensitivities["du_dq"]@(self.q_gen  * self.param_scale - self.q_gen_last) >= np.array([bus.u_pu_min for bus in network.buses])]
+        
+        # line
+        for l in range(n_line):
+            line = network.lines[l]
+            s_line = np.sqrt(3) * line.i_n * network.buses[line.from_bus].u_rated
+            cons += [cp.SOC(1.0, cp.hstack([
+                self.P_line_meas[l] + sensitivities["dP_line_dp"][l,:]@(self.p_gen * self.param_scale - self.p_gen_last) + sensitivities["dP_line_dq"][l,:]@(self.q_gen * self.param_scale - self.q_gen_last),
+                self.Q_line_meas[l] + sensitivities["dQ_line_dp"][l,:]@(self.p_gen * self.param_scale - self.p_gen_last) + sensitivities["dQ_line_dq"][l,:]@(self.q_gen * self.param_scale - self.q_gen_last)
+            ])/s_line)]    
+
+        # transformer
+        if self.n_transformer >= 1:
+            for t in range(self.n_transformer):
+                transformer = network.transformers[t]
+                s_transformer = transformer.sn
+                cons += [cp.SOC(1.0, cp.hstack([
+                    self.P_transformer_meas[t] + sensitivities["dP_transformer_dp"][t,:]@(self.p_gen * self.param_scale - self.p_gen_last) + sensitivities["dP_transformer_dq"][t,:]@(self.q_gen * self.param_scale - self.q_gen_last),
+                    self.Q_transformer_meas[t] + sensitivities["dQ_transformer_dp"][t,:]@(self.p_gen * self.param_scale - self.p_gen_last) + sensitivities["dQ_transformer_dq"][t,:]@(self.q_gen * self.param_scale - self.q_gen_last)
+                ])/s_transformer)]                 
+        
+        # Objective
+        grad_p = 2.0 * cp.multiply( np.array([gen.c2_p for gen in network.renew_gens]), (self.p_gen_last - self.p_norm))
+        grad_p += np.array([gen.c1_p for gen in network.renew_gens])
+        grad_q = 2.0 * cp.multiply(np.array([gen.c2_q for gen in network.renew_gens]), (self.q_gen_last - self.q_norm))
+        grad_q += np.array([gen.c1_q for gen in network.renew_gens])
+
+        obj = cp.Minimize(
+            cp.sum_squares(self.p_gen - (self.p_gen_last - alpha * grad_p) / self.param_scale) +
+            cp.sum_squares(self.q_gen - (self.q_gen_last - alpha * grad_q) / self.param_scale)
+        )
+
+        # construct problem
+        prob = cp.Problem(obj, cons)
+
+        return prob, cons, obj
+
+    def solve_problem(self, opt_input: OptimizationInputs):
+        """
+        Update CVXPY parameters using structured optimization inputs,
+        solve the cached optimization problem, and return optimized setpoints.
+
+        Parameters
+        ----------
+        opt_input : OptimizationInputs
+            Structured input model containing:
+                - u_pu_meas : np.ndarray
+                    Measured node voltages [p.u.]
+                - P_line_meas, Q_line_meas : np.ndarray
+                    Measured active/reactive line power flows
+                - p_gen_last, q_gen_last : np.ndarray
+                    Previous generator active/reactive power setpoints
+                - P_transformer_meas, Q_transformer_meas : np.ndarray, optional
+                    Measured transformer active/reactive power (if applicable)
+
+        Returns
+        -------
+        np.ndarray
+            Optimized generator setpoints of shape (n_generators, 2),
+            where each row contains [p_opt, q_opt].
+        """
+
+        # Update CVXPY parameter values
+        param_dict = opt_input.to_dict()
+        self.u_pu_meas.value = param_dict["u_pu_meas"]
+        self.P_line_meas.value = param_dict["P_line_meas"]
+        self.Q_line_meas.value = param_dict["Q_line_meas"]
+        self.p_gen_last.value = param_dict["p_gen_last"]
+        self.q_gen_last.value = param_dict["q_gen_last"]
+        if self.n_transformer >= 1:
+            self.P_transformer_meas.value = param_dict["P_transformer_meas"]
+            self.Q_transformer_meas.value = param_dict["Q_transformer_meas"]
+        
+        try:
+            self.prob.solve(solver=getattr(cp, self.solver), **self.solver_kwargs)
+        except cp.error.SolverError as e:
+            print(f"Solver error: {e}")
+            print("Returning previous generator setpoints.")
+            return np.column_stack((param_dict["p_gen_last"], param_dict["q_gen_last"]))
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            return np.column_stack((param_dict["p_gen_last"], param_dict["q_gen_last"]))
+
+        # Check solver status
+        if self.prob.status == cp.OPTIMAL:
+            return np.column_stack((self.p_gen.value, self.q_gen.value)) * self.param_scale
+        else:
+            print(f"Solver finished with status: {self.prob.status}. Returning previous generator setpoints.")
+            return np.column_stack((param_dict["p_gen_last"], param_dict["q_gen_last"]))
---
+++ packages/grid-feedback-optimizer/grid_feedback_optimizer-0.1.5.tar.gz/grid_feedback_optimizer-0.1.5/src/grid_feedback_optimizer/engine/primal_dual_optimizer.py
@@ -0,0 +1,151 @@
+from grid_feedback_optimizer.models.network import Network
+from grid_feedback_optimizer.engine.renew_gen_projection import RenewGenProjection
+from grid_feedback_optimizer.models.solve_data import OptimizationInputs
+import numpy as np
+import math
+from collections.abc import Callable
+
+class PrimalDualOptimizer:
+    """
+    A primal-dual gradient projection feedback optimizer.
+    """
+    def __init__(self, network: Network, sensitivities: dict, alpha: float = 0.5,
+                 alpha_v: float = 10.0, alpha_l: float = 10.0, alpha_t: float = 10.0,
+                 solver: str = "CLARABEL", **solver_kwargs):
+        """
+        Initialize optimizer and build cached problem.
+        """
+        self.sensitivities = sensitivities
+        self.alpha = alpha
+        self.alpha_v = alpha_v
+        self.alpha_l = alpha_l
+        self.alpha_t = alpha_t
+        self.renew_gen_proj = RenewGenProjection(solver = solver, **solver_kwargs)
+
+        # === Scaling factors ===
+        s_inv_mean = np.mean([gen.s_inv for gen in network.renew_gens])
+        self.param_scale = 10**math.floor(math.log10(s_inv_mean)) # Round down to nearest lower power of 10
+
+        # read parameters from network
+        self.n_bus = len(network.buses)
+        self.n_line = len(network.lines)
+        self.n_transformer = len(network.transformers)
+        self.n_gen = len(network.renew_gens)
+
+        self.u_pu_max = np.array([bus.u_pu_max for bus in network.buses])
+        self.u_pu_min = np.array([bus.u_pu_min for bus in network.buses])
+        self.s_line = np.sqrt(3) * np.array([line.i_n * network.buses[line.from_bus].u_rated for line in network.lines])
+
+        self.c2_p = np.array([gen.c2_p for gen in network.renew_gens])
+        self.c1_p = np.array([gen.c1_p for gen in network.renew_gens])
+        self.c2_q = np.array([gen.c2_q for gen in network.renew_gens])
+        self.c1_q = np.array([gen.c1_q for gen in network.renew_gens])
+        self.p_norm = np.array([gen.p_norm for gen in network.renew_gens])
+        self.q_norm = np.array([gen.q_norm for gen in network.renew_gens])
+        self.p_min = np.array([gen.p_min for gen in network.renew_gens])
+        self.p_max = np.array([gen.p_max for gen in network.renew_gens])
+        self.s_inv = np.array([gen.s_inv for gen in network.renew_gens])
+        
+        # initialize dual variables
+        self.dual_v_upp = np.zeros(self.n_bus)        
+        self.dual_v_low = np.zeros(self.n_bus)        
+        self.dual_line = np.zeros(self.n_line)        
+        if self.n_transformer >= 1:
+            self.dual_transformer = np.zeros(self.n_transformer)
+            self.s_transformer = np.array([transformer.sn for transformer in network.transformers])
+
+    @staticmethod
+    def calc_loading(p: np.ndarray | float, q: np.ndarray | float, s: np.ndarray | float) -> np.ndarray | float:
+        return np.sqrt(p**2 + q**2) / s
+    
+    @staticmethod
+    def calc_pf(p: np.ndarray | float, q: np.ndarray | float) -> np.ndarray | float:
+        return p / np.sqrt(p**2 + q**2)
+
+    @staticmethod
+    def calc_rpf(p: np.ndarray | float, q: np.ndarray | float) -> np.ndarray | float:
+        return q / np.sqrt(p**2 + q**2)
+
+
+    def solve_problem(self, opt_input: OptimizationInputs, grad_callback: Callable | None = None, **callback_kwargs):
+        """
+        Update parameters and implement primal-dual gradient projection.
+
+        Parameters
+        ----------
+        opt_input : OptimizationInputs
+            Structured input model containing:
+                - u_pu_meas : np.ndarray
+                    Measured node voltages [p.u.]
+                - P_line_meas, Q_line_meas : np.ndarray
+                    Measured active/reactive line power flows
+                - p_gen_last, q_gen_last : np.ndarray
+                    Previous generator active/reactive power setpoints
+                - P_transformer_meas, Q_transformer_meas : np.ndarray, optional
+                    Measured transformer active/reactive power (if applicable)
+        grad_callback: callable | None = None
+            A function which takes grad_p and grad_q and outputs grad_p and grad_q
+
+        Returns
+        -------
+        np.ndarray
+            Optimized generator setpoints of shape (n_generators, 2),
+            where each row contains [p_opt, q_opt].
+        """
+
+        # update dual variables
+        param_dict = opt_input.to_dict()
+        self.dual_v_upp += self.alpha_v * (param_dict["u_pu_meas"] - self.u_pu_max)
+        self.dual_v_upp[self.dual_v_upp < 0] = 0.0
+        self.dual_v_low += self.alpha_v * (self.u_pu_min - param_dict["u_pu_meas"])
+        self.dual_v_low[self.dual_v_low < 0] = 0.0
+        self.dual_line += self.alpha_l * (self.calc_loading(param_dict["P_line_meas"], param_dict["Q_line_meas"], self.s_line)-1.0)
+        self.dual_line[self.dual_line < 0] = 0.0
+        if self.n_transformer >= 1:
+            self.dual_transformer += self.alpha_t * (self.calc_loading(param_dict["P_transformer_meas"], param_dict["Q_transformer_meas"], self.s_transformer)-1.0)
+            self.dual_transformer[self.dual_transformer < 0] = 0.0
+
+        # update primal variables
+        grad_p = 2.0 * self.c2_p * (param_dict["p_gen_last"]/self.param_scale - self.p_norm/self.param_scale) + self.c1_p
+        grad_p += self.sensitivities["du_dp"].T @ (self.dual_v_upp - self.dual_v_low)*self.param_scale
+        grad_p += self.sensitivities["dP_line_dp"].T @ (self.dual_line * self.calc_pf(param_dict["P_line_meas"],param_dict["Q_line_meas"]) / self.s_line)*self.param_scale
+        grad_p += self.sensitivities["dQ_line_dp"].T @ (self.dual_line * self.calc_rpf(param_dict["P_line_meas"],param_dict["Q_line_meas"]) / self.s_line)*self.param_scale
+        if self.n_transformer >= 1:
+            grad_p += self.sensitivities["dP_transformer_dp"].T @ (self.dual_transformer * self.calc_pf(param_dict["P_transformer_meas"],param_dict["Q_transformer_meas"]) / self.s_transformer)*self.param_scale
+            grad_p += self.sensitivities["dQ_transformer_dp"].T @ (self.dual_transformer * self.calc_rpf(param_dict["P_transformer_meas"],param_dict["Q_transformer_meas"]) / self.s_transformer)*self.param_scale
+
+        grad_q = 2.0 * self.c2_q * (param_dict["q_gen_last"]/self.param_scale - self.q_norm/self.param_scale) + self.c1_q
+        grad_q += self.sensitivities["du_dq"].T @ (self.dual_v_upp - self.dual_v_low)*self.param_scale
+        grad_q += self.sensitivities["dP_line_dq"].T @ (self.dual_line * self.calc_pf(param_dict["P_line_meas"],param_dict["Q_line_meas"]) / self.s_line)*self.param_scale
+        grad_q += self.sensitivities["dQ_line_dq"].T @ (self.dual_line * self.calc_rpf(param_dict["P_line_meas"],param_dict["Q_line_meas"]) / self.s_line)*self.param_scale
+        if self.n_transformer >= 1:
+            grad_q += self.sensitivities["dP_transformer_dq"].T @ (self.dual_transformer * self.calc_pf(param_dict["P_transformer_meas"],param_dict["Q_transformer_meas"]) / self.s_transformer)*self.param_scale
+            grad_q += self.sensitivities["dQ_transformer_dq"].T @ (self.dual_transformer * self.calc_rpf(param_dict["P_transformer_meas"],param_dict["Q_transformer_meas"]) / self.s_transformer)*self.param_scale
+
+        # apply callback function, e.g. when including an extra gradient term from transformer-level setpoint tracking
+        if grad_callback is not None:
+            grad_p, grad_q = grad_callback(grad_p, grad_q, **callback_kwargs)
+
+        # gradient descent
+        p_target = param_dict["p_gen_last"]/self.param_scale - self.alpha * grad_p
+        q_target = param_dict["q_gen_last"]/self.param_scale - self.alpha * grad_q
+
+        # call projection
+        p_gen_opt = np.zeros(self.n_gen)
+        q_gen_opt = np.zeros(self.n_gen)
+        for i in range(self.n_gen):
+            if self.p_min[i] == 0.0: # generator
+                p_gen_opt[i], q_gen_opt[i] = self.renew_gen_proj.analytic_projection(self.p_max[i]/self.param_scale, self.s_inv[i]/self.param_scale, p_target[i], q_target[i])
+            elif self.p_max[i] == 0.0: # flex load
+                p_gen_opt[i], q_gen_opt[i] = self.renew_gen_proj.analytic_projection(-self.p_min[i]/self.param_scale, self.s_inv[i]/self.param_scale, -p_target[i], q_target[i])
+                p_gen_opt[i] *= -1.0
+            elif self.p_min[i] < 0 and self.p_max[i] > 0: # flex gen/load
+                if p_target[i] >= 0:
+                    p_gen_opt[i], q_gen_opt[i] = self.renew_gen_proj.analytic_projection(self.p_max[i]/self.param_scale, self.s_inv[i]/self.param_scale, p_target[i], q_target[i])
+                else:
+                    p_gen_opt[i], q_gen_opt[i] = self.renew_gen_proj.analytic_projection(-self.p_min[i]/self.param_scale, self.s_inv[i]/self.param_scale, -p_target[i], q_target[i])
+                    p_gen_opt[i] *= -1.0
+            else: # no easy analytic solution
+                p_gen_opt[i], q_gen_opt[i] = self.renew_gen_proj.opt_projection(self.p_max[i]/self.param_scale, self.p_min[i]/self.param_scale, self.s_inv[i]/self.param_scale, p_target[i], q_target[i])
+        
+        return self.param_scale * np.column_stack((p_gen_opt, q_gen_opt))
