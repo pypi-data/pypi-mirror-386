fd0904fcd647fad54f6eaaa22575769fb31454d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 16:07:28 +0000

    Add package ApiClientV2/1.0.1/apiclientv2-1.0.1-py3-none-any.whl

---
+++ packages/ApiClientV2/apiclientv2-1.0.1-py3-none-any.whl/api_client_v2/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from api_client_v2.api.api_key_api import ApiKeyApi
+from api_client_v2.api.api_key_model_api import ApiKeyModelApi
+from api_client_v2.api.api_key_model_verify_api import ApiKeyModelVerifyApi
+from api_client_v2.api.api_key_model_versioning_api import ApiKeyModelVersioningApi
+from api_client_v2.api.api_key_repository_api import ApiKeyRepositoryApi
+
