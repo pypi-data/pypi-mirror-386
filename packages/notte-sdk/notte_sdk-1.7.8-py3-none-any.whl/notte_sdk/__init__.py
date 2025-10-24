96d347c8354a5e8a63be19cb93f2f1e6aa285759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:28 +0000

    Add package notte-sdk/1.7.8/notte_sdk-1.7.8-py3-none-any.whl

---
+++ packages/notte-sdk/notte_sdk-1.7.8-py3-none-any.whl/notte_sdk/__init__.py
@@ -0,0 +1,11 @@
+from notte_core import check_notte_version
+
+from notte_sdk.client import NotteClient
+from notte_sdk.endpoints.agents import RemoteAgent
+from notte_sdk.endpoints.sessions import RemoteSession
+from notte_sdk.errors import retry
+from notte_sdk.utils import generate_cookies
+
+__version__ = check_notte_version("notte_sdk")
+
+__all__ = ["NotteClient", "RemoteSession", "RemoteAgent", "retry", "generate_cookies"]
