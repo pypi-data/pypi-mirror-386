db817206efdf062b92cdca500a83047205c2e7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 04:06:53 +0000

    Add package rindex/1.2.0/rindex-1.2.0-cp310-cp310-win_arm64.whl

---
+++ packages/rindex/rindex-1.2.0-cp310-cp310-win_arm64.whl/rindex/c_rindex.pyx
@@ -0,0 +1,33 @@
+# cython: freethreading_compatible = True
+
+from cython import Py_ssize_t, critical_section
+
+cdef extern from "Python.h":
+    Py_ssize_t PY_SSIZE_T_MAX
+
+
+@critical_section
+def rindex(
+    seq, value, start: Py_ssize_t = 0, stop: Py_ssize_t = PY_SSIZE_T_MAX, /
+) -> Py_ssize_t:
+    cdef Py_ssize_t size = len(seq)
+
+    if start < 0:
+        start += size
+        if start < 0:
+            start = 0
+
+    if size < stop:
+        stop = size
+    elif stop < 0:
+        stop += size
+        if stop < 0:
+            stop = 0
+
+    cdef Py_ssize_t i
+    for i in range(stop - 1, start - 1, -1):
+        elem = seq[i]
+        if elem is value or elem == value:
+            return i
+
+    raise ValueError("sequence.index(x): x not in sequence")
