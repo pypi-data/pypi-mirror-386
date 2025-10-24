0aaaec22368ad9c40d358a1623eabb2d43d210ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:40 +0000

    Add package avogadro/1.102.0/avogadro-1.102.0-cp311-cp311-win_arm64.whl

---
+++ packages/avogadro/avogadro-1.102.0-cp311-cp311-win_arm64.whl/avogadro-1.102.0.data/data/include/avogadro/quantumio/nwchemjson.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+  This source file is part of the Avogadro project.
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
+******************************************************************************/
+
+#ifndef AVOGADRO_QUANTUMIO_NWCHEMJSON_H
+#define AVOGADRO_QUANTUMIO_NWCHEMJSON_H
+
+#include "avogadroquantumioexport.h"
+#include <avogadro/io/fileformat.h>
+
+namespace Avogadro {
+namespace QuantumIO {
+
+/**
+ * @class NWChemJson nwchemjson.h <avogadro/quantumio/nwchemjson.h>
+ * @brief Implementation of the NWChem JSON format.
+ * @author Marcus D. Hanwell
+ */
+
+class AVOGADROQUANTUMIO_EXPORT NWChemJson : public Io::FileFormat
+{
+public:
+  NWChemJson();
+  ~NWChemJson() override;
+
+  Operations supportedOperations() const override
+  {
+    return Read | File | Stream | String;
+  }
+
+  FileFormat* newInstance() const override { return new NWChemJson; }
+  std::string identifier() const override { return "Avogadro: NWCHEMJSON"; }
+  std::string name() const override { return "NWChem JSON"; }
+  std::string description() const override
+  {
+    return "TODO: Describe the format.";
+  }
+
+  std::string specificationUrl() const override { return ""; }
+
+  std::vector<std::string> fileExtensions() const override;
+  std::vector<std::string> mimeTypes() const override;
+
+  bool read(std::istream& in, Core::Molecule& molecule) override;
+  bool write(std::ostream& out, const Core::Molecule& molecule) override;
+};
+
+} // namespace QuantumIO
+} // namespace Avogadro
+
+#endif // AVOGADRO_IO_NWCHEMJSON_H
---
+++ packages/avogadro/avogadro-1.102.0-cp311-cp311-win_arm64.whl/avogadro/__init__.py
@@ -0,0 +1,4 @@
+from . import cjson
+from . import connect
+from . import core
+from . import io
