f1f1ebc06c1fecebfc04ca5a54471dcabb15a5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 15:07:23 +0000

    Add package viso-sdk-python/1.1.10/viso_sdk_python-1.1.10-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/viso-sdk-python/viso_sdk_python-1.1.10-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/viso_sdk/constants/__init__.py
@@ -0,0 +1,9 @@
+from viso_sdk.constants.constants import *
+from viso_sdk.constants.variables import *
+from viso_sdk.constants.modules import *
+
+NODE_TYPE = variables.get_node_type()
+NODE_ID = variables.get_node_id()
+
+ROOT_DIR = variables.get_container_dir()
+LOG_DIR = variables.get_log_dir()
