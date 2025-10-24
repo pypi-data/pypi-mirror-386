a492728167a7e4eb99d1d6d8f516a5dd368a1b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 06:06:07 +0000

    Add package cmake/4.1.2/cmake-4.1.2.tar.gz

---
+++ packages/cmake/cmake-4.1.2.tar.gz/cmake-4.1.2/CMakeUrls.cmake
@@ -0,0 +1,29 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2.tar.gz")
+set(unix_source_sha256       "643f04182b7ba323ab31f526f785134fb79cba3188a852206ef0473fee282a15")
+
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2.zip")
+set(windows_source_sha256    "2fac01cc2cd7f432554543f750fa7ec58ab1504ca097e4417958db11d9f7a559")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256    "NA")
+
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "773cc679c3a7395413bd096523f8e5d6c39f8718af4e12eb4e4195f72f35e4ab")
+
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "ec5dce73ff03191feb46032a9ae94a3c9410343cc1fa3cbdd054bb5d8951fc78")
+
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2-windows-i386.zip")
+set(win32_binary_sha256      "5af2f262d20dbb7d3ecf1d0afa11563b8d1783ec7d05a016586e6cd6fd81e08a")
+
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2-windows-x86_64.zip")
+set(win64_binary_sha256      "82730741149681597510a4bc7095da1460b472dac24eaf9c23ad7440f47753ab")
+
+set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2-windows-arm64.zip")
+set(winarm64_binary_sha256   "396bb14c77d12615dfbd666ef6ecd67d3442eb449c321aad25d738216fd9962d")
