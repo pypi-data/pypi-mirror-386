c438831ddd8f0f809f781f0eae17cc9eb1923430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 07:05:35 +0000

    Add package pymomentum-gpu/0.1.77.post12/pymomentum_gpu-0.1.77.post12-cp313-cp313-manylinux_2_39_x86_64.whl

---
+++ packages/pymomentum-gpu/pymomentum_gpu-0.1.77.post12-cp313-cp313-manylinux_2_39_x86_64.whl/include/momentum/character_solver/vertex_projection_error_function.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <momentum/character_solver/fwd.h>
+#include <momentum/character_solver/skeleton_error_function.h>
+#include <momentum/math/fwd.h>
+
+namespace momentum {
+
+template <typename T>
+struct VertexProjectionConstraintT {
+  int vertexIndex = -1;
+  T weight = 1;
+  Eigen::Vector2<T> targetPosition;
+  Eigen::Matrix<T, 3, 4> projection; // Projection matrix
+
+  template <typename T2>
+  VertexProjectionConstraintT<T2> cast() const {
+    return {
+        this->vertexIndex,
+        (T)this->weight,
+        this->targetPosition.template cast<T2>(),
+    };
+  }
+};
+
+template <typename T>
+class VertexProjectionErrorFunctionT : public SkeletonErrorFunctionT<T> {
+ public:
+  explicit VertexProjectionErrorFunctionT(const Character& character, uint32_t maxThreads = 0);
+  ~VertexProjectionErrorFunctionT() override;
+
+  [[nodiscard]] double getError(
+      const ModelParametersT<T>& modelParameters,
+      const SkeletonStateT<T>& state) final;
+
+  double getGradient(
+      const ModelParametersT<T>& modelParameters,
+      const SkeletonStateT<T>& state,
+      Eigen::Ref<Eigen::VectorX<T>> gradient) final;
+
+  double getJacobian(
+      const ModelParametersT<T>& modelParameters,
+      const SkeletonStateT<T>& state,
+      Eigen::Ref<Eigen::MatrixX<T>> jacobian,
+      Eigen::Ref<Eigen::VectorX<T>> residual,
+      int& usedRows) final;
+
+  [[nodiscard]] size_t getJacobianSize() const final;
+
+  void addConstraint(
+      int vertexIndex,
+      T weight,
+      const Eigen::Vector2<T>& targetPosition,
+      const Eigen::Matrix<T, 3, 4>& projection);
+  void clearConstraints();
+
+  [[nodiscard]] const std::vector<VertexProjectionConstraintT<T>>& getConstraints() const {
+    return constraints_;
+  }
+
+  [[nodiscard]] const Character& getCharacter() const {
+    return character_;
+  }
+
+  [[nodiscard]] size_t numConstraints() const {
+    return constraints_.size();
+  }
+
+ private:
+  double calculateJacobian(
+      const ModelParametersT<T>& modelParameters,
+      const SkeletonStateT<T>& state,
+      const VertexProjectionConstraintT<T>& constr,
+      Ref<Eigen::MatrixX<T>> jac,
+      Ref<Eigen::VectorX<T>> res) const;
+
+  double calculateGradient(
+      const ModelParametersT<T>& modelParameters,
+      const SkeletonStateT<T>& state,
+      const VertexProjectionConstraintT<T>& constr,
+      Eigen::Ref<Eigen::VectorX<T>> gradient) const;
+
+  // Utility function used now in calculateJacobian and calculateGradient
+  // to calculate derivatives with respect to position in world space (considering skinning)
+  void calculateDWorldPos(
+      const SkeletonStateT<T>& state,
+      const VertexProjectionConstraintT<T>& constr,
+      const Eigen::Vector3<T>& d_restPos,
+      Eigen::Vector3<T>& d_worldPos) const;
+
+  void updateMeshes(const ModelParametersT<T>& modelParameters, const SkeletonStateT<T>& state);
+
+  const Character& character_;
+
+  std::vector<VertexProjectionConstraintT<T>> constraints_;
+
+  std::unique_ptr<MeshT<T>>
+      neutralMesh_; // Rest mesh without facial expression basis,
+                    // used to restore the neutral shape after facial expressions are applied.
+                    // Not used with there is a shape basis.
+  std::unique_ptr<MeshT<T>> restMesh_; // The rest positions of the mesh after shape basis
+                                       // (and potentially facial expression) has been applied
+  std::unique_ptr<MeshT<T>>
+      posedMesh_; // The posed mesh after the skeleton transforms have been applied.
+
+  uint32_t maxThreads_;
+
+  T _nearClip = 1.0f;
+};
+
+} // namespace momentum
