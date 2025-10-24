ac644fd43140e17836ee293418a61856f31be5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 16:07:41 +0000

    Add package dbt-rabbit-bigquery/1.0.2/dbt_rabbit_bigquery-1.0.2.tar.gz

---
+++ packages/dbt-rabbit-bigquery/dbt_rabbit_bigquery-1.0.2.tar.gz/dbt_rabbit_bigquery-1.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import find_namespace_packages, setup
+
+package_name = "dbt-rabbit-bigquery"
+package_version = "1.0.2"
+description = """The Rabbit BigQuery adapter plugin for dbt"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="Rabbit Team",
+    author_email="success@followrabbit.ai",
+    url="https://followrabbit.ai",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
+    include_package_data=True,
+    install_requires=[
+        "dbt-bigquery>=1.5.0,<2.0.0",
+        "rabbit-bq-job-optimizer>=0.1.12",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+)
+
