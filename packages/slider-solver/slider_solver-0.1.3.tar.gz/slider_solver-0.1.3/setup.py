df15fe52c837a598493e2e9e13ddb2c90d708a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 15:07:15 +0000

    Add package slider-solver/0.1.3/slider_solver-0.1.3.tar.gz

---
+++ packages/slider-solver/slider_solver-0.1.3.tar.gz/slider_solver-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="slider_solver",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python",
+        "numpy",
+        "Pillow"
+    ],
+    python_requires=">=3.8",
+    author="MiChen",
+    author_email="rmcyyds@q.com",
+    description="Universal Slider CAPTCHA Gap Detection",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/slider_solver",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
