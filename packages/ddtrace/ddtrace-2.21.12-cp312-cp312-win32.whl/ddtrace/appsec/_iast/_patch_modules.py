bcd02d025e55873401a5f7b038d9adbfb9083975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:01 +0000

    Add package ddtrace/2.21.12/ddtrace-2.21.12-cp312-cp312-win32.whl

---
+++ packages/ddtrace/ddtrace-2.21.12-cp312-cp312-win32.whl/ddtrace/appsec/_iast/_patch_modules.py
@@ -0,0 +1,24 @@
+from wrapt.importer import when_imported
+
+
+IAST_PATCH = {
+    "code_injection": True,
+    "command_injection": True,
+    "header_injection": True,
+    "weak_cipher": True,
+    "weak_hash": True,
+}
+
+
+def patch_iast(patch_modules=IAST_PATCH):
+    """Load IAST vulnerabilities sink points.
+
+    IAST_PATCH: list of implemented vulnerabilities
+    """
+    # TODO: Devise the correct patching strategy for IAST
+    from ddtrace._monkey import _on_import_factory
+
+    for module in (m for m, e in patch_modules.items() if e):
+        when_imported("hashlib")(_on_import_factory(module, "ddtrace.appsec._iast.taint_sinks.%s", raise_errors=False))
+
+    when_imported("json")(_on_import_factory("json_tainting", "ddtrace.appsec._iast._patches.%s", raise_errors=False))
