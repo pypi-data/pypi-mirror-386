79fba2caf49f8bec3adf923533ba99f25609b802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 06:06:27 +0000

    Add package bluer-sandbox/5.508.1/bluer_sandbox-5.508.1.tar.gz

---
+++ packages/bluer-sandbox/bluer_sandbox-5.508.1.tar.gz/bluer_sandbox-5.508.1/bluer_sandbox/bps/utils/receiver.py
@@ -0,0 +1,132 @@
+import asyncio
+from bleak import BleakScanner
+from bleak.backends.device import BLEDevice
+from bleak.backends.scanner import AdvertisementData
+import argparse
+import dataclasses
+import signal
+import struct
+
+from blueness import module
+from bluer_options import string
+from bluer_options.terminal.functions import hr
+from bluer_sandbox import NAME
+from bluer_sandbox.logger import logger
+
+NAME = module.name(__file__, NAME)
+
+
+def to_dict(obj):
+    """Safely convert a dataclass or object to a dict."""
+    if dataclasses.is_dataclass(obj):
+        return dataclasses.asdict(obj)
+
+    if hasattr(obj, "__dict__"):
+        return vars(obj)
+
+    if isinstance(obj, dict):
+        return obj
+
+    return {"repr": repr(obj)}
+
+
+async def main(
+    grep: str = "",
+    timeout: float = 10.0,
+):
+    logger.info(
+        "{}: LE Scan for {} (Ctrl+C to stop) ...".format(
+            NAME,
+            string.pretty_duration(
+                timeout,
+                short=True,
+            ),
+        )
+    )
+
+    def callback(
+        device: BLEDevice,
+        advertisement_data: AdvertisementData,
+    ):
+        if grep and (device.name is None or grep not in device.name):
+            return
+
+        logger.info(hr(width=30))
+        logger.info(f"device name: {device.name}")
+        logger.info(f"device address: {device.address}")
+
+        if advertisement_data:
+            try:
+                logger.info(f"rssi: {advertisement_data.rssi}")
+            except:
+                logger.warning("rssi not found.")
+
+            try:
+                x_, y_, z_, sigma_, tx_power = struct.unpack(
+                    "<fffff", advertisement_data.manufacturer_data[0xFFFF]
+                )
+                logger.info(
+                    ", ".join(
+                        [
+                            f"x: {x_:.2f}",
+                            f"y: {y_:.2f}",
+                            f"z: {z_:.2f}",
+                            f"sigma: {sigma_:.2f}",
+                            f"tx_power: {tx_power:.2f}",
+                        ]
+                    )
+                )
+            except:
+                logger.info(advertisement_data)
+
+    scanner = BleakScanner(detection_callback=callback)
+    await scanner.start()
+    logger.info("scanning started...")
+
+    stop_event = asyncio.Event()
+
+    def handle_sigint():
+        logger.info("Ctrl+C detected, stopping scan ...")
+        stop_event.set()
+
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT, handle_sigint)
+
+    try:
+        # wait either for timeout or Ctrl+C
+        await asyncio.wait_for(stop_event.wait(), timeout=timeout)
+    except asyncio.TimeoutError:
+        logger.info(
+            "timeout ({}) reached, stopping advertisement.".format(
+                string.pretty_duration(
+                    timeout,
+                    short=True,
+                )
+            )
+        )
+
+    await scanner.stop()
+    logger.info("scan stopped.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(NAME)
+    parser.add_argument(
+        "--grep",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "--timeout",
+        type=float,
+        default=10.0,
+        help="in seconds",
+    )
+    args = parser.parse_args()
+
+    asyncio.run(
+        main(
+            grep=args.grep,
+            timeout=args.timeout,
+        )
+    )
---
+++ packages/bluer-sandbox/bluer_sandbox-5.508.1.tar.gz/bluer_sandbox-5.508.1/bluer_sandbox/help/bps.py
@@ -0,0 +1,153 @@
+from typing import List
+
+from bluer_options.terminal import show_usage, xtra
+
+
+def help_beacon(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = xtra("~start_bluetooth,verbose", mono=mono)
+
+    args = [
+        "[--x <1.0>]",
+        "[--y <2.0>]",
+        "[--z <3.0>]",
+        "[--sigma <4.0>]",
+        "[--spacing <2.0>]",
+        "[--timeout <10.0 | -1>]",
+    ]
+
+    return show_usage(
+        [
+            "@bps",
+            "beacon",
+            f"[{options}]",
+        ]
+        + args,
+        "start beacon.",
+        mono=mono,
+    )
+
+
+def help_install(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    return show_usage(
+        [
+            "@bps",
+            "install",
+        ],
+        "install bps.",
+        mono=mono,
+    )
+
+
+def help_introspect(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = "".join(
+        [
+            xtra("~start_bluetooth,verbose,", mono=mono),
+            "unique_bus_name=<1:234>",
+        ]
+    )
+
+    return show_usage(
+        [
+            "@bps",
+            "introspect",
+            f"[{options}]",
+        ],
+        "introspect <1:234>.",
+        mono=mono,
+    )
+
+
+def help_receiver(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = xtra("~start_bluetooth,verbose", mono=mono)
+
+    args = [
+        "[--grep <sparrow>]",
+        "[--timeout <10>]",
+    ]
+
+    usage_1 = show_usage(
+        [
+            "@bps",
+            "receiver",
+            f"[{options}]",
+        ]
+        + args,
+        "start receiver.",
+        mono=mono,
+    )
+
+    # ---
+    options = xtra("~python,~start_bluetooth,verbose", mono=mono)
+
+    usage_2 = show_usage(
+        [
+            "@bps",
+            "receiver",
+            f"[{options}]",
+        ],
+        "start receiver.",
+        mono=mono,
+    )
+
+    return "\n".join(
+        [
+            usage_1,
+            usage_2,
+        ]
+    )
+
+
+def help_start_bluetooth(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = xtra("verbose", mono=mono)
+
+    return show_usage(
+        [
+            "@bps",
+            "start_bluetooth",
+            f"[{options}]",
+        ],
+        "start bluetooth.",
+        mono=mono,
+    )
+
+
+def help_test(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = xtra("~start_bluetooth,verbose", mono=mono)
+
+    return show_usage(
+        [
+            "@bps",
+            "test",
+            f"[{options}]",
+        ],
+        "d-bus ping test.",
+        mono=mono,
+    )
+
+
+help_functions = {
+    "beacon": help_beacon,
+    "install": help_install,
+    "introspect": help_introspect,
+    "receiver": help_receiver,
+    "start_bluetooth": help_start_bluetooth,
+    "test": help_test,
+}
