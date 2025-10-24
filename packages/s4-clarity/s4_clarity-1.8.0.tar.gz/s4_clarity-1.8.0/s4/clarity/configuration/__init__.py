bf8b783f33350fb7e29c458e6893cbf66186a316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:30 +0000

    Add package s4-clarity/1.8.0/s4_clarity-1.8.0.tar.gz

---
+++ packages/s4-clarity/s4_clarity-1.8.0.tar.gz/s4_clarity-1.8.0/s4/clarity/configuration/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 2016 Semaphore Solutions, Inc.
+# ---------------------------------------------------------------------------
+
+from .protocol import Protocol, StepConfiguration, ProtocolStepField
+from .workflow import Workflow
+from .process_type import ProcessType, ProcessTemplate, Automation
+from .udf import Udf
+from .stage import Stage
+from .instrument_type import InstrumentType
+
+module_members = [
+    Automation,
+    InstrumentType,
+    ProcessTemplate,
+    ProcessType,
+    Protocol,
+    ProtocolStepField,
+    Stage,
+    StepConfiguration,
+    Udf,
+    Workflow,
+]
+
+__all__ = [m.__name__ for m in module_members]
+
+# The below __module__ assignments allow Sphinx to generate links that point to this parent module.
+for member in module_members:
+    member.__module__ = "s4.clarity.configuration"
---
+++ packages/s4-clarity/s4_clarity-1.8.0.tar.gz/s4_clarity-1.8.0/setup.py
@@ -0,0 +1,64 @@
+# Copyright 2016 Semaphore Solutions, Inc.
+# ---------------------------------------------------------------------------
+import os
+
+from setuptools import setup, find_packages
+
+setup_dir = os.path.abspath(os.path.dirname(__file__))
+
+version = {}
+with open(os.path.join(setup_dir, 's4', 'clarity', 'version.py'), 'r') as f:
+    exec(f.read(), version)
+
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='s4-clarity',
+    version=version['__version__'],
+    author='Semaphore Solutions',
+    url='https://github.com/SemaphoreSolutions/s4-clarity-lib',
+    author_email='info@semaphoresolutions.ca',
+    description='A general purpose library for interacting with Clarity LIMS',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=find_packages(exclude=['*.test', '*.test.*', 'test.*', 'test']),
+    python_requires='>=2.7',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    install_requires=(
+        'requests>=2.22.0',
+        'argparse',
+        'python-dateutil',
+        'six',
+        'future',
+        "typing; python_version < '3.5'",
+        'urllib3>=1.25.2'
+    ),
+    tests_require=(
+        'mock',
+        'pytest'
+    ),
+    project_urls={
+        'Documentation': 'https://readthedocs.org/projects/s4-clarity-lib',
+        'Source': 'https://github.com/SemaphoreSolutions/s4-clarity-lib',
+    }
+)
