2494ee94a83255608f44684224fd847611b8fcc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:52 +0000

    Add package imgseries/0.12.0/imgseries-0.12.0.tar.gz

---
+++ packages/imgseries/imgseries-0.12.0.tar.gz/imgseries-0.12.0/tests/test_contour_tracking.py
@@ -0,0 +1,60 @@
+"""Tests for the imgseries module (pytest), image series analysis programs.
+
+Note: This does not test the interactive options. See example
+Jupyter notebooks for more details and interactive modes.
+"""
+
+# Standard library
+from pathlib import Path
+
+# Local imports
+import imgseries
+from imgseries import ImgSeries, ImgStack
+from imgseries import ContourTracking, ContourTrackingResults
+
+
+# =============================== Misc. config ===============================
+
+modulefolder = Path(imgseries.__file__).parent / '..'
+basefolder = modulefolder / 'examples/data/for-tests-do-not-modify'
+folders = [basefolder / '..' / folder for folder in ('img1', 'img2')]
+
+tiff_stack = Path('examples/data/stack') / 'ImgStack.tif'
+
+# ================== Test contour tracking on ImgSeries ===================
+
+images = ImgSeries(folders, savepath=basefolder)
+images.load_times('Img_Files.tsv')  # in case files have changed creation time
+
+ct = ContourTracking(images)
+ct.contours.load('Img_ContourTracking')
+
+
+def test_contour_tracking_basic():
+    ct.run()
+    assert len(ct.results.data['properties']) == 50
+
+
+def test_contour_tracking_range():
+    ct.run(start=10, skip=3)
+    assert ct.results.data['properties'].shape == (14, 15)
+
+
+def test_contour_tracking_load():
+    ctresults = ContourTrackingResults(savepath=basefolder)
+    ctresults.load('Img_ContourTracking')
+    assert round(ctresults.data['properties'].at[4, 'x3']) == 322
+
+
+# ================== Test contour tracking on image stack ===================
+
+img_stack = ImgStack(tiff_stack)
+
+ctstack = ContourTracking(img_stack, savepath=basefolder / 'stack')
+ctstack.contours.load()
+
+
+def test_contourstack_tracking_basic():
+    ctstack.run()
+    assert ctstack.img_series.data.shape == (200, 100, 112)
+    assert len(ctstack.results.data['properties']) == 200
