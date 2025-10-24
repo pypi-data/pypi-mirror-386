147ff115b0e625e53308408496e252f82bb747a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 15:06:23 +0000

    Add package devstack-cli/10.0.318/devstack_cli-10.0.318-py3-none-any.whl

---
+++ packages/devstack-cli/devstack_cli-10.0.318-py3-none-any.whl/version.py
@@ -0,0 +1,9 @@
+"""
+constants, set by build
+"""
+
+MAJOR = '10'
+BRANCH_NAME = 'release-10'
+BUILD_DATE = '2025-10-24-041217'
+SHORT_SHA = '8202cdc'
+VERSION = '10+release-10.2025-10-24-041217.8202cdc'
