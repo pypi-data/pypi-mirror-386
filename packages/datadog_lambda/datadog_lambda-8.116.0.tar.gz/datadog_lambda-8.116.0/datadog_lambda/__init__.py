bef2c38180478443b853ab7d24bd3ea4335408f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:30 +0000

    Add package datadog_lambda/8.116.0/datadog_lambda-8.116.0.tar.gz

---
+++ packages/datadog_lambda/datadog_lambda-8.116.0.tar.gz/datadog_lambda-8.116.0/datadog_lambda/__init__.py
@@ -0,0 +1,27 @@
+from datadog_lambda.cold_start import initialize_cold_start_tracing
+import os
+
+
+if os.environ.get("DD_INSTRUMENTATION_TELEMETRY_ENABLED") is None:
+    # Telemetry is required for Appsec Software Composition Analysis
+    os.environ["DD_INSTRUMENTATION_TELEMETRY_ENABLED"] = os.environ.get(
+        "DD_APPSEC_ENABLED", "false"
+    )
+
+
+initialize_cold_start_tracing()
+
+# The minor version corresponds to the Lambda layer version.
+# E.g.,, version 0.5.0 gets packaged into layer version 5.
+from datadog_lambda.version import __version__  # noqa: E402 F401
+from datadog_lambda.logger import initialize_logging  # noqa: E402
+
+
+initialize_logging(__name__)
+
+
+from datadog_lambda.patch import patch_all  # noqa: E402
+
+# Patch third-party libraries for tracing, must be done before importing any
+# handler code.
+patch_all()
