685658165caee68af48b8c2b2ec922a68a211a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 04:07:21 +0000

    Add package differt-core/0.6.2/differt_core-0.6.2-cp311-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/differt-core/differt_core-0.6.2-cp311-abi3-musllinux_1_2_x86_64.whl/differt_core/scene/_sionna.py
@@ -0,0 +1,7 @@
+__all__ = ("Material", "Shape", "SionnaScene")
+
+from differt_core import _differt_core
+
+Material = _differt_core.scene.sionna.Material
+Shape = _differt_core.scene.sionna.Shape
+SionnaScene = _differt_core.scene.sionna.SionnaScene
