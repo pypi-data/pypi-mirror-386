8acd75c0d94a4ac729878a68b8517580caebb522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 04:07:17 +0000

    Add package daniil-baruah/0.4.1/daniil_baruah-0.4.1.tar.gz

---
+++ packages/daniil-baruah/daniil_baruah-0.4.1.tar.gz/daniil_baruah-0.4.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='daniil-baruah',
+    version='0.4.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Daniil Baruah',
+    author_email="daniilbaruah76@gmail.com",
+    description='My first module',
+    python_requires='>=3.10',
+)
