59fa21719f040e4fda0169afb8fc28ce74594d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 04:07:05 +0000

    Add package secontrol/0.1.5/secontrol-0.1.5-py3-none-any.whl

---
+++ packages/secontrol/secontrol-0.1.5-py3-none-any.whl/secontrol/examples_direct_connect/__init__.py
@@ -0,0 +1,11 @@
+"""Примеры использования библиотеки secontrol."""
+
+__all__ = [
+    "list_grids",
+    "lamp_blind",
+    "toggle_device",
+    "color_grid_blocks",
+    "damage_listener",
+    "radar_telemetry",
+]
+
