c6f5781b09d30d9a6c68131e2a629793f75f24a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 15:07:12 +0000

    Add package ekuiper/0.0.1.post18767826745/ekuiper-0.0.1.post18767826745.tar.gz

---
+++ packages/ekuiper/ekuiper-0.0.1.post18767826745.tar.gz/ekuiper-0.0.1.post18767826745/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ekuiper',
+    version='0.0.1-18767826745',
+    packages=find_packages(),
+    url='https://github.com/lf-edge/ekuiper',
+    license='Apache License 2.0',
+    author='LF Edge eKuiper team',
+    author_email='huangjy@emqx.io',
+    description='Python SDK for eKuiper portable plugin',
+    install_requires=["pynng==0.8.1"],
+)
