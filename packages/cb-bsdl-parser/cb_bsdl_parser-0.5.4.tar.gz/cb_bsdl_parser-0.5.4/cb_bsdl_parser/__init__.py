8b7eeefd679933227bba508a4561a4caa117c073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:18:03 +0000

    Add package cb-bsdl-parser/0.5.4/cb_bsdl_parser-0.5.4.tar.gz

---
+++ packages/cb-bsdl-parser/cb_bsdl_parser-0.5.4.tar.gz/cb_bsdl_parser-0.5.4/cb_bsdl_parser/__init__.py
@@ -0,0 +1,4 @@
+from .CBBsdlLexer import *
+from .CBBsdlParser import *
+from .cb_bsdl import *
+from .__version__ import *
---
+++ packages/cb-bsdl-parser/cb_bsdl_parser-0.5.4.tar.gz/cb_bsdl_parser-0.5.4/cb_bsdl_parser/__version__.py
@@ -0,0 +1,6 @@
+__author__ = "Thomas, Thomas@chriesibaum.dev"
+__copyright__ = "Copyright 2025, Chriesibaum GmbH"
+__version__ = "0.5.4"
+
+def get_version():
+    return (__version__)
