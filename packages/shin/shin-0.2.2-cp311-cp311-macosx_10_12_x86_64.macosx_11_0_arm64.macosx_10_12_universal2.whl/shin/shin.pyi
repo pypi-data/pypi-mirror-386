73828ddf52bba5d956e0306ac3f7e817f0325c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:18:43 +0000

    Add package shin/0.2.2/shin-0.2.2-cp311-cp311-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl

---
+++ packages/shin/shin-0.2.2-cp311-cp311-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/shin/shin.pyi
@@ -0,0 +1,7 @@
+def optimise(
+    inverse_odds: list[float],
+    sum_inverse_odds: float,
+    n: int,
+    max_iterations: int = ...,
+    convergence_threshold: float = ...,
+) -> tuple[float, float, float]: ...
