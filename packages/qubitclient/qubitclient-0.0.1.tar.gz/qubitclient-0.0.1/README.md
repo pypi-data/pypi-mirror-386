e58bbd38034af05c94a7772ccfbd7d1ee9d49162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:08 +0000

    Add package qubitclient/0.0.1/qubitclient-0.0.1.tar.gz

---
+++ packages/qubitclient/qubitclient-0.0.1.tar.gz/qubitclient-0.0.1/README.md
@@ -0,0 +1,90 @@
+# QubitClient
+
+#### 介绍
+**qubit-client**
+QubitClient 是用于使用Qubit服务的示例。
+
+# 更新日志   
+
+近期更新:
+
+- **增加scope功能包**: 增加多种任务功能(20251022).
+- **增加曲线类型**: 增加cosin类型曲线拟合(20250606).
+- **构建基础项目:** 基础功能与结构构建.    
+
+
+# 使用
+
+### scope功能包
+#### 运行示例代码
+```python
+python demo/scope_demo.py
+```
+
+### 曲线分割功能
+#### 使用说明
+1.拷贝config.py.example文件为config.py，并修改配置参数。
+```
+cp config.py.example config.py
+```
+2.运行
+单次请求多个文件：
+```bash
+python client_demo.py
+```
+批量多次请求
+```bash
+python script/test.py
+```
+#### 定义实例
+```
+client = QubitSegClient(url=url, api_key=api_key,curve_type=CurveType.POLY)
+```
+curve_type: CurveType.COSINE(cosin拟合) or CurveType.POLY(多项式拟合)
+
+#### 请求输入
+
+```python
+response = client.request(file_list=dict_list)
+```
+dict_list格式为：
+```json
+[
+    {
+        "bias":np.ndarray shape(A),
+        "frequency":np.ndarray shape(B),
+        "iq_avg":np.ndarray shape(B,A),
+    },
+    ...
+]
+```
+
+
+#### 返回值
+返回请求为response
+```python
+res = response.json()
+```
+res格式为：
+```json
+{
+    "state":'success',
+    "result":result
+}
+```
+其中result格式：
+```json
+[
+    {
+        "params_list":List[List[float]],//每条线段的多项式参数列表
+        "linepoints_list":List[List[[row_index,col_index]]],//每条线段的点坐标列表
+        "confidence_list":List[float],//每条线段的置信度
+    },//每一个npz文件的结果
+    {
+        ...
+    },
+    ...
+]
+```
+
+
---
+++ packages/qubitclient/qubitclient-0.0.1.tar.gz/qubitclient-0.0.1/qubitclient/QubitSeg.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 yaqiang.sun.
+# This source code is licensed under the license found in the LICENSE file
+# in the root directory of this source tree.
+#########################################################################
+# Author: yaqiangsun
+# Created Time: 2025/04/15 10:36:23
+########################################################################
+
+import logging
+import numpy as np
+
+from qubitclient.utils.request_tool import file_request,file_request_with_dict
+from qubitclient.utils.result_parser import parser_result
+from qubitclient.utils.result_parser import convet_axis
+from qubitclient.curve.curve_type import CurveType
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class QubitSegClient(object):
+    def __init__(self, url, api_key,curve_type:CurveType=None):
+        self.url = url
+        self.api_key = api_key
+        self.curve_type = curve_type
+    def request(self, file_list:list[str|dict[str,np.ndarray]]):
+        if len(file_list)>0:
+            if type(file_list[0]) == str:
+                response = file_request(file_path_list=file_list,url=self.url,api_key=self.api_key,curve_type=self.curve_type)
+            elif type(file_list[0]) == dict:# 多个content字典
+                response = file_request_with_dict(dict_list=file_list,url=self.url,api_key=self.api_key,curve_type=self.curve_type)
+            else:
+                raise ValueError("file_list must be a list of str or dict")
+        else:
+            raise ValueError("file_list must not be empty")
+        return response
+    def parser_result_with_image(self,response,images):
+        if response.status_code == 200:
+            # logging.info("Result: %s", response.json())
+            result = response.json()
+            result = result["result"]
+            result_images = parser_result(result=result,images=images)
+            # for image in result_images:
+            #     cv2.imwrite("tmp/client/test.jpg",image)
+            return result_images
+        else:
+            logging.error("Error: %s %s", response.status_code, response.text)
+            return []
+    def get_result(self,response):
+        if response.status_code == 200:
+            # logging.info("Result: %s", response.json())
+            result = response.json()
+            result = result["result"]
+            return result
+        else:
+            logging.error("Error: %s %s", response.status_code, response.text)
+            return []
+    
+    def convert_axis(self,points:list[float,float],x_dim:np.ndarray,y_dim:np.ndarray):
+        return convet_axis(points=points,x_dim=x_dim,y_dim=y_dim)
+    
