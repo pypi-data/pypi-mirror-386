ca94bdec8b6f253a5c8c8b05eb48ccb8f9924477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:10 +0000

    Add package ordeq_common/2.1.0/ordeq_common-2.1.0-py3-none-any.whl

---
+++ packages/ordeq_common/ordeq_common-2.1.0-py3-none-any.whl/ordeq_common/io/printer.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Any
+
+from ordeq import Output
+
+
+@dataclass(frozen=True, eq=False)
+class Print(Output[Any]):
+    """Output that prints data on save. Mostly useful for debugging purposes.
+    The difference between other utilities like `StringBuffer` and `Pass` is
+    that `Print` shows the output of the node directly on the console.
+
+    Example:
+
+    ```pycon
+    >>> from ordeq_common import Print, Literal
+    >>> from ordeq import node, run
+    >>> @node(
+    ...     inputs=Literal("hello, world!"),
+    ...     outputs=Print()
+    ... )
+    ... def print_message(message: str) -> str:
+    ...     return message.capitalize()
+
+    >>> result = run(print_message)
+    Hello, world!
+
+    ```
+
+    """
+
+    def save(self, data: Any) -> None:
+        print(data)
