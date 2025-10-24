e8ff791eab2d40ed7264a05ec15cae3cb3c52efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 15:07:14 +0000

    Add package mblt-model-zoo/0.3.3/mblt_model_zoo-0.3.3-py3-none-any.whl

---
+++ packages/mblt-model-zoo/mblt_model_zoo-0.3.3-py3-none-any.whl/mblt_model_zoo/vision/object_detection/__init__.py
@@ -0,0 +1,19 @@
+from .yolov3 import YOLOv3u, YOLOv3_sppu, YOLOv3_spp
+from .yolov5 import (
+    YOLOv5su,
+    YOLOv5s6u,
+    YOLOv5mu,
+    YOLOv5m6u,
+    YOLOv5lu,
+    YOLOv5l6,
+    YOLOv5l6u,
+    YOLOv5xu,
+    YOLOv5x6,
+    YOLOv5x6u,
+)
+from .yolov7 import YOLOv7, YOLOv7d6, YOLOv7e6, YOLOv7e6e, YOLOv7w6, YOLOv7x
+from .yolov8 import YOLOv8s, YOLOv8m, YOLOv8l, YOLOv8x
+from .yolov9 import YOLOv9m, YOLOv9c, GELANm, GELANc
+from .yolov10 import YOLOv10b
+from .yolo11 import YOLO11s, YOLO11m, YOLO11l, YOLO11x
+from .yolo12 import YOLO12s, YOLO12m
---
+++ packages/mblt-model-zoo/mblt_model_zoo-0.3.3-py3-none-any.whl/mblt_model_zoo/vision/pose_estimation/__init__.py
@@ -0,0 +1,2 @@
+from .yolov8_pose import YOLOv8xPose
+from .yolo11_pose import YOLO11xPose
