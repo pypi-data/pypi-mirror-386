5a7e9fbef4cee201c6a13e89446b4d5f9711e361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 14:08:58 +0000

    Add package hstrat/1.20.15/hstrat-1.20.15-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/hstrat/hstrat-1.20.15-cp310-cp310-macosx_11_0_arm64.whl/hstrat/_auxiliary_lib/_alifestd_find_leaf_ids.py
@@ -0,0 +1,50 @@
+import numpy as np
+import ordered_set as ods
+import pandas as pd
+
+from ._alifestd_has_contiguous_ids import alifestd_has_contiguous_ids
+from ._alifestd_parse_ancestor_ids import alifestd_parse_ancestor_ids
+from ._alifestd_try_add_ancestor_id_col import alifestd_try_add_ancestor_id_col
+
+
+def alifestd_find_leaf_ids(phylogeny_df: pd.DataFrame) -> np.ndarray:
+    """What ids are not listed in any `ancestor_list`?
+
+    Input dataframe is not mutated by this operation.
+    """
+
+    if alifestd_has_contiguous_ids(phylogeny_df):
+        phylogeny_df = alifestd_try_add_ancestor_id_col(phylogeny_df)
+        if "ancestor_id" in phylogeny_df:
+
+            # root is self ref, but must exclude to handle only-root phylo
+            internal_node_idxs = phylogeny_df["ancestor_id"].to_numpy()[
+                phylogeny_df["ancestor_id"] != phylogeny_df["id"]
+            ]
+
+            leaf_pos_filter = np.ones(len(phylogeny_df), dtype=np.bool_)
+            leaf_pos_filter[internal_node_idxs] = False
+
+            return np.flatnonzero(leaf_pos_filter)
+
+    all_ids = ods.OrderedSet(phylogeny_df["id"])
+    internal_ids = (
+        set(
+            ancestor_id
+            for ancestor_list in phylogeny_df["ancestor_list"]
+            for ancestor_id in (
+                alifestd_parse_ancestor_ids(ancestor_list)
+                if isinstance(ancestor_list, str)
+                else ancestor_list
+            )
+        )
+        if "ancestor_id" not in phylogeny_df
+        else set(
+            # root is self ref, but must exclude to handle only-root phylo
+            phylogeny_df.loc[
+                phylogeny_df["ancestor_id"] != phylogeny_df["id"],
+                "ancestor_id",
+            ]
+        )
+    )
+    return np.fromiter(all_ids - internal_ids, dtype=int)
