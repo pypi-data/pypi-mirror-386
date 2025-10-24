b313c3af0add1ee6f66f7fee7c58158c9b981ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 15:06:25 +0000

    Add package devstack-cli/11.0.237/devstack_cli-11.0.237.tar.gz

---
+++ packages/devstack-cli/devstack_cli-11.0.237.tar.gz/devstack_cli-11.0.237/src/version.py
@@ -0,0 +1,9 @@
+"""
+constants, set by build
+"""
+
+MAJOR = '11'
+BRANCH_NAME = 'release-11'
+BUILD_DATE = '2025-10-24-025352'
+SHORT_SHA = '000400e'
+VERSION = '11+release-11.2025-10-24-025352.000400e'
