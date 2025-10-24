817969fa49521654c5a4da13356430321c18b16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:06:29 +0000

    Add package myprog-lib/0.1.0/myprog_lib-0.1.0.tar.gz

---
+++ packages/myprog-lib/myprog_lib-0.1.0.tar.gz/myprog_lib-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myprog_lib",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Deepak M",
+    description="A simple library for program name lookup",
+    python_requires=">=3.6",
+)
