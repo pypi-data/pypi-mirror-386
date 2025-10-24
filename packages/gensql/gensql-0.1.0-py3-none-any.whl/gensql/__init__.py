ac9663b230ec33a07621159e04a4e1b05dc95d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 07:05:46 +0000

    Add package gensql/0.1.0/gensql-0.1.0-py3-none-any.whl

---
+++ packages/gensql/gensql-0.1.0-py3-none-any.whl/gensql/__init__.py
@@ -0,0 +1,42 @@
+import subprocess
+import sys
+import os
+from importlib import resources
+
+from py4j.java_gateway import JavaGateway
+
+__gateway_server = None
+__gateway = None
+
+def start_server():
+    global __gateway
+    global __gateway_server
+    if __gateway_server == None:
+        with resources.path(__package__, "gateway.jar") as gateway_jar:
+            __gateway_server = subprocess.Popen(
+                ["java", "-jar", gateway_jar],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                bufsize=1,
+                universal_newlines=True,
+            )
+        wait_text="Running..."
+        for line in __gateway_server.stdout:
+            print(line)
+            if wait_text in line:
+                break
+        __gateway = JavaGateway()
+
+def slurpDB(path):
+    start_server()
+    p = os.path.abspath(path)
+    return __gateway.entry_point.slurpDB(p)
+
+def query(text, db):
+    data = __gateway.entry_point.query(text, db)
+    return [dict(x) for x in data]
+
+def queryStrict(text, db)
+    data = __gateway.entry_point.queryStrict(text, db)
+    return [dict(x) for x in data]
