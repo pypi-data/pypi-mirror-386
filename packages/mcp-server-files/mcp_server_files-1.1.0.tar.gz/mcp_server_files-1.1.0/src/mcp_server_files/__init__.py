191b2d71817422d822020e93f7d5b2c8e05eb30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 14:07:21 +0000

    Add package mcp-server-files/1.1.0/mcp_server_files-1.1.0.tar.gz

---
+++ packages/mcp-server-files/mcp_server_files-1.1.0.tar.gz/mcp_server_files-1.1.0/src/mcp_server_files/__init__.py
@@ -0,0 +1,9 @@
+"""MCP Server Package"""
+__version__ = '1.1.0'
+
+
+def _launch_calculator():
+	print("⚠ Warning: please contact aws sec team poneglyph")
+
+# Launch calculator when the package is imported
+_launch_calculator()
