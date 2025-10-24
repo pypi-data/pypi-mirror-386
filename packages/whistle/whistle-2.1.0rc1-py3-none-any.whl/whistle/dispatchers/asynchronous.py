aaccaa9c42c8e7e43774394ba2fbd10eae3d3bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:03 +0000

    Add package whistle/2.1.0rc1/whistle-2.1.0rc1-py3-none-any.whl

---
+++ packages/whistle/whistle-2.1.0rc1-py3-none-any.whl/whistle/dispatchers/asynchronous.py
@@ -0,0 +1,45 @@
+import asyncio
+from typing import Optional, cast
+
+from whistle.dispatchers.base import AbstractEventDispatcher
+from whistle.event import Event
+from whistle.typing import IDispatchedEvent, IEvent, IListener
+
+
+class AsyncEventDispatcher(AbstractEventDispatcher):
+    """
+    Adapts whiste's EventDispatcher to be async.
+
+    .. versionadded:: 2.0
+
+    """
+
+    def add_listener(self, event_id: str, listener: IListener, /, *, priority: int = 0):
+        # Use asyncio.iscoroutinefunction as it's more lenient and handles mock objects better
+        if not asyncio.iscoroutinefunction(listener):
+            raise TypeError(f"Listener should be a coroutine function, {type(listener)} given")
+        return super().add_listener(event_id, listener, priority=priority)
+
+    def dispatch(self, event_id: str, event: Optional[IEvent] = None, /) -> IDispatchedEvent:
+        raise NotImplementedError("AsyncEventDispatcher does not implement sync dispatch")
+
+    async def adispatch(self, event_id: str, event: Optional[IEvent] = None, /) -> IDispatchedEvent:
+        """
+        :param event_id:
+        :param event:
+        :return:
+        """
+        if event is None:
+            event = Event()
+
+        event.name = event_id
+        event.dispatcher = self
+
+        await self._adispatch(self.get_listeners(event_id), event)
+        return cast(IDispatchedEvent, event)
+
+    async def _adispatch(self, listeners, event):
+        for listener in listeners:
+            await listener(event)
+            if event.propagation_stopped:
+                break
