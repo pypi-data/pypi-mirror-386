da282daec5921fc9bc65ec916c2ce8a5787a5c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 05:05:44 +0000

    Add package avogadro/1.102.0/avogadro-1.102.0-cp314-cp314t-macosx_11_0_arm64.whl

---
+++ packages/avogadro/avogadro-1.102.0-cp314-cp314t-macosx_11_0_arm64.whl/avogadro-1.102.0.data/data/include/avogadro/core/ringperceiver.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+  This source file is part of the Avogadro project.
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
+******************************************************************************/
+
+#ifndef AVOGADRO_CORE_RINGPERCEIVER_H
+#define AVOGADRO_CORE_RINGPERCEIVER_H
+
+#include "avogadrocoreexport.h"
+
+#include <cstddef>
+#include <vector>
+
+namespace Avogadro::Core {
+
+class Molecule;
+
+class AVOGADROCORE_EXPORT RingPerceiver
+{
+public:
+  // construction and destruction
+  explicit RingPerceiver(const Molecule* m = nullptr);
+  ~RingPerceiver() = default;
+
+  // properties
+  void setMolecule(const Molecule* m);
+  const Molecule* molecule() const;
+
+  // ring perception
+  std::vector<std::vector<size_t>>& rings();
+
+private:
+  bool m_ringsPerceived;
+  const Molecule* m_molecule;
+  std::vector<std::vector<size_t>> m_rings;
+};
+
+} // namespace Avogadro::Core
+
+#endif // AVOGADRO_CORE_RINGPERCEIVER_H
