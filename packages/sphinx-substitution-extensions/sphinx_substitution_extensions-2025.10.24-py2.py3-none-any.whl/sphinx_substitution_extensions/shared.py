d34cf0f47aa9538ae7d25158ab882b9ded1dadb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:06:49 +0000

    Add package sphinx-substitution-extensions/2025.10.24/sphinx_substitution_extensions-2025.10.24-py2.py3-none-any.whl

---
+++ packages/sphinx-substitution-extensions/sphinx_substitution_extensions-2025.10.24-py2.py3-none-any.whl/sphinx_substitution_extensions/shared.py
@@ -0,0 +1,8 @@
+"""
+Constants and functions shared between modules.
+"""
+
+# This is hardcoded in doc8 as a valid option so be wary that changing this
+# may break doc8 linting.
+# See https://github.com/PyCQA/doc8/pull/34.
+SUBSTITUTION_OPTION_NAME = "substitutions"
