2230f75e870c19d049f88282d48b4b6312dc675b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 14:08:41 +0000

    Add package duper-python/0.2.0/duper_python-0.2.0-cp312-cp312-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/duper-python/duper_python-0.2.0-cp312-cp312-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/duper/__init__.py
@@ -0,0 +1,24 @@
+r"""Utilities for converting to and from Python types into the Duper format.
+
+:mod:`duper` exposes an API similar to :mod:`json` and :mod:`pickle`, except
+that custom Pydantic ``BaseModel``s are returned.."""
+
+from ._duper import (
+    dumps,
+    dump,
+    loads,
+    load,
+    Duper,
+    DuperType,
+)
+from .pydantic import BaseModel
+
+__all__ = [
+    "dumps",
+    "dump",
+    "loads",
+    "load",
+    "Duper",
+    "DuperType",
+    "BaseModel",
+]
