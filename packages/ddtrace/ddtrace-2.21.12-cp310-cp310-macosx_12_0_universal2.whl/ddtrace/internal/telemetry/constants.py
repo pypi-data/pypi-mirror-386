7100d22aef5268883c1198e5b3648480d4f6c692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:06:51 +0000

    Add package ddtrace/2.21.12/ddtrace-2.21.12-cp310-cp310-macosx_12_0_universal2.whl

---
+++ packages/ddtrace/ddtrace-2.21.12-cp310-cp310-macosx_12_0_universal2.whl/ddtrace/internal/telemetry/constants.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TELEMETRY_NAMESPACE(Enum):
+    TRACERS = "tracers"
+    APPSEC = "appsec"
+    IAST = "iast"
+    CIVISIBILITY = "civisibility"
+    MLOBS = "mlobs"
+
+
+TELEMETRY_TYPE_GENERATE_METRICS = "generate-metrics"
+TELEMETRY_TYPE_DISTRIBUTION = "distributions"
+TELEMETRY_TYPE_LOGS = "logs"
+
+
+class TELEMETRY_LOG_LEVEL(Enum):
+    DEBUG = "DEBUG"
+    WARNING = "WARN"
+    ERROR = "ERROR"
+
+
+class TELEMETRY_APM_PRODUCT(str, Enum):
+    LLMOBS = "mlobs"
+    DYNAMIC_INSTRUMENTATION = "dynamic_instrumentation"
+    PROFILER = "profiler"
+    APPSEC = "appsec"
