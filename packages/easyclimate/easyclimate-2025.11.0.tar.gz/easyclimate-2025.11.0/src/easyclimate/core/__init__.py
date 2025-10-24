1cb73a9b0d4e39d4253e23901fffe1fa87401869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:18:16 +0000

    Add package easyclimate/2025.11.0/easyclimate-2025.11.0.tar.gz

---
+++ packages/easyclimate/easyclimate-2025.11.0.tar.gz/easyclimate-2025.11.0/src/easyclimate/core/__init__.py
@@ -0,0 +1,16 @@
+from .diff import *
+from .eddy import *
+from .extract import *
+from .read import *
+from .stat import *
+from .variability import *
+from .yearstat import *
+from .tutorial import *
+from .datanode import *
+
+from . import utility
+from . import mk_test
+from . import eof
+from . import windspharm
+from . import spharm
+from . import normalized
