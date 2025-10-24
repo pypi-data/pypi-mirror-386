0c86e322e5a2c80fd32cd4cc821b8ddcab1b967f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 04:07:16 +0000

    Add package anilibria-api-client/0.2.1/anilibria_api_client-0.2.1-py3-none-any.whl

---
+++ packages/anilibria-api-client/anilibria_api_client-0.2.1-py3-none-any.whl/anilibria_api_client/__init__.py
@@ -0,0 +1,29 @@
+from .api_client import AsyncAnilibriaAPI
+
+from .types import (
+    CollectionType,
+    ContentType,
+    AgeRating,
+    Seasons,
+    SortType,
+    PublishStatusesType,
+    ProductionStatusesType
+)
+
+from .models import (
+    TimeCode,
+    Release,
+    ReleaseCollection
+)
+
+from .exceptions import (
+    AnilibriaException,
+    AnilibriaValidationException
+)
+
+from .helper import (
+    async_download,
+    auth,
+    download_torrent_file,
+    auto_paginate
+)
