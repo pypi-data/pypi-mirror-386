5aa0003e70d05bef3b5f14512e5d220524bc4978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 03:19:12 +0000

    Add package pybibtexer/0.2.7/pybibtexer-0.2.7-py3-none-any.whl

---
+++ packages/pybibtexer/pybibtexer-0.2.7-py3-none-any.whl/pybibtexer/bib/bibtexparser/middlewares/block/entry_fields_delete.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+
+from ...library import Library
+from ...model import Block, Entry
+from ..middleware import BlockMiddleware
+
+
+class DeleteFieldsInEntry(BlockMiddleware):
+    """Delete fields by user."""
+
+    def __init__(
+        self, delete_field_keys: List[str], entry_type: Optional[str] = None, allow_inplace_modification: bool = True
+    ):
+        super().__init__(allow_inplace_modification=allow_inplace_modification, allow_parallel_execution=True)
+
+        self.entry_type = entry_type
+        self.delete_field_keys = delete_field_keys
+
+    # docstr-coverage: inherited
+    def transform_entry(self, entry: Entry, library: Library) -> Block:
+        if self.entry_type is None:
+            entry_type = entry.entry_type
+        else:
+            entry_type = self.entry_type
+
+        if entry_type == entry.entry_type:
+            for key in self.delete_field_keys:
+                del entry[key]
+        return entry
+
+    # docstr-coverage: inherited
+    @classmethod
+    def metadata_key(cls) -> str:
+        return "delete_custom_fields"
