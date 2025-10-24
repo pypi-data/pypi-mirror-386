bd0b2110ca9fb4c1deddbe36608ce0c05c4acba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 14:08:34 +0000

    Add package pierre-storage/0.2.0/pierre_storage-0.2.0-py3-none-any.whl

---
+++ packages/pierre-storage/pierre_storage-0.2.0-py3-none-any.whl/pierre_storage/__init__.py
@@ -0,0 +1,66 @@
+"""Pierre Git Storage SDK for Python.
+
+A Python SDK for interacting with Pierre's git storage system.
+"""
+
+from pierre_storage.client import GitStorage, create_client
+from pierre_storage.errors import ApiError, RefUpdateError
+from pierre_storage.types import (
+    BranchInfo,
+    CommitInfo,
+    CommitResult,
+    CommitSignature,
+    DiffFileState,
+    DiffStats,
+    FileDiff,
+    FilteredFile,
+    GetBranchDiffResult,
+    GetCommitDiffResult,
+    GitStorageOptions,
+    ListBranchesResult,
+    ListCommitsResult,
+    ListFilesResult,
+    RefUpdate,
+    Repo,
+    RestoreCommitResult,
+)
+from pierre_storage.webhook import (
+    WebhookPushEvent,
+    parse_signature_header,
+    validate_webhook,
+    validate_webhook_signature,
+)
+
+__version__ = "0.1.4"
+
+__all__ = [
+    # Main client
+    "GitStorage",
+    "create_client",
+    # Errors
+    "ApiError",
+    "RefUpdateError",
+    # Types
+    "BranchInfo",
+    "CommitInfo",
+    "CommitResult",
+    "CommitSignature",
+    "DiffFileState",
+    "DiffStats",
+    "FileDiff",
+    "FilteredFile",
+    "GetBranchDiffResult",
+    "GetCommitDiffResult",
+    "GitStorageOptions",
+    "ListBranchesResult",
+    "ListCommitsResult",
+    "ListFilesResult",
+    "RefUpdate",
+    "Repo",
+    "RestoreCommitResult",
+    # Webhook
+    "WebhookPushEvent",
+    "parse_signature_header",
+    "validate_webhook",
+    "validate_webhook_signature",
+]
