1207bf3413eefb39c34d2ade93e4b92c17d9c2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 07:06:19 +0000

    Add package lakeshore-tempcontrol/0.16.13/lakeshore_tempcontrol-0.16.13-py3-none-any.whl

---
+++ packages/lakeshore-tempcontrol/lakeshore_tempcontrol-0.16.13-py3-none-any.whl/lakeshore_tempcontrol/cgse_explore.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "show_processes",
+]
+
+
+import re
+
+from egse.process import ProcessInfo
+from egse.process import get_processes
+
+
+def show_processes():
+    """Returns of list of ProcessInfo data classes for matching processes from this package."""
+
+    def filter_procs(pi: ProcessInfo):
+        pattern = r"(lakeshore336)_(ui|cs|sim)"
+
+        return re.search(pattern, pi.command)
+
+    return get_processes(filter_procs)
