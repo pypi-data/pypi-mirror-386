2ffc56a7a9c69c704ee30f71cb72acfb3ce85db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 17:07:20 +0000

    Add package gllm-inference-binary/0.5.52/gllm_inference_binary-0.5.52-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.52-cp312-cp312-manylinux_2_31_x86_64.whl/gllm_inference/lm_invoker/schema/google.pyi
@@ -0,0 +1,24 @@
+class Key:
+    """Defines valid keys in Google."""
+    ARGS: str
+    CONTENT: str
+    FINISH_MESSAGE: str
+    FINISH_REASON: str
+    FUNCTION: str
+    FUNCTION_CALL: str
+    HTTP_OPTIONS: str
+    NAME: str
+    RETRY_OPTIONS: str
+    SYSTEM_INSTRUCTION: str
+    THINKING_CONFIG: str
+    TIMEOUT: str
+    TOOLS: str
+    RESPONSE_SCHEMA: str
+    RESPONSE_MIME_TYPE: str
+    VERTEXAI: str
+
+class InputType:
+    """Defines valid input types in Google."""
+    APPLICATION_JSON: str
+    MODEL: str
+    USER: str
