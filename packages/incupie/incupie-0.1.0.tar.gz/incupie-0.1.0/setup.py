0de611821351457b07bfd6eb3f8ddb7781ceb35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-24 08:07:25 +0000

    Add package incupie/0.1.0/incupie-0.1.0.tar.gz

---
+++ packages/incupie/incupie-0.1.0.tar.gz/incupie-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="incupie",
+    version='0.1.0',
+    package=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
