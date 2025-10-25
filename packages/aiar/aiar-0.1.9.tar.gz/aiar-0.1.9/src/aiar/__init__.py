df7eac4639f15f96a1869e0ccce307f2986440e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:07:03 +0000

    Add package aiar/0.1.9/aiar-0.1.9.tar.gz

---
+++ packages/aiar/aiar-0.1.9.tar.gz/aiar-0.1.9/src/aiar/__init__.py
@@ -0,0 +1,13 @@
+from .aiar_base import _main, get_gitignore_spec, find_git_root, find_files_to_archive, create_aiar, extract_aiar
+
+
+__all__ = [
+    "_main",
+    "get_gitignore_spec",
+    "find_git_root",
+    "find_files_to_archive",
+    "create_aiar",
+    "extract_aiar",
+]
+
+__version__ = "0.1.9"
