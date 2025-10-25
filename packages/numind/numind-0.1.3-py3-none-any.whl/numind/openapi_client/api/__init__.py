610e32d1a8e3390c5f379f96489171f964de8f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:06:45 +0000

    Add package numind/0.1.3/numind-0.1.3-py3-none-any.whl

---
+++ packages/numind/numind-0.1.3-py3-none-any.whl/numind/openapi_client/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from numind.openapi_client.api.documents_api import DocumentsApi
+from numind.openapi_client.api.examples_api import ExamplesApi
+from numind.openapi_client.api.extraction_api import ExtractionApi
+from numind.openapi_client.api.files_api import FilesApi
+from numind.openapi_client.api.inference_api import InferenceApi
+from numind.openapi_client.api.jobs_api import JobsApi
+from numind.openapi_client.api.organizations_api import OrganizationsApi
+from numind.openapi_client.api.playground_api import PlaygroundApi
+from numind.openapi_client.api.project_management_api import ProjectManagementApi
+from numind.openapi_client.api.default_api import DefaultApi
