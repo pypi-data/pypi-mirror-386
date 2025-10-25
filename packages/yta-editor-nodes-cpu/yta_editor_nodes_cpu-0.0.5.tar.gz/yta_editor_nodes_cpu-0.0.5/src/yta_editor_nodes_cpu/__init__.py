ca7b65ecf27ebd5f73bcc4e725e7d919bd722465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:21:03 +0000

    Add package yta-editor-nodes-cpu/0.0.5/yta_editor_nodes_cpu-0.0.5.tar.gz

---
+++ packages/yta-editor-nodes-cpu/yta_editor_nodes_cpu-0.0.5.tar.gz/yta_editor_nodes_cpu-0.0.5/src/yta_editor_nodes_cpu/__init__.py
@@ -0,0 +1,13 @@
+"""
+This is the nodes module, in which we have
+all the classes that make the concept work.
+
+This module is specific to process with CPU.
+
+Note for the developer:
+- Make sure you use 'f1' dtype with [0, 255]
+values (np.uint8) for the inputs, using the
+float precission sometimes but always reutrning
+those inputs for the textures are those are 
+the ones we can handle with our OpenGL system.
+"""
