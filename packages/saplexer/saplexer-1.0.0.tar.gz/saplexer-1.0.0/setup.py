605da54f99eff0f3bf89172da26f3c3cbcb3b012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:20:59 +0000

    Add package saplexer/1.0.0/saplexer-1.0.0.tar.gz

---
+++ packages/saplexer/saplexer-1.0.0.tar.gz/saplexer-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saplexer",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A simple package that displays all text files from folders A and B.",
+    author="Jeevakrishna.V",
+    license="MIT",
+    python_requires=">=3.7",
+)
