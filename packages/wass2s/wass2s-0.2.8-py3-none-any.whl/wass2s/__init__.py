b64a1cfc506847212663257a990b717523219852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:41 +0000

    Add package wass2s/0.2.8/wass2s-0.2.8-py3-none-any.whl

---
+++ packages/wass2s/wass2s-0.2.8-py3-none-any.whl/wass2s/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.2.0"
+
+from wass2s.was_verification import *
+from wass2s.was_analog import *
+from wass2s.was_cca import *
+from wass2s.was_compute_predictand import *
+from wass2s.was_download import *
+from wass2s.was_eof import *
+from wass2s.was_linear_models import *
+from wass2s.was_machine_learning import *
+from wass2s.was_merge_predictand import *
+from wass2s.was_mme import *
+from wass2s.was_pcr import *
+from wass2s.utils import *
+from wass2s.was_cross_validate import *
+from wass2s.was_transformdata import *
+from wass2s.was_bias_correction import *
+# from wass2s.was_hydro_processing import *
+
