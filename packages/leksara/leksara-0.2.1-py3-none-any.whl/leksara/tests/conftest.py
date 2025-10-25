8201ca83b2db3672d4bac18aa7b4a70f11621b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:57 +0000

    Add package leksara/0.2.1/leksara-0.2.1-py3-none-any.whl

---
+++ packages/leksara/leksara-0.2.1-py3-none-any.whl/leksara/tests/conftest.py
@@ -0,0 +1,7 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+PACKAGE_ROOT = ROOT.parent
+if str(PACKAGE_ROOT) not in sys.path:
+    sys.path.insert(0, str(PACKAGE_ROOT))
