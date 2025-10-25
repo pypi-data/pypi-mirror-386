f59727007dd2a95f85f6d706b09b4e97bd12268e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 07:06:44 +0000

    Add package anfis-toolbox/0.1.0/anfis_toolbox-0.1.0.tar.gz

---
+++ packages/anfis-toolbox/anfis_toolbox-0.1.0.tar.gz/anfis_toolbox-0.1.0/anfis_toolbox/__init__.py
@@ -0,0 +1,9 @@
+"""ANFIS Toolbox - A Python toolbox for Adaptive Neuro-Fuzzy Inference Systems."""
+
+__version__ = "0.1.0"
+
+# Expose high-level estimators
+from .classifier import ANFISClassifier
+from .regressor import ANFISRegressor
+
+__all__ = ["ANFISClassifier", "ANFISRegressor"]
