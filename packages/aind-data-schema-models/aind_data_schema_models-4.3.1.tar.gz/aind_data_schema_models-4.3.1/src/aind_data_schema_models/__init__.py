2a8fa41fd8b4a1a59dab9152c82da5dd791fac2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:19 +0000

    Add package aind-data-schema-models/4.3.1/aind_data_schema_models-4.3.1.tar.gz

---
+++ packages/aind-data-schema-models/aind_data_schema_models-4.3.1.tar.gz/aind_data_schema_models-4.3.1/src/aind_data_schema_models/__init__.py
@@ -0,0 +1,3 @@
+"""Init package"""
+
+__version__ = "4.3.1"
---
+++ packages/aind-data-schema-models/aind_data_schema_models-4.3.1.tar.gz/aind_data_schema_models-4.3.1/src/aind_data_schema_models/registries.py
@@ -0,0 +1,16 @@
+"""Registries"""
+
+from enum import Enum
+
+
+class Registry(str, Enum):
+    """Registries"""
+
+    ADDGENE = "Addgene (ADDGENE)"
+    EMAPA = "Edinburgh Mouse Atlas Project (EMAPA)"
+    MGI = "Mouse Genome Informatics (MGI)"
+    NCBI = "National Center for Biotechnology Information (NCBI)"
+    ORCID = "Open Researcher and Contributor ID (ORCID)"
+    ROR = "Research Organization Registry (ROR)"
+    RRID = "Research Resource Identifiers (RRID)"
+    UNIPROT = "Universal Protein Resource (UNIPROT)"
