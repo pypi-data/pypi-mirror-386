b28cfa9e541656b01b781189fc82450c7f074894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:06:57 +0000

    Add package flowlens-mcp-server/0.2.6/flowlens_mcp_server-0.2.6-py3-none-any.whl

---
+++ packages/flowlens-mcp-server/flowlens_mcp_server-0.2.6-py3-none-any.whl/flowlens_mcp_server/utils/settings.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from pydantic_settings import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    flowlens_url: str = "https://flowlens-api.magentic.ai"
+    flowlens_max_string_length: int = 50
+    flowlens_save_dir_path: str = "./magentic_flowlens_mcp_data/"
+    flowlens_api_token: Optional[str] = None
+    flowlens_agent_name: Optional[str] = "unknown_agent"
+    flowlens_mcp_version: str = "0.2.6"
+    flowlens_session_uuid: str = "unknown_session"
+    
+
+
+settings = AppSettings()
