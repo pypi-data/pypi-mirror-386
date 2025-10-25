a8bd1f2ee7dd3f820025368d452a602916aa1c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 07:06:44 +0000

    Add package unit-sdk/1.1.0/unit_sdk-1.1.0-py3-none-any.whl

---
+++ packages/unit-sdk/unit_sdk-1.1.0-py3-none-any.whl/unit_sdk/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 PageKey Solutions, LLC
+
+from ._version import version
+from .lib import hello
+
+__version__ = version
+
+
+__all__ = [
+    "__version__",
+    "hello",
+    "goodbye",
+]
---
+++ packages/unit-sdk/unit_sdk-1.1.0-py3-none-any.whl/unit_sdk/lib.py
@@ -0,0 +1,7 @@
+# Copyright 2025 PageKey Solutions, LLC
+
+def hello():
+    print("Hello world!")
+
+def goodbye():
+    print("Have a nice day!")
