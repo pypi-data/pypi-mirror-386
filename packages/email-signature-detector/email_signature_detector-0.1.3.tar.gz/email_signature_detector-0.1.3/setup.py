963b5aeab79c14cc01e44948134cc02ca1e9b15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:07:11 +0000

    Add package email-signature-detector/0.1.3/email_signature_detector-0.1.3.tar.gz

---
+++ packages/email-signature-detector/email_signature_detector-0.1.3.tar.gz/email_signature_detector-0.1.3/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='email_signature_detector',
+    version='0.1.3',
+    packages=find_packages(where='src', include=['email_signature_detector*']),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy',
+        'onnxruntime',
+        'transformers',
+        'torch',
+    ],
+    author='Numeo AI Team',
+    author_email='team@numeo.ai',
+    description='A library to predict email signatures using an ONNX model.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/numeo-ai/email-signature-detector',
+    include_package_data=True,
+    package_data={
+        'email_signature_detector': ['model/tokenizer/*'],
+    },
+)
