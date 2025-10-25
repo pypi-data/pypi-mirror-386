d258ffaf82a274499520c58b37621ac7e05b6cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:06 +0000

    Add package xvideos-api/1.8/xvideos_api-1.8.tar.gz

---
+++ packages/xvideos-api/xvideos_api-1.8.tar.gz/xvideos_api-1.8/xvideos_api/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Client", "Video", "Pornstar", "sorting",
+           "errors", "consts"]
+
+
+from xvideos_api.xvideos_api import Client, Video, Pornstar
+from xvideos_api.modules import sorting, errors, consts
