ecee33032cc12b784b8883986f17e4575b8c87ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:07:01 +0000

    Add package github-backup-supabase/1.0.2/github_backup_supabase-1.0.2.tar.gz

---
+++ packages/github-backup-supabase/github_backup_supabase-1.0.2.tar.gz/github_backup_supabase-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="github-backup-supabase",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "requests",
+        "supabase",
+        "python-dotenv"
+    ],
+    entry_points={
+        "console_scripts": [
+            "github-backup=github_backup_supabase.cli:cli",
+        ],
+    },
+)
