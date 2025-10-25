f21f86b81d21f9c3b22077b1d3bc2e387312349f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 04:07:43 +0000

    Add package pyromark/0.9.7/pyromark-0.9.7-cp314-cp314t-win_amd64.whl

---
+++ packages/pyromark/pyromark-0.9.7-cp314-cp314t-win_amd64.whl/pyromark/_pyromark.pyi
@@ -0,0 +1,34 @@
+from typing import Final
+
+from typing_extensions import final
+
+from pyromark._options import Options
+from pyromark.event import Event, Range
+
+__version__: Final[str]
+
+def events(
+    markdown: str,
+    /,
+    *,
+    options: Options = Options(0),  # noqa: B008, PYI011
+    merge_text: bool = True,
+) -> tuple[Event, ...]: ...
+def events_with_range(
+    markdown: str,
+    /,
+    *,
+    options: Options = Options(0),  # noqa: B008, PYI011
+) -> tuple[tuple[Event, Range], ...]: ...
+def html(markdown: str, /, *, options: Options = Options(0)) -> str: ...  # noqa: B008, PYI011
+
+@final
+class Markdown:
+    def __init__(self, *, options: Options = Options(0)) -> None: ...  # noqa: B008, PYI011
+    def events(
+        self, markdown: str, /, *, merge_text: bool = True
+    ) -> tuple[Event, ...]: ...
+    def events_with_range(
+        self, markdown: str, /
+    ) -> tuple[tuple[Event, Range], ...]: ...
+    def html(self, markdown: str, /) -> str: ...
