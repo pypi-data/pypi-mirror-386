a397adcf21daff6b7ba93f8c1b418248fab04599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:06:30 +0000

    Add package nidaqmx/1.3.0/nidaqmx-1.3.0-py3-none-any.whl

---
+++ packages/nidaqmx/nidaqmx-1.3.0-py3-none-any.whl/nidaqmx/__init__.py
@@ -0,0 +1,31 @@
+"""The NI-DAQmx API for Python."""
+
+from nidaqmx.errors import (
+    DaqError,
+    DaqReadError,
+    DaqResourceWarning,
+    DaqWarning,
+    DaqWriteError,
+)
+from nidaqmx.grpc_session_options import *  # noqa: F403 - 'from nidaqmx.grpc_session_options import *' used; unable to detect undefined names (auto-generated noqa)
+from nidaqmx.scale import Scale
+from nidaqmx.task import Task
+from nidaqmx.types import CtrFreq, CtrTick, CtrTime
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # type: ignore[no-redef]
+
+__version__ = version(__name__)
+
+__all__ = [  # noqa: F405 - 'errors' may be undefined, or defined from star imports: nidaqmx.grpc_session_options (auto-generated noqa)
+    "errors",
+    "scale",
+    "stream_readers",
+    "stream_writers",
+    "task",
+]
+
+# Do not add a null logging handler. If the application has not configured logging, the
+# default behavior is to log warnings and errors to stderr.
