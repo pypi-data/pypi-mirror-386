efde3f7f2293824b402b61470cef0c368ebda464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 04:07:50 +0000

    Add package traceback-with-variables/2.2.1/traceback_with_variables-2.2.1.tar.gz

---
+++ packages/traceback-with-variables/traceback_with_variables-2.2.1.tar.gz/traceback_with_variables-2.2.1/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+from traceback_with_variables import __version__
+
+with open('README.md', 'r', encoding='utf-8') as in_:
+    long_description = in_.read()
+
+setuptools.setup(
+    name='traceback_with_variables',
+    version=__version__,
+    author='Andrey Lyashko',
+    author_email='andydashlandy@gmail.com',
+    description='Adds variables to python traceback. Simple, lightweight, cont'
+    'rollable. Debug reasons of exceptions by logging or pretty printing color'
+    'ful variable contexts for each frame in a stacktrace, showing every value'
+    '. Dump locals environments after errors to console, files, and loggers. W'
+    'orks with Jupyter and IPython.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/andy-landy/traceback_with_variables',
+    packages=['traceback_with_variables'],
+    keywords=['python', 'traceback', 'locals', 'logging', 'debugging', 'print',
+              'variables', 'python3', 'stacktrace', 'arguments', 'errors',
+              'error-handling', 'dump', 'exception-handling', 'exceptions',
+              'pretty', 'pretty-print', 'frame', 'simple',
+              'colors', 'jupyter', 'jupyter-notebook', 'ipython', 'customize'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries',
+    ],
+    entry_points={'console_scripts': [
+        'traceback-with-variables = traceback_with_variables.main:main',
+    ]},
+    extras_require={
+        "test": [
+            'flake8',
+            'ipython',
+            'pytest-cov',
+            'notebook==6.2.0',
+        ],
+    },
+    install_requires=[],
+    python_requires='>=3.7',
+)
