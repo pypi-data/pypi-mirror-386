969e6bb76785cdebe9946211c8511e73b1403033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:31 +0000

    Add package microservice-chassis-grupo2/0.1.6/microservice_chassis_grupo2-0.1.6-py3-none-any.whl

---
+++ packages/microservice-chassis-grupo2/microservice_chassis_grupo2-0.1.6-py3-none-any.whl/microservice_chassis_grupo2/core/rabbitmq_core.py
@@ -0,0 +1,19 @@
+from aio_pika import connect_robust, ExchangeType
+from microservice_chassis_grupo2.core.config import settings
+
+PUBLIC_KEY_PATH = "/home/pyuser/code/auth_public.pem"
+
+async def get_channel():
+    connection = await connect_robust(settings.RABBITMQ_HOST)
+    channel = await connection.channel()
+    
+    return connection, channel
+
+async def declare_exchange(channel):
+    exchange = await channel.declare_exchange(
+        settings.EXCHANGE_NAME,
+        ExchangeType.TOPIC,
+        durable=True
+    )
+
+    return exchange
