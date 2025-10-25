a22839fc026ee46d89f964761552a632385a0647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 04:07:14 +0000

    Add package sparv-sbx-sentiment-kb-sent/0.4.1/sparv_sbx_sentiment_kb_sent-0.4.1-py3-none-any.whl

---
+++ packages/sparv-sbx-sentiment-kb-sent/sparv_sbx_sentiment_kb_sent-0.4.1-py3-none-any.whl/sbx_sentiment_kb_sent/__init__.py
@@ -0,0 +1,19 @@
+"""Sparv plugin for annotating sentences with sentiment analysis."""
+
+from sparv import api as sparv_api  # type: ignore [import-untyped]
+
+from sbx_sentiment_kb_sent.annotations import annotate_sentence
+from sbx_sentiment_kb_sent.constants import PROJECT_NAME
+
+__all__ = ["annotate_sentence"]
+
+__description__ = "Annotate sentence with sentiment analysis."
+__version__ = "0.4.1"
+
+__config__ = [
+    sparv_api.Config(
+        f"{PROJECT_NAME}.num_decimals",
+        description="The number of decimals to round the score to",
+        default=3,
+    ),
+]
