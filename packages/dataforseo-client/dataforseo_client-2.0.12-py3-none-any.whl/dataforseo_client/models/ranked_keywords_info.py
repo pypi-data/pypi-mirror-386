a6f621fb8f2a567d6b85c3ba0bd84c729de13af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 04:07:50 +0000

    Add package dataforseo-client/2.0.12/dataforseo_client-2.0.12-py3-none-any.whl

---
+++ packages/dataforseo-client/dataforseo_client-2.0.12-py3-none-any.whl/dataforseo_client/models/ranked_keywords_info.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+import pprint
+import re  # noqa: F401
+import json
+
+from pydantic import BaseModel, ConfigDict, Field, StrictFloat, StrictInt, StrictStr, StrictBool
+from typing import Any, ClassVar, Dict, List, Optional, Union
+from typing import Optional, Set, Any, Dict, List
+from typing_extensions import Self
+
+from dataforseo_client.models.dataforseo_labs_available_filters_google_info import DataforseoLabsAvailableFiltersGoogleInfo
+from dataforseo_client.models.dataforseo_labs_available_filters_amazon_info import DataforseoLabsAvailableFiltersAmazonInfo
+
+
+
+class RankedKeywordsInfo(BaseModel):
+    """
+    RankedKeywordsInfo
+    """ # noqa: E501
+    google: Optional[DataforseoLabsAvailableFiltersGoogleInfo] = Field(default=None, description=r"")
+    bing: Optional[Dict[str, Optional[StrictStr]]] = Field(default=None, description=r"")
+    amazon: Optional[DataforseoLabsAvailableFiltersAmazonInfo] = Field(default=None, description=r"")
+    __properties: ClassVar[List[str]] = [
+        "google", 
+        "bing", 
+        "amazon", 
+        ]
+
+    additional_properties: Dict[str, Any] = Field(default_factory=dict)
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        validate_assignment=True,
+        protected_namespaces=(),
+    )
+
+    def to_str(self) -> str:
+        return pprint.pformat(self.model_dump(by_alias=True))
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+    @classmethod
+    def from_json(cls, json_str: str) -> Optional[Self]:
+        return cls.from_dict(json.loads(json_str))
+
+    def to_dict(self) -> Dict[str, Any]:
+        excluded_fields: Set[str] = set([
+        ])
+
+        _dict = {}
+
+        _dict['google'] = self.google.to_dict() if self.google else None
+        _dict['bing'] = self.bing
+        _dict['amazon'] = self.amazon.to_dict() if self.amazon else None
+        return _dict
+
+
+    @classmethod
+    def from_dict(cls, obj: Optional[Dict[str, Any]]) -> Optional[Self]:
+        if obj is None:
+            return None
+
+        if not isinstance(obj, dict):
+            return cls.model_validate(obj)
+
+        _obj = cls.model_validate({
+            "google": DataforseoLabsAvailableFiltersGoogleInfo.from_dict(obj["google"]) if obj.get("google") is not None else None,
+            "bing": obj.get("bing"),
+            "amazon": DataforseoLabsAvailableFiltersAmazonInfo.from_dict(obj["amazon"]) if obj.get("amazon") is not None else None,
+        })
+
+        additional_properties = {k: v for k, v in obj.items() if k not in cls.__properties}
+        _obj.additional_properties = additional_properties
+        return _obj
