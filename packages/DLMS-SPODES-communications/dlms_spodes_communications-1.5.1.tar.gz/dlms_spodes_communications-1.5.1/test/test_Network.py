74ab44d75445f4566add9469b1403e5fbc532e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:20:56 +0000

    Add package DLMS-SPODES-communications/1.5.1/dlms_spodes_communications-1.5.1.tar.gz

---
+++ packages/DLMS-SPODES-communications/dlms_spodes_communications-1.5.1.tar.gz/dlms_spodes_communications-1.5.1/test/test_Network.py
@@ -0,0 +1,30 @@
+import asyncio
+import unittest
+from src.DLMS_SPODES_communications.network import Network
+from .functools2 import open_close
+
+
+class TestType(unittest.TestCase):
+    def setUp(self) -> None:
+        self.m = Network(
+            host="178.170.223.217",
+            port="8888",
+            to_connection=2
+        )
+        print(repr(self.m))
+
+    def test_open_close(self) -> None:
+        asyncio.run(open_close(self.m))
+
+    def test_Network(self) -> None:
+        async def main(m: Network) -> None:
+            await m.open()
+            print(F"{m.is_open()=}")
+            data = bytes.fromhex("7E A0 07 03 21 93 0F 01 7E")
+            await m.send(data)
+            buf = bytearray()
+            await m.receive(buf)
+            print(F"{buf.hex(' ')=}")
+            await m.close()
+
+        asyncio.run(main(self.m))
