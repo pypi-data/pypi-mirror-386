3ff218bc8c1c861a5e040a23b9ea5245fada3f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:07:22 +0000

    Add package aind-behavior-curriculum/0.0.36/aind_behavior_curriculum-0.0.36-py3-none-any.whl

---
+++ packages/aind-behavior-curriculum/aind_behavior_curriculum-0.0.36-py3-none-any.whl/aind_behavior_curriculum/base.py
@@ -0,0 +1,45 @@
+"""
+Base behavior pydantic object
+"""
+
+from pydantic import BaseModel, ConfigDict
+
+
+class AindBehaviorModel(BaseModel):
+    """
+    Defines Pydantic configurations applied to all behavior models.
+    BaseModel: Validate arguments on initialization.
+    Configurations:
+
+    - **extra**: 'forbid'
+        Do not allow a model to be initialized with undocumented parameters.
+    - **validate_assignment**: True
+        Revalidate fields against schema on any change to model instance.
+    - **validate_default**: True
+        Validate default fields on subclasses.
+    - **strict**: True
+        Enforce strict typing.
+    """
+
+    model_config = ConfigDict(
+        extra="forbid",  # Potentially change to 'ignore'
+        validate_assignment=True,
+        validate_default=True,
+        strict=True,
+        str_strip_whitespace=True,
+    )
+
+
+class AindBehaviorModelExtra(BaseModel):
+    """
+    Same as AindBehaviorModel w/ extra = "allow".
+    Helpful for deserialization of nested subclasses.
+    """
+
+    model_config = ConfigDict(
+        extra="allow",
+        validate_assignment=True,
+        validate_default=True,
+        strict=True,
+        str_strip_whitespace=True,
+    )
