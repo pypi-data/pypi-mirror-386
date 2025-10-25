2877f6ec590b77c911eb2bb69c7b6056d980a519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:06:59 +0000

    Add package py-irclib/0.8.0/py_irclib-0.8.0-py3-none-any.whl

---
+++ packages/py-irclib/py_irclib-0.8.0-py3-none-any.whl/irclib/util/compare.py
@@ -0,0 +1,18 @@
+"""IRC string comparison utilities."""
+
+import re
+from typing import Final
+
+__all__ = ("match_mask",)
+
+GLOB_MAP: Final = {"?": ".", "*": ".*"}
+
+
+def match_mask(mask: str, pattern: str) -> bool:
+    """Match hostmask patterns in the standard banmask syntax (eg '*!*@host')."""
+    re_pattern = ""
+    for c in pattern:
+        re_pattern += GLOB_MAP.get(c, re.escape(c))
+
+    regex = re.compile(f"^{re_pattern}$")
+    return bool(regex.match(mask))
