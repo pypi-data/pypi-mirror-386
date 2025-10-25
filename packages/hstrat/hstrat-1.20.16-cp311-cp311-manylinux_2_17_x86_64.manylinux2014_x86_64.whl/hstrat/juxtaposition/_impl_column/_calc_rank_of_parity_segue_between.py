6295e74c1c7c56b38a50660fd2e3eb138fa3b24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:21:05 +0000

    Add package hstrat/1.20.16/hstrat-1.20.16-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/hstrat/hstrat-1.20.16-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/hstrat/juxtaposition/_impl_column/_calc_rank_of_parity_segue_between.py
@@ -0,0 +1,39 @@
+import typing
+
+from ..._auxiliary_lib import HereditaryStratigraphicArtifact
+from ._calc_rank_of_first_retained_disparity_between import (
+    calc_rank_of_first_retained_disparity_between,
+)
+from ._calc_rank_of_last_retained_commonality_between import (
+    calc_rank_of_last_retained_commonality_between,
+)
+
+
+def calc_rank_of_parity_segue_between(
+    first: HereditaryStratigraphicArtifact,
+    second: HereditaryStratigraphicArtifact,
+    confidence_level_commonality: float,
+    confidence_level_disparity: float,
+) -> typing.Tuple[typing.Optional[int], typing.Optional[int]]:
+    """Find last matching and first mismatching strata between columns.
+
+    Implementation detail. Searches forward from index 0 for first mismatching
+    retained differentiae between columns.
+    """
+
+    return (
+        (
+            calc_rank_of_last_retained_commonality_between(
+                first, second, confidence_level_commonality
+            )
+            if confidence_level_commonality is not None
+            else None
+        ),
+        (
+            calc_rank_of_first_retained_disparity_between(
+                first, second, confidence_level_disparity
+            )
+            if confidence_level_disparity is not None
+            else None
+        ),
+    )
