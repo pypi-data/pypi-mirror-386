668d5ef23ca36875391bc3fc4439be005351b17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:36 +0000

    Add package minify-html-onepass/0.18.0/minify_html_onepass-0.18.0.tar.gz

---
+++ packages/minify-html-onepass/minify_html_onepass-0.18.0.tar.gz/minify_html_onepass-0.18.0/minify-html-onepass/src/unit/instruction.rs
@@ -0,0 +1,19 @@
+use crate::err::ProcessingResult;
+use crate::proc::MatchAction::*;
+use crate::proc::MatchMode::*;
+use crate::proc::Processor;
+use aho_corasick::AhoCorasick;
+use aho_corasick::AhoCorasickBuilder;
+use once_cell::sync::Lazy;
+
+static INSTRUCTION_END: Lazy<AhoCorasick> =
+  Lazy::new(|| AhoCorasickBuilder::new().build(["?>"]).unwrap());
+
+#[inline(always)]
+pub fn process_instruction(proc: &mut Processor) -> ProcessingResult<()> {
+  proc.m(IsSeq(b"<?"), Keep).expect();
+  proc
+    .m(ThroughSeq(&INSTRUCTION_END), Keep)
+    .require("instruction end")?;
+  Ok(())
+}
