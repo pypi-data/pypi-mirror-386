4d813c8be87c57c53c9241b1a70fd949b2a96704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:21:05 +0000

    Add package hstrat/1.20.16/hstrat-1.20.16-cp310-cp310-musllinux_1_2_i686.whl

---
+++ packages/hstrat/hstrat-1.20.16-cp310-cp310-musllinux_1_2_i686.whl/hstrat/genome_instrumentation/__init__.pyi
@@ -0,0 +1,28 @@
+"""Data structures to annotate genomes with."""
+
+from _HereditaryStratigraphicColumn import HereditaryStratigraphicColumn
+from _HereditaryStratigraphicColumnBundle import (
+    HereditaryStratigraphicColumnBundle,
+)
+from _HereditaryStratigraphicSurface import HereditaryStratigraphicSurface
+from _HereditaryStratum import HereditaryStratum
+from stratum_ordered_stores import (
+    HereditaryStratumOrderedStoreDict,
+    HereditaryStratumOrderedStoreList,
+    HereditaryStratumOrderedStoreTree,
+    provided_stratum_ordered_stores,
+)
+
+from . import stratum_ordered_stores
+
+__all__ = [
+    "stratum_ordered_stores",
+    "HereditaryStratum",
+    "HereditaryStratigraphicColumn",
+    "HereditaryStratigraphicColumnBundle",
+    "HereditaryStratigraphicSurface",
+    "HereditaryStratumOrderedStoreDict",
+    "HereditaryStratumOrderedStoreList",
+    "HereditaryStratumOrderedStoreTree",
+    "provided_stratum_ordered_stores",
+]
---
+++ packages/hstrat/hstrat-1.20.16-cp310-cp310-musllinux_1_2_i686.whl/hstrat/phylogenetic_inference/pairwise/__init__.py
@@ -0,0 +1,7 @@
+"""Functions to infer phylogenetic history between two extant hstrat
+columns."""
+
+from ..._auxiliary_lib import lazy_attach_stub
+
+__getattr__, __dir__, __all__ = lazy_attach_stub(__name__, __file__)
+del lazy_attach_stub
