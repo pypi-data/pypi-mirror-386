7fb31197207f30aceb72ab87de188c2979cd5df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:57 +0000

    Add package aenet-gpr/2.8.3/aenet_gpr-2.8.3.tar.gz

---
+++ packages/aenet-gpr/aenet_gpr-2.8.3.tar.gz/aenet_gpr-2.8.3/aenet_gpr/src/__init__.py
@@ -0,0 +1,12 @@
+from .gpr_iterative import GaussianProcess
+from .gpr_batch import GaussianProcess, numerical_descriptor_gradient_parallel
+# from .gpr_batch_internal import GaussianProcessInternal
+from .pytorch_kernel import FPKernel, FPKernelNoforces
+# from .pytorch_kernel_internal import FPKernelInternal
+from .pytorch_kerneltypes import SquaredExp
+from .calculator import GPRCalculator
+from .prior import ConstantPrior
+
+__all__ = ["gpr_iterative", "gpr_batch", "gpr_batch_internal", "pytorch_kernel", "pytorch_kernel_internal", "pytorch_kerneltypes", "calculator", "prior",
+           "GaussianProcess", "numerical_descriptor_gradient_parallel", "FPKernel", "FPKernelNoforces", "SquaredExp", "GPRCalculator", "ConstantPrior",
+           ]
