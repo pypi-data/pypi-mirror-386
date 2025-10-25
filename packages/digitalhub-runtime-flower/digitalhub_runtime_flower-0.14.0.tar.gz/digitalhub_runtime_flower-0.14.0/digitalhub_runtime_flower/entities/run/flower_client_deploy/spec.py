c38e7c5f506eec7c12587ecf2525d8e9e9c1ded6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:35 +0000

    Add package digitalhub-runtime-flower/0.14.0/digitalhub_runtime_flower-0.14.0.tar.gz

---
+++ packages/digitalhub-runtime-flower/digitalhub_runtime_flower-0.14.0.tar.gz/digitalhub_runtime_flower-0.14.0/digitalhub_runtime_flower/entities/run/flower_client_deploy/spec.py
@@ -0,0 +1,99 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from typing import Optional
+
+from digitalhub_runtime_flower.entities.run._base.spec import RunSpecFlowerRun, RunValidatorFlowerRun
+
+
+class RunSpecFlowerClientDeploy(RunSpecFlowerRun):
+    """
+    RunSpecFlowerClientDeploy specifications.
+    """
+
+    def __init__(
+        self,
+        task: str,
+        local_execution: bool = False,
+        function: str | None = None,
+        workflow: str | None = None,
+        node_selector: list[dict] | None = None,
+        volumes: list[dict] | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list[dict] | None = None,
+        envs: list[dict] | None = None,
+        secrets: list[str] | None = None,
+        profile: str | None = None,
+        runtime_class: str | None = None,
+        priority_class: str | None = None,
+        image: str | None = None,
+        base_image: str | None = None,
+        requirements: list[str] | None = None,
+        superlink: str | None = None,
+        node_config: dict | None = None,
+        root_certificates: str | None = None,
+        private_key_secret: str | None = None,
+        public_key_secret: str | None = None,
+        isolation: str | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            task=task,
+            local_execution=local_execution,
+            function=function,
+            workflow=workflow,
+            node_selector=node_selector,
+            volumes=volumes,
+            resources=resources,
+            affinity=affinity,
+            tolerations=tolerations,
+            envs=envs,
+            secrets=secrets,
+            profile=profile,
+            runtime_class=runtime_class,
+            priority_class=priority_class,
+            **kwargs,
+        )
+        self.image = image
+        self.base_image = base_image
+        self.requirements = requirements
+        self.superlink = superlink
+        self.node_config = node_config
+        self.root_certificates = root_certificates
+        self.private_key_secret = private_key_secret
+        self.public_key_secret = public_key_secret
+        self.isolation = isolation
+
+
+class RunValidatorFlowerClientDeploy(RunValidatorFlowerRun):
+    """
+    RunValidatorFlowerClientDeploy validator.
+    """
+
+    # Function parameters
+    image: Optional[str] = None
+    base_image: Optional[str] = None
+    requirements: Optional[list[str]] = None
+
+    # Run parameters
+    superlink: Optional[str] = None
+    """Flower superlink."""
+
+    node_config: Optional[dict] = None
+    """Node configuration."""
+
+    root_certificates: Optional[str] = None
+    """Root certificates."""
+
+    private_key_secret: Optional[str] = None
+    """Private key secret name."""
+
+    public_key_secret: Optional[str] = None
+    """Public key string formatted as public key PEM."""
+
+    isolation: Optional[str] = None
+    """Whether to use process or subprocess isolation."""
---
+++ packages/digitalhub-runtime-flower/digitalhub_runtime_flower-0.14.0.tar.gz/digitalhub_runtime_flower-0.14.0/digitalhub_runtime_flower/entities/task/flower_server_build/spec.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
+# SPDX-License-Identifier: Apache-2.0
+from __future__ import annotations
+
+from typing import Optional
+
+from digitalhub.entities.task._base.spec import TaskSpecFunction, TaskValidatorFunction
+
+
+class TaskSpecFlowerServerBuild(TaskSpecFunction):
+    """
+    TaskSpecFlowerServerBuild specifications.
+    """
+
+    def __init__(
+        self,
+        function: str,
+        node_selector: list[dict] | None = None,
+        volumes: list[dict] | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list[dict] | None = None,
+        envs: list[dict] | None = None,
+        secrets: list[str] | None = None,
+        profile: str | None = None,
+        runtime_class: str | None = None,
+        priority_class: str | None = None,
+        instructions: list[str] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            function=function,
+            node_selector=node_selector,
+            volumes=volumes,
+            resources=resources,
+            affinity=affinity,
+            tolerations=tolerations,
+            envs=envs,
+            secrets=secrets,
+            profile=profile,
+            runtime_class=runtime_class,
+            priority_class=priority_class,
+            **kwargs,
+        )
+        self.instructions = instructions
+
+
+class TaskValidatorFlowerServerBuild(TaskValidatorFunction):
+    """
+    TaskValidatorFlowerServerBuild validator.
+    """
+
+    instructions: Optional[list[str]] = None
