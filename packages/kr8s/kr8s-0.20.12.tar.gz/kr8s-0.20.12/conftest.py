0d02a8ed7dee8bd1d7b2bedd74c906a70a2dcee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:21:13 +0000

    Add package kr8s/0.20.12/kr8s-0.20.12.tar.gz

---
+++ packages/kr8s/kr8s-0.20.12.tar.gz/kr8s-0.20.12/conftest.py
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2025, Kr8s Developers (See LICENSE for list)
+# SPDX-License-Identifier: BSD 3-Clause License
+import gc
+import os
+import time
+from collections.abc import Generator
+
+import pytest
+from pytest_kind.cluster import KindCluster
+
+
+@pytest.fixture
+def ensure_gc():
+    """Ensure garbage collection is run before and after the test."""
+    gc.collect()
+    yield
+    gc.collect()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def k8s_cluster(request) -> Generator[KindCluster, None, None]:
+    image = None
+    if version := os.environ.get("KUBERNETES_VERSION"):
+        image = f"kindest/node:v{version}"
+
+    kind_cluster = KindCluster(
+        name="pytest-kind",
+        image=image,
+    )
+    kind_cluster.create()
+    os.environ["KUBECONFIG"] = str(kind_cluster.kubeconfig_path)
+    # CI fix, wait for default service account to be created before continuing
+    while True:
+        try:
+            kind_cluster.kubectl("get", "serviceaccount", "default")
+            break
+        except Exception:
+            time.sleep(1)
+    yield kind_cluster
+    del os.environ["KUBECONFIG"]
+    if not request.config.getoption("keep_cluster"):  # pragma: no cover
+        kind_cluster.delete()
