14f5b2609347701093f80600fe12b3f2efb8281f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:20:59 +0000

    Add package flt/2025.2.1/flt-2025.2.1-cp314-cp314t-macosx_11_0_arm64.whl

---
+++ packages/flt/flt-2025.2.1-cp314-cp314t-macosx_11_0_arm64.whl/flt/__init__.py
@@ -0,0 +1,21 @@
+"""
+Fast Legendre Transform
+"""
+
+__all__ = [
+    "dlt",
+    "idlt",
+    "theta",
+    "dct2dlt",
+    "dlt2dct",
+    "quadrature",
+]
+
+from flt.generic import (
+    dlt,
+    idlt,
+    theta,
+    dct2dlt,
+    dlt2dct,
+    quadrature,
+)
