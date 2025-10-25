47f17c6d1a2bcfb30822f3dda84a1ebaaf4edab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:58 +0000

    Add package aiar/0.1.8/aiar-0.1.8-py3-none-any.whl

---
+++ packages/aiar/aiar-0.1.8-py3-none-any.whl/aiar/__init__.py
@@ -0,0 +1,11 @@
+from .aiar_base import _main, get_gitignore_spec, find_git_root, find_files_to_archive, create_aiar, extract_aiar
+
+
+__all__ = [
+    "_main",
+    "get_gitignore_spec",
+    "find_git_root",
+    "find_files_to_archive",
+    "create_aiar",
+    "extract_aiar",
+]
