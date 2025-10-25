eda2c3c4d517227fb332cec65dc2eed64de77090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:57 +0000

    Add package curies/0.12.2/curies-0.12.2.tar.gz

---
+++ packages/curies/curies-0.12.2.tar.gz/curies-0.12.2/src/curies/version.py
@@ -0,0 +1,13 @@
+"""Version information for :mod:`curies`."""
+
+__all__ = [
+    "VERSION",
+    "get_version",
+]
+
+VERSION = "0.12.2"
+
+
+def get_version() -> str:
+    """Get the :mod:`curies` version string."""
+    return VERSION
