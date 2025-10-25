a4677749c7dc36ae87639437b97c2258e9e845be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:06 +0000

    Add package ascii-magic/2.6.0/ascii_magic-2.6.0.tar.gz

---
+++ packages/ascii-magic/ascii_magic-2.6.0.tar.gz/ascii_magic-2.6.0/ascii_magic/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from ascii_magic.ascii_art import AsciiArt
+from ascii_magic.constants import Front, Back, CHARS_BY_DENSITY
+from ascii_magic.functions import (
+    quick_test,
+    from_image,
+    from_pillow_image,
+    from_url,
+    from_clipboard,
+    from_gemini,
+)
