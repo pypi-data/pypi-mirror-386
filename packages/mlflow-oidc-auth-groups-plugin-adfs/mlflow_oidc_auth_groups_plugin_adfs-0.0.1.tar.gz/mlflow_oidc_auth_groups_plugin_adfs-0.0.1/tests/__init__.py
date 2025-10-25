5271a62db17f90b15e7a98d2530a210b59310d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:20:59 +0000

    Add package mlflow-oidc-auth-groups-plugin-adfs/0.0.1/mlflow_oidc_auth_groups_plugin_adfs-0.0.1.tar.gz

---
+++ packages/mlflow-oidc-auth-groups-plugin-adfs/mlflow_oidc_auth_groups_plugin_adfs-0.0.1.tar.gz/mlflow_oidc_auth_groups_plugin_adfs-0.0.1/tests/__init__.py
@@ -0,0 +1,7 @@
+"""Tests suite for `mlflow_oidc_auth_groups_plugin_adfs`."""
+
+from pathlib import Path
+
+TESTS_DIR = Path(__file__).parent
+TMP_DIR = TESTS_DIR / "tmp"
+FIXTURES_DIR = TESTS_DIR / "fixtures"
