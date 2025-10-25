9f8ee827e034a9bdeab31caf4f5769f85133d2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:07:20 +0000

    Add package pymomentum-cpu/0.1.78.post11/pymomentum_cpu-0.1.78.post11-cp313-cp313-win_amd64.whl

---
+++ packages/pymomentum-cpu/pymomentum_cpu-0.1.78.post11-cp313-cp313-win_amd64.whl/lib/cmake/momentum/momentumTargets-release.cmake
@@ -0,0 +1,259 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "momentum::common" for configuration "Release"
+set_property(TARGET momentum::common APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::common PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_common.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::common )
+list(APPEND _cmake_import_check_files_for_momentum::common "${_IMPORT_PREFIX}/lib/momentum_common.lib" )
+
+# Import target "momentum::online_qr" for configuration "Release"
+set_property(TARGET momentum::online_qr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::online_qr PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_online_qr.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::online_qr )
+list(APPEND _cmake_import_check_files_for_momentum::online_qr "${_IMPORT_PREFIX}/lib/momentum_online_qr.lib" )
+
+# Import target "momentum::math" for configuration "Release"
+set_property(TARGET momentum::math APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::math PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_math.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::math )
+list(APPEND _cmake_import_check_files_for_momentum::math "${_IMPORT_PREFIX}/lib/momentum_math.lib" )
+
+# Import target "momentum::simd_generalized_loss" for configuration "Release"
+set_property(TARGET momentum::simd_generalized_loss APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::simd_generalized_loss PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_simd_generalized_loss.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::simd_generalized_loss )
+list(APPEND _cmake_import_check_files_for_momentum::simd_generalized_loss "${_IMPORT_PREFIX}/lib/momentum_simd_generalized_loss.lib" )
+
+# Import target "momentum::skeleton" for configuration "Release"
+set_property(TARGET momentum::skeleton APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::skeleton PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_skeleton.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::skeleton )
+list(APPEND _cmake_import_check_files_for_momentum::skeleton "${_IMPORT_PREFIX}/lib/momentum_skeleton.lib" )
+
+# Import target "momentum::character" for configuration "Release"
+set_property(TARGET momentum::character APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::character PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_character.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::character )
+list(APPEND _cmake_import_check_files_for_momentum::character "${_IMPORT_PREFIX}/lib/momentum_character.lib" )
+
+# Import target "momentum::solver" for configuration "Release"
+set_property(TARGET momentum::solver APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::solver PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_solver.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::solver )
+list(APPEND _cmake_import_check_files_for_momentum::solver "${_IMPORT_PREFIX}/lib/momentum_solver.lib" )
+
+# Import target "momentum::character_solver" for configuration "Release"
+set_property(TARGET momentum::character_solver APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::character_solver PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_character_solver.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::character_solver )
+list(APPEND _cmake_import_check_files_for_momentum::character_solver "${_IMPORT_PREFIX}/lib/momentum_character_solver.lib" )
+
+# Import target "momentum::simd_constraints" for configuration "Release"
+set_property(TARGET momentum::simd_constraints APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::simd_constraints PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_simd_constraints.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::simd_constraints )
+list(APPEND _cmake_import_check_files_for_momentum::simd_constraints "${_IMPORT_PREFIX}/lib/momentum_simd_constraints.lib" )
+
+# Import target "momentum::character_sequence_solver" for configuration "Release"
+set_property(TARGET momentum::character_sequence_solver APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::character_sequence_solver PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_character_sequence_solver.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::character_sequence_solver )
+list(APPEND _cmake_import_check_files_for_momentum::character_sequence_solver "${_IMPORT_PREFIX}/lib/momentum_character_sequence_solver.lib" )
+
+# Import target "momentum::diff_ik" for configuration "Release"
+set_property(TARGET momentum::diff_ik APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::diff_ik PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_diff_ik.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::diff_ik )
+list(APPEND _cmake_import_check_files_for_momentum::diff_ik "${_IMPORT_PREFIX}/lib/momentum_diff_ik.lib" )
+
+# Import target "momentum::io_common" for configuration "Release"
+set_property(TARGET momentum::io_common APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_common PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_common.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_common )
+list(APPEND _cmake_import_check_files_for_momentum::io_common "${_IMPORT_PREFIX}/lib/momentum_io_common.lib" )
+
+# Import target "momentum::io_skeleton" for configuration "Release"
+set_property(TARGET momentum::io_skeleton APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_skeleton PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_skeleton.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_skeleton )
+list(APPEND _cmake_import_check_files_for_momentum::io_skeleton "${_IMPORT_PREFIX}/lib/momentum_io_skeleton.lib" )
+
+# Import target "momentum::io_shape" for configuration "Release"
+set_property(TARGET momentum::io_shape APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_shape PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_shape.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_shape )
+list(APPEND _cmake_import_check_files_for_momentum::io_shape "${_IMPORT_PREFIX}/lib/momentum_io_shape.lib" )
+
+# Import target "momentum::io_fbx" for configuration "Release"
+set_property(TARGET momentum::io_fbx APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_fbx PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_fbx.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_fbx )
+list(APPEND _cmake_import_check_files_for_momentum::io_fbx "${_IMPORT_PREFIX}/lib/momentum_io_fbx.lib" )
+
+# Import target "momentum::io_gltf" for configuration "Release"
+set_property(TARGET momentum::io_gltf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_gltf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_gltf.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_gltf )
+list(APPEND _cmake_import_check_files_for_momentum::io_gltf "${_IMPORT_PREFIX}/lib/momentum_io_gltf.lib" )
+
+# Import target "momentum::io_urdf" for configuration "Release"
+set_property(TARGET momentum::io_urdf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_urdf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_urdf.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_urdf )
+list(APPEND _cmake_import_check_files_for_momentum::io_urdf "${_IMPORT_PREFIX}/lib/momentum_io_urdf.lib" )
+
+# Import target "momentum::io_legacy_json" for configuration "Release"
+set_property(TARGET momentum::io_legacy_json APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_legacy_json PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_legacy_json.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_legacy_json )
+list(APPEND _cmake_import_check_files_for_momentum::io_legacy_json "${_IMPORT_PREFIX}/lib/momentum_io_legacy_json.lib" )
+
+# Import target "momentum::io_motion" for configuration "Release"
+set_property(TARGET momentum::io_motion APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_motion PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_motion.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_motion )
+list(APPEND _cmake_import_check_files_for_momentum::io_motion "${_IMPORT_PREFIX}/lib/momentum_io_motion.lib" )
+
+# Import target "momentum::io_marker" for configuration "Release"
+set_property(TARGET momentum::io_marker APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io_marker PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io_marker.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io_marker )
+list(APPEND _cmake_import_check_files_for_momentum::io_marker "${_IMPORT_PREFIX}/lib/momentum_io_marker.lib" )
+
+# Import target "momentum::io" for configuration "Release"
+set_property(TARGET momentum::io APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::io PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_io.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::io )
+list(APPEND _cmake_import_check_files_for_momentum::io "${_IMPORT_PREFIX}/lib/momentum_io.lib" )
+
+# Import target "momentum::marker_tracker" for configuration "Release"
+set_property(TARGET momentum::marker_tracker APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::marker_tracker PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_marker_tracker.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::marker_tracker )
+list(APPEND _cmake_import_check_files_for_momentum::marker_tracker "${_IMPORT_PREFIX}/lib/momentum_marker_tracker.lib" )
+
+# Import target "momentum::app_utils" for configuration "Release"
+set_property(TARGET momentum::app_utils APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::app_utils PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_app_utils.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::app_utils )
+list(APPEND _cmake_import_check_files_for_momentum::app_utils "${_IMPORT_PREFIX}/lib/momentum_app_utils.lib" )
+
+# Import target "momentum::process_markers" for configuration "Release"
+set_property(TARGET momentum::process_markers APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::process_markers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_process_markers.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::process_markers )
+list(APPEND _cmake_import_check_files_for_momentum::process_markers "${_IMPORT_PREFIX}/lib/momentum_process_markers.lib" )
+
+# Import target "momentum::rerun" for configuration "Release"
+set_property(TARGET momentum::rerun APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(momentum::rerun PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/momentum_rerun.lib"
+  )
+
+list(APPEND _cmake_import_check_targets momentum::rerun )
+list(APPEND _cmake_import_check_files_for_momentum::rerun "${_IMPORT_PREFIX}/lib/momentum_rerun.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
