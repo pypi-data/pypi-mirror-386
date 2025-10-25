c5adbb1e648087e72f46b742c7500752efb78421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:20:56 +0000

    Add package django-whatsapp-api-wrapper/0.6.2/django_whatsapp_api_wrapper-0.6.2-py3-none-any.whl

---
+++ packages/django-whatsapp-api-wrapper/django_whatsapp_api_wrapper-0.6.2-py3-none-any.whl/django_whatsapp_api_wrapper/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import whatsapp_webhook
+from django.urls import include
+
+urlpatterns = [
+    path("webhook/", whatsapp_webhook, name="whatsapp_webhook"),
+    path("", include("django_whatsapp_api_wrapper.whatsapp_cloud_api.templates.urls")),
+    path("messages/", include("django_whatsapp_api_wrapper.whatsapp_cloud_api.messages.api.urls")),
+    path("embedded-signup/", include("django_whatsapp_api_wrapper.embedded_signup.urls")),
+]
