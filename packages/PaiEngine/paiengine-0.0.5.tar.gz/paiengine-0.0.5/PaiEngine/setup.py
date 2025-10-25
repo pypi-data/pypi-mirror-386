349e942f9d7e841a8d9598e27a07c33c65843184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:06:36 +0000

    Add package PaiEngine/0.0.5/paiengine-0.0.5.tar.gz

---
+++ packages/PaiEngine/paiengine-0.0.5.tar.gz/paiengine-0.0.5/PaiEngine/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+from PaiEngine import Attribute
+
+current_folder = Path(__file__).parent
+packages_folder = find_packages(where=str(current_folder))
+
+setup(
+    name="PaiEngine",
+    version=Attribute.version,
+    packages=packages_folder,
+    install_requires=[
+        "pygame>=2.0.0"
+    ],
+    description="PaiEngine - UI engine",
+    long_description=(current_folder / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    author="Pai100707",
+    url="https://github.com/Pai100707/PaiEngine",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
