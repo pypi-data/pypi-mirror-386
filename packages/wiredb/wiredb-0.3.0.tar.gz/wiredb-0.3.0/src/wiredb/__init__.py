46ff5c678c7469b3100c8edad311ff56c9d7ec91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 04:08:09 +0000

    Add package wiredb/0.3.0/wiredb-0.3.0.tar.gz

---
+++ packages/wiredb/wiredb-0.3.0.tar.gz/wiredb-0.3.0/src/wiredb/__init__.py
@@ -0,0 +1,7 @@
+from .client import ClientWire as ClientWire
+from .client import Provider as Provider
+from .client import connect as connect
+from .server import Room as Room
+from .server import RoomManager as RoomManager
+from .server import ServerWire as ServerWire
+from .server import bind as bind
