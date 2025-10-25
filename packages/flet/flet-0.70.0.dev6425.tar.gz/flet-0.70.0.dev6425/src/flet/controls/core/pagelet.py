b9e0797f79badb70241b54fb1a37b7bafff84285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 05:06:59 +0000

    Add package flet/0.70.0.dev6425/flet-0.70.0.dev6425.tar.gz

---
+++ packages/flet/flet-0.70.0.dev6425.tar.gz/flet-0.70.0.dev6425/src/flet/controls/core/pagelet.py
@@ -0,0 +1,133 @@
+from typing import Optional, Union
+
+from flet.controls.adaptive_control import AdaptiveControl
+from flet.controls.base_control import control
+from flet.controls.control import Control
+from flet.controls.cupertino.cupertino_app_bar import CupertinoAppBar
+from flet.controls.cupertino.cupertino_navigation_bar import CupertinoNavigationBar
+from flet.controls.layout_control import LayoutControl
+from flet.controls.material.app_bar import AppBar
+from flet.controls.material.bottom_app_bar import BottomAppBar
+from flet.controls.material.navigation_bar import NavigationBar
+from flet.controls.material.navigation_drawer import NavigationDrawer
+from flet.controls.transform import OffsetValue
+from flet.controls.types import ColorValue, FloatingActionButtonLocation
+
+__all__ = ["Pagelet"]
+
+
+@control("Pagelet")
+class Pagelet(LayoutControl, AdaptiveControl):
+    """
+    Implements the basic Material Design visual layout structure.
+
+    Use it for projects that require a "page within a page" layouts with its own
+    [`AppBar`][flet.], [`BottomAppBar`][flet.], [`NavigationDrawer`][flet.],
+    such as demos and galleries.
+    """
+
+    content: Control
+    """
+    A child Control contained by the Pagelet.
+
+    The control in the content of the Pagelet is positioned at the top-left of the
+    available space between the app bar and the bottom of the Pagelet.
+
+    Raises:
+        ValueError: If [`content`][(c).] is not visible.
+    """
+
+    appbar: Optional[Union[AppBar, CupertinoAppBar]] = None
+    """
+    An [`AppBar`][flet.] control to display at the top
+    of the Pagelet.
+    """
+
+    navigation_bar: Optional[Union[NavigationBar, CupertinoNavigationBar]] = None
+    """
+    A navigation bar ([`NavigationBar`][flet.] or
+    [`CupertinoNavigationBar`][flet.]) control to display
+    at the bottom of the `Pagelet`.
+
+    Note:
+        If both the `navigation_bar` and [`bottom_appbar`][(c).]
+        properties are specified, `navigation_bar` takes precedence and will
+        be displayed.
+    """
+
+    bottom_appbar: Optional[BottomAppBar] = None
+    """
+    A [`BottomAppBar`][flet.] control to display at
+    the bottom of the `Pagelet`.
+
+    Note:
+        If both the `bottom_appbar` and [`navigation_bar`][(c).]
+        properties are specified, `bottom_appbar` takes precedence and will
+        be displayed.
+    """
+
+    bottom_sheet: Optional[Control] = None
+    """
+    The persistent bottom sheet to show information that supplements the primary
+    content of the Pagelet.
+    """
+
+    drawer: Optional[NavigationDrawer] = None
+    """
+    A [`NavigationDrawer`][flet.] control to
+    display as a panel sliding from the start edge of the page.
+    """
+
+    end_drawer: Optional[NavigationDrawer] = None
+    """
+    A [`NavigationDrawer`][flet.] control to
+    display as a panel sliding from the end edge of the page.
+    """
+
+    floating_action_button: Optional[Control] = None
+    """
+    A [`FloatingActionButton`][flet.]
+    control to display on top of Pagelet content.
+    """
+
+    floating_action_button_location: Optional[
+        Union[FloatingActionButtonLocation, OffsetValue]
+    ] = FloatingActionButtonLocation.END_FLOAT
+    """
+    Defines a position for the `FloatingActionButton`.
+    """
+
+    bgcolor: Optional[ColorValue] = None
+    """
+    Background color of the Pagelet.
+    """
+
+    def before_update(self):
+        super().before_update()
+        if not self.content.visible:
+            raise ValueError("content must be visible")
+
+    # todo: deprecate show_* in favor of a open/close methods, or page.open/close
+    # Drawer
+    #
+    def show_drawer(self, drawer: NavigationDrawer):
+        self.drawer = drawer
+        self.drawer.open = True
+        self.update()
+
+    def close_drawer(self):
+        if self.drawer is not None:
+            self.drawer.open = False
+            self.update()
+
+    # End_drawer
+    #
+    def show_end_drawer(self, end_drawer: NavigationDrawer):
+        self.end_drawer = end_drawer
+        self.end_drawer.open = True
+        self.update()
+
+    def close_end_drawer(self):
+        if self.end_drawer is not None:
+            self.end_drawer.open = False
+            self.update()
---
+++ packages/flet/flet-0.70.0.dev6425.tar.gz/flet-0.70.0.dev6425/src/flet/controls/scrollable_control.py
@@ -0,0 +1,159 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional, Union
+
+from flet.controls.animation import AnimationCurve
+from flet.controls.base_control import control
+from flet.controls.control import Control
+from flet.controls.control_event import Event, EventHandler
+from flet.controls.duration import DurationValue
+from flet.controls.keys import ScrollKey
+from flet.controls.types import (
+    Number,
+    ScrollMode,
+)
+
+__all__ = ["OnScrollEvent", "ScrollDirection", "ScrollType", "ScrollableControl"]
+
+
+class ScrollType(Enum):
+    START = "start"
+    UPDATE = "update"
+    END = "end"
+    USER = "user"
+    OVERSCROLL = "overscroll"
+
+
+class ScrollDirection(Enum):
+    IDLE = "idle"
+    FORWARD = "forward"
+    REVERSE = "reverse"
+
+
+@dataclass
+class OnScrollEvent(Event["ScrollableControl"]):
+    event_type: ScrollType
+    pixels: float
+    min_scroll_extent: float
+    max_scroll_extent: float
+    viewport_dimension: float
+    scroll_delta: Optional[float] = None
+    direction: Optional[ScrollDirection] = None
+    overscroll: Optional[float] = None
+    velocity: Optional[float] = None
+
+
+@control(kw_only=True)
+class ScrollableControl(Control):
+    scroll: Optional[ScrollMode] = None
+    """
+    Enables a vertical scrolling for the Column to prevent its content overflow.
+
+    Defaults to `ScrollMode.None`.
+    """
+
+    auto_scroll: bool = False
+    """
+    `True` if scrollbar should automatically move its position to the end when children
+    updated. Must be `False` for `scroll_to()` method to work.
+    """
+
+    scroll_interval: Number = 10
+    """
+    Throttling in milliseconds for `on_scroll` event.
+    """
+
+    on_scroll: Optional[EventHandler[OnScrollEvent]] = None
+    """
+    Called when scroll position is changed by a user.
+    class.
+    """
+
+    async def scroll_to(
+        self,
+        offset: Optional[float] = None,
+        delta: Optional[float] = None,
+        scroll_key: Union[ScrollKey, str, int, float, bool, None] = None,
+        duration: Optional[DurationValue] = None,
+        curve: Optional[AnimationCurve] = None,
+    ):
+        """
+        Moves scroll position to either absolute `offset`, relative `delta` or jump to
+        the control with specified `key`.
+
+        `offset` is an absolute value between minimum and maximum extents of a
+        scrollable control, for example:
+
+        ```python
+        await products.scroll_to(offset=100, duration=1000)
+        ```
+
+        `offset` could be a negative to scroll from the end of a scrollable. For
+        example, to scroll to the very end:
+
+        ```python
+        await products.scroll_to(offset=-1, duration=1000)
+        ```
+
+        `delta` allows moving scroll relatively to the current position. Use positive
+        `delta` to scroll forward and negative `delta` to scroll backward. For example,
+        to move scroll on 50 pixels forward:
+
+        ```python
+        await products.scroll_to(delta=50)
+        ```
+
+        `key` allows moving scroll position to a control with specified `key`. Most of
+        Flet controls have `key` property which is translated to Flutter as
+        "global key". `key` must be unique for the entire page/view. For example:
+
+        ```python
+        import flet as ft
+
+
+        def main(page: ft.Page):
+            cl = ft.Column(
+                spacing=10,
+                height=200,
+                width=200,
+                scroll=ft.ScrollMode.ALWAYS,
+            )
+            for i in range(0, 50):
+                cl.controls.append(ft.Text(f"Text line {i}", key=str(i)))
+
+            async def scroll_to_key(e):
+                await cl.scroll_to(scroll_key="20", duration=1000)
+
+            page.add(
+                ft.Container(cl, border=ft.border.all(1)),
+                ft.Button("Scroll to key '20'", on_click=scroll_to_key),
+            )
+
+
+        ft.run(main)
+        ```
+
+        Note:
+            `scroll_to()` method won't work with `ListView` and `GridView` controls
+            building their items dynamically.
+
+        `duration` is scrolling animation duration in milliseconds. Defaults to `0` -
+        no animation.
+
+        `curve` configures animation curve. Property value is
+        [`AnimationCurve`][flet.]
+        enum.
+
+        Defaults to `AnimationCurve.EASE`.
+        """
+
+        await self._invoke_method(
+            "scroll_to",
+            {
+                "offset": offset,
+                "delta": delta,
+                "scroll_key": scroll_key,
+                "duration": duration,
+                "curve": curve,
+            },
+        )
