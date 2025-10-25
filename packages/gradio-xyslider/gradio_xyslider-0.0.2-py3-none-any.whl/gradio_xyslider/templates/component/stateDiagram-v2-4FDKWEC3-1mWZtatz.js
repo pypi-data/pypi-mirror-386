0eb8588e74784ec8d95c6d55bb0f39832c10bb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:34 +0000

    Add package gradio-xyslider/0.0.2/gradio_xyslider-0.0.2-py3-none-any.whl

---
+++ packages/gradio-xyslider/gradio_xyslider-0.0.2-py3-none-any.whl/gradio_xyslider/templates/component/stateDiagram-v2-4FDKWEC3-1mWZtatz.js
@@ -0,0 +1,16 @@
+import { s as a, b as t, a as r, S as s } from "./chunk-DI55MBZ5-BMSeB6c7.js";
+import { _ as i } from "./mermaid.core-CSMXsFf-.js";
+var _ = {
+  parser: r,
+  get db() {
+    return new s(2);
+  },
+  renderer: t,
+  styles: a,
+  init: /* @__PURE__ */ i((e) => {
+    e.state || (e.state = {}), e.state.arrowMarkerAbsolute = e.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  _ as diagram
+};
