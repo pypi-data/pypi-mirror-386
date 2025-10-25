bdeeb6a1f415de939c92cf7b55da6827265492a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:06:33 +0000

    Add package cprs/0.1.0/cprs-0.1.0-cp311-cp311-manylinux_2_34_aarch64.whl

---
+++ packages/cprs/cprs-0.1.0-cp311-cp311-manylinux_2_34_aarch64.whl/cosine_similarity_module/__init__.py
@@ -0,0 +1,5 @@
+from .cosine_similarity_module import *
+
+__doc__ = cosine_similarity_module.__doc__
+if hasattr(cosine_similarity_module, "__all__"):
+    __all__ = cosine_similarity_module.__all__
