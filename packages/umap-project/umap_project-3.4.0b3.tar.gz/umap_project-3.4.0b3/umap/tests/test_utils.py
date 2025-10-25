c6ebe6e3a06457746d797c80a6e504374dcabf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:07:04 +0000

    Add package umap-project/3.4.0b3/umap_project-3.4.0b3.tar.gz

---
+++ packages/umap-project/umap_project-3.4.0b3.tar.gz/umap_project-3.4.0b3/umap/tests/test_utils.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import pytest
+
+from umap.utils import gzip_file, normalize_string
+
+
+def test_gzip_file():
+    # Let's use any old file so we can check that the date of the gzip file is set.
+    src = Path(__file__).parent / "settings.py"
+    dest = Path("/tmp/test_settings.py.gz")
+    gzip_file(src, dest)
+    src_stat = src.stat()
+    dest_stat = dest.stat()
+    dest.unlink()
+    assert src_stat.st_mtime == dest_stat.st_mtime
+
+
+@pytest.mark.parametrize(
+    "input,output",
+    (
+        ("Vélo", "velo"),
+        ("Éducation", "education"),
+        ("stävänger", "stavanger"),
+    ),
+)
+def test_normalize_string(input, output):
+    assert normalize_string(input) == output
