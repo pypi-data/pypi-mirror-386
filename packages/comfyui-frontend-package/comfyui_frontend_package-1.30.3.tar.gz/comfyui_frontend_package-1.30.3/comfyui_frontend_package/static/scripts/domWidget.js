f67941026ddb37583b390c2f365f9b9e320b6ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:32 +0000

    Add package comfyui-frontend-package/1.30.3/comfyui_frontend_package-1.30.3.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.30.3.tar.gz/comfyui_frontend_package-1.30.3/comfyui_frontend_package/static/scripts/domWidget.js
@@ -0,0 +1,6 @@
+// Shim for scripts/domWidget.ts
+export const isDOMWidget = window.comfyAPI.domWidget.isDOMWidget;
+export const isComponentWidget = window.comfyAPI.domWidget.isComponentWidget;
+export const DOMWidgetImpl = window.comfyAPI.domWidget.DOMWidgetImpl;
+export const ComponentWidgetImpl = window.comfyAPI.domWidget.ComponentWidgetImpl;
+export const addWidget = window.comfyAPI.domWidget.addWidget;
