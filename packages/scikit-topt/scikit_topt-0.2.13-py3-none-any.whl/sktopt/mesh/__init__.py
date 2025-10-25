2aaacfcf7d6f0247df2804bed7a0326ce724591e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:06:44 +0000

    Add package scikit-topt/0.2.13/scikit_topt-0.2.13-py3-none-any.whl

---
+++ packages/scikit-topt/scikit_topt-0.2.13-py3-none-any.whl/sktopt/mesh/__init__.py
@@ -0,0 +1,10 @@
+
+from sktopt.mesh.task import TaskConfig
+from sktopt.mesh import toy_problem
+from sktopt.mesh import loader
+
+__all__ = [
+    "TaskConfig",
+    "toy_problem",
+    "loader"
+]
