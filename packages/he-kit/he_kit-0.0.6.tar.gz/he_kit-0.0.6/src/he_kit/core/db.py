1b0dee0e8a05eb24a20c6bfd55e74205209f18e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:06:32 +0000

    Add package he-kit/0.0.6/he_kit-0.0.6.tar.gz

---
+++ packages/he-kit/he_kit-0.0.6.tar.gz/he_kit-0.0.6/src/he_kit/core/db.py
@@ -0,0 +1,45 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+engine: AsyncEngine | None = None
+session_maker: async_sessionmaker[AsyncSession] | None = None
+
+
+@asynccontextmanager
+async def db_lifespan(app):
+    """Set up and tear down the database engine/sessionmaker."""
+    global engine, session_maker
+
+    engine = create_async_engine(
+        app.settings.DB_URL,
+        echo=app.settings.DB_ECHO_SQL,
+        pool_pre_ping=True,
+    )
+    session_maker = async_sessionmaker(engine, expire_on_commit=False, autoflush=False)
+
+    yield
+
+    await engine.dispose()
+    engine = None
+    session_maker = None
+
+
+async def db_session() -> AsyncIterator[AsyncSession]:
+    """Provide a databse session for use in dependency injection."""
+    assert session_maker is not None, "Database not initialized"
+    async with session_maker() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
