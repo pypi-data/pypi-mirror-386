3cf39732ef8b2014afa2372078ed0fbb9aa156c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:06:39 +0000

    Add package nexios/3.0.0.post0/nexios-3.0.0.post0-py3-none-any.whl

---
+++ packages/nexios/nexios-3.0.0.post0-py3-none-any.whl/nexios/templates/standard/main.py
@@ -0,0 +1,24 @@
+from nexios import NexiosApp
+
+from nexios.templating import render, TemplateEngine
+from routes.index.route import index_router
+
+
+app = NexiosApp()
+engine = TemplateEngine()
+engine.setup_environment()
+
+
+app.mount_router(index_router)
+
+
+@app.on_startup
+async def startup():
+    """Function that runs on application startup."""
+    print("{{project_name_title}} starting up...")
+
+
+@app.on_shutdown
+async def shutdown():
+    """Function that runs on application shutdown."""
+    print("{{project_name_title}} shutting down...")
