707ed83a59a36e1e4b914069884689869b07c9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:46 +0000

    Add package e2b-devcontainers/0.1.3/e2b_devcontainers-0.1.3-py3-none-any.whl

---
+++ packages/e2b-devcontainers/e2b_devcontainers-0.1.3-py3-none-any.whl/e2b_devcontainers/utils.py
@@ -0,0 +1,9 @@
+def escape_string(str: str) -> str:
+    return (
+        str.replace("\\", "\\\\")
+        .replace("'", "\\'")
+        .replace('"', '\\"')
+        .replace("\n", "\\n")
+        .replace("\r", "\\r")
+        .replace("\t", "\\t")
+    )
