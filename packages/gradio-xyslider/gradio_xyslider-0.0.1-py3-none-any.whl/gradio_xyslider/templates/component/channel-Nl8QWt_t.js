6afd14d1a9e4c85aba1790235499953493027bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 14:07:26 +0000

    Add package gradio-xyslider/0.0.1/gradio_xyslider-0.0.1-py3-none-any.whl

---
+++ packages/gradio-xyslider/gradio_xyslider-0.0.1-py3-none-any.whl/gradio_xyslider/templates/component/channel-Nl8QWt_t.js
@@ -0,0 +1,5 @@
+import { ap as o, aq as n } from "./mermaid.core-CAtCjjG5.js";
+const t = (a, r) => o.lang.round(n.parse(a)[r]);
+export {
+  t as c
+};
---
+++ packages/gradio-xyslider/gradio_xyslider-0.0.1-py3-none-any.whl/gradio_xyslider/templates/component/index.js
@@ -0,0 +1,4 @@
+import { I as f } from "./Index-DBXTq2RP.js";
+export {
+  f as default
+};
