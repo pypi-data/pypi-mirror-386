298510ee381ed80ce9218567466170c16fbd6d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 06:06:53 +0000

    Add package leksara/0.1.3/leksara-0.1.3.tar.gz

---
+++ packages/leksara/leksara-0.1.3.tar.gz/leksara-0.1.3/leksara/functions/cleaner/__init__.py
@@ -0,0 +1,11 @@
+# cleaner subpackage
+__all__ = [
+    "remove_tags",
+    "case_normal",
+    "remove_stopwords",
+    "remove_whitespace",
+    "remove_punctuation",
+    "remove_digits",
+    "replace_url",
+    "remove_emoji"
+]
---
+++ packages/leksara/leksara-0.1.3.tar.gz/leksara-0.1.3/leksara/functions/review/__init__.py
@@ -0,0 +1,9 @@
+# review subpackage
+__all__ = [
+    "replace_rating",
+    "shorten_elongation",
+    "replace_acronym",
+    "normalize_slangs",
+    "expand_contraction",
+    "word_normalization"
+]
