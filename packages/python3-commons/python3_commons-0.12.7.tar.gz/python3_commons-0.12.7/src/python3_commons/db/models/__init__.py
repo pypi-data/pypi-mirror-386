680354e3062cc8e213fad9821d20699df3b4b886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:07:02 +0000

    Add package python3-commons/0.12.7/python3_commons-0.12.7.tar.gz

---
+++ packages/python3-commons/python3_commons-0.12.7.tar.gz/python3_commons-0.12.7/src/python3_commons/db/models/__init__.py
@@ -0,0 +1,8 @@
+from python3_commons.db.models.auth import ApiKey as ApiKey
+from python3_commons.db.models.auth import User as User
+from python3_commons.db.models.auth import UserGroup as UserGroup
+from python3_commons.db.models.rbac import RBACApiKeyRole as RBACApiKeyRole
+from python3_commons.db.models.rbac import RBACPermission as RBACPermission
+from python3_commons.db.models.rbac import RBACRole as RBACRole
+from python3_commons.db.models.rbac import RBACRolePermission as RBACRolePermission
+from python3_commons.db.models.rbac import RBACUserRole as RBACUserRole
