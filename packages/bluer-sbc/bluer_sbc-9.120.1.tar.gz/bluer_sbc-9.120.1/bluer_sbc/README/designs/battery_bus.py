28f7a24bb2cf5eab97765a38fb82674c962703de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 07:06:07 +0000

    Add package bluer-sbc/9.120.1/bluer_sbc-9.120.1.tar.gz

---
+++ packages/bluer-sbc/bluer_sbc-9.120.1.tar.gz/bluer_sbc-9.120.1/bluer_sbc/README/designs/battery_bus.py
@@ -0,0 +1,36 @@
+from bluer_objects import README
+from bluer_objects.README.items import ImageItems
+from bluer_objects.README.consts import assets_url
+from bluer_objects.README.consts import designs_url
+
+from bluer_sbc.designs.battery_bus.parts import dict_of_parts as parts
+
+assets2 = assets_url(
+    suffix="battery-bus",
+    volume=2,
+)
+
+marquee = README.Items(
+    [
+        {
+            "name": "battery bus",
+            "marquee": f"{assets2}/20251007_221902.jpg",
+            "url": "./bluer_sbc/docs/battery-bus.md",
+        }
+    ]
+)
+
+items = ImageItems(
+    {
+        f"{assets2}/concept.png": "",
+        designs_url(
+            "battery-bus/electrical/wiring.png?raw=true",
+        ): designs_url(
+            "battery-bus/electrical/wiring.svg",
+        ),
+        f"{assets2}/20251007_221902.jpg": "",
+        f"{assets2}/20251007_220642.jpg": "",
+        f"{assets2}/20251007_220520.jpg": "",
+        f"{assets2}/20251007_220601.jpg": "",
+    }
+)
