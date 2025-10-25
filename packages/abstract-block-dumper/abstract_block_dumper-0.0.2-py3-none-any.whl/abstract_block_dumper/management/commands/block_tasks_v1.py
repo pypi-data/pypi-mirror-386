417e5cfb7f2104d25e62e30f406fded9d877d513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 03:20:57 +0000

    Add package abstract-block-dumper/0.0.2/abstract_block_dumper-0.0.2-py3-none-any.whl

---
+++ packages/abstract-block-dumper/abstract_block_dumper-0.0.2-py3-none-any.whl/abstract_block_dumper/management/commands/block_tasks_v1.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from abstract_block_dumper._internal.dal.memory_registry import task_registry
+from abstract_block_dumper._internal.discovery import ensure_modules_loaded
+from abstract_block_dumper._internal.services.scheduler import task_scheduler_factory
+
+
+class Command(BaseCommand):
+    help = "Run the block scheduler daemon."
+
+    def handle(self, *args, **options) -> None:
+        self.stdout.write("Syncing decorated functions...")
+        ensure_modules_loaded()
+        functions_counter = len(task_registry.get_functions())
+        self.stdout.write(self.style.SUCCESS(f"Synced {functions_counter} functions"))
+
+        scheduler = task_scheduler_factory()
+        self.stdout.write("Starting block scheduler...")
+        scheduler.start()
