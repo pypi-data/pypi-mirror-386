663d346df19563a81149973978959311c36dc12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 04:07:33 +0000

    Add package mat3ra-code/2025.10.24.post0/mat3ra_code-2025.10.24.post0.tar.gz

---
+++ packages/mat3ra-code/mat3ra_code-2025.10.24.post0.tar.gz/mat3ra_code-2025.10.24.post0/tests/py/unit/__init__.py
@@ -0,0 +1,70 @@
+import json
+from enum import Enum
+
+from mat3ra.code.entity import InMemoryEntityPydantic
+from pydantic import BaseModel
+
+REFERENCE_OBJECT_VALID = {"key1": "value1", "key2": 1}
+REFERENCE_OBJECT_VALID_WITH_EXTRA_KEY = {"key1": "value1", "key2": 1, "key-to-clean": "will-be-removed"}
+REFERENCE_OBJECT_VALID_WITH_MISSING_KEY = {"key2": 1}
+REFERENCE_OBJECT_VALID_UPDATED = {"key1": "value1-updated", "key2": 2}
+REFERENCE_OBJECT_INVALID = {"key1": "value1", "key2": "value2"}
+REFERENCE_OBJECT_VALID_JSON = json.dumps(REFERENCE_OBJECT_VALID)
+REFERENCE_OBJECT_NESTED_VALID = {"nested_key1": {**REFERENCE_OBJECT_VALID}}
+REFERENCE_OBJECT_NESTED_VALID_UPDATED = {"nested_key1": {**REFERENCE_OBJECT_VALID_UPDATED}}
+
+REFERENCE_OBJECT_DOUBLE_NESTED_VALID = {"double_nested_key1": {**REFERENCE_OBJECT_NESTED_VALID}}
+
+
+class ExampleSchema(BaseModel):
+    key1: str
+    key2: int
+
+
+class ExampleDefaultableSchema(BaseModel):
+    key1: str = "value1"
+    key2: int
+
+
+class ExampleNestedSchema(BaseModel):
+    nested_key1: ExampleSchema
+
+
+class ExampleDoubleNestedSchema(BaseModel):
+    double_nested_key1: ExampleNestedSchema
+
+
+class ExampleClass(ExampleSchema, InMemoryEntityPydantic):
+    pass
+
+
+class ExampleDefaultableClass(ExampleDefaultableSchema, InMemoryEntityPydantic):
+    pass
+
+
+class ExampleNestedClass(ExampleNestedSchema, InMemoryEntityPydantic):
+    @property
+    def nested_key1_instance(self) -> ExampleClass:
+        return ExampleClass.create(self.nested_key1.model_dump())
+
+
+class ExampleNestedKeyAsClassInstanceClass(ExampleNestedSchema, InMemoryEntityPydantic):
+    nested_key1: ExampleClass
+
+
+class ExampleDoubleNestedKeyAsClassInstancesClass(ExampleDoubleNestedSchema, InMemoryEntityPydantic):
+    double_nested_key1: ExampleNestedKeyAsClassInstanceClass
+
+
+class SampleEnum(str, Enum):
+    VALUE1 = "value1"
+    VALUE2 = "value2"
+
+
+class SampleModelWithEnum(BaseModel):
+    type: SampleEnum
+    name: str = "test"
+
+
+class SampleEntityWithEnum(SampleModelWithEnum, InMemoryEntityPydantic):
+    pass
