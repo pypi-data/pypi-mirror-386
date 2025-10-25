52e313c59b5dca9d8070cdd060c5a912dda106ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 15:06:57 +0000

    Add package square-database-helper/2.6.3/square_database_helper-2.6.3-py3-none-any.whl

---
+++ packages/square-database-helper/square_database_helper-2.6.3-py3-none-any.whl/square_database_helper/pydantic_models.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, Optional, List
+
+from pydantic import BaseModel, RootModel
+
+
+class FilterConditionsV0(BaseModel):
+    eq: Optional[Any] = None
+    ne: Optional[Any] = None
+    lt: Optional[Any] = None
+    lte: Optional[Any] = None
+    gt: Optional[Any] = None
+    gte: Optional[Any] = None
+    like: Optional[str] = None
+    in_: Optional[List[Any]] = None
+    is_null: Optional[bool] = None
+
+
+class FiltersV0(RootModel):
+    root: Dict[str, FilterConditionsV0]
